8ca8258a27652a04020ef83f708f680474ba96af
zehrasenakgul <zehrasenakgul@gmail.com>
2023-09-11 11:43:12 +0300

    Added page title and content to the about page

--- resources/views/client/blank.blade.php
+++ resources/views/client/blank.blade.php
@@ -1,8 +1,21 @@
 @extends('client.layouts.master')
 @section('content')
-    <section class="properties-right list featured portfolio blog pt-5 bg-white">
+    <section class="about-section" style="padding: 60px 0">
         <div class="container">
-            {!! $pageInfo->content !!}
+            <div class="row">
+                <div class="col-lg-6 offset-lg-3">
+                    <div class="main-title text-center">
+                        <h2 class="mt0">{{ $pageInfo->title }}</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12 col-xl-12">
+                    <div class="about_content">
+                        <p> {!! $pageInfo->content !!} </p>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 @endsection
