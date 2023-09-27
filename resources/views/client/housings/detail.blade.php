48ebaab66dedcfcda2fbccad6f8734f152d63e0c
zehrasenakgul <zehrasenakgul@gmail.com>
2023-09-27 14:13:36 +0300

    Update housing detail page with instruction tuning and RLHF (generated by useblackbox.ai )

--- resources/views/client/housings/detail.blade.php
+++ resources/views/client/housings/detail.blade.php
@@ -3,14 +3,16 @@
 
 @php
 
-    function getData($housing,$key){
-        $housing_type_data= json_decode($housing->housing_type_data);
+    function getData($housing, $key)
+    {
+        $housing_type_data = json_decode($housing->housing_type_data);
         $a = $housing_type_data->$key;
         return $a[0];
     }
 
-    function getImages($housing,$key){
-        $housing_type_data= json_decode($housing->housing_type_data);
+    function getImages($housing, $key)
+    {
+        $housing_type_data = json_decode($housing->housing_type_data);
         $a = $housing_type_data->$key;
         return $a;
     }
@@ -38,12 +40,12 @@
                                 <div class="single detail-wrapper mr-2">
                                     <div class="detail-wrapper-body">
                                         <div class="listing-title-bar">
-                                            <h4>{{getData($housing,'price')}} TL</h4>
+                                            <h4>{{ getData($housing, 'price') }} TL</h4>
                                             <!-- <div class="mt-0">
-                                                                        <a href="#listing-location" class="listing-address">
-                                                                            <p>$ 1,200 / sq ft</p>
-                                                                        </a>
-                                                                    </div> -->
+                                                                                                                <a href="#listing-location" class="listing-address">
+                                                                                                                    <p>$ 1,200 / sq ft</p>
+                                                                                                                </a>
+                                                                                                            </div> -->
                                         </div>
                                     </div>
                                 </div>
@@ -53,24 +55,18 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-lg-8 col-md-12 blog-pots">
+                <div class="col-md-7 blog-pots">
                     <div class="row">
                         <div class="col-md-12">
                             <!-- main slider carousel items -->
                             <div id="listingDetailsSlider" class="carousel listing-details-sliders slide mb-30">
                                 <!-- <h5 class="mb-4">Gallery</h5> -->
                                 <div class="carousel-inner">
-                                    @php
-                                        $i = 0;
-                                    @endphp
-                                    @foreach (json_decode(getImages($housing,'images')) as $image)
-                                        <div class="item carousel-item {{ $i == 0 ? 'active' : '' }}"
-                                            data-slide-number="{{ $i }}">
+                                    @foreach (json_decode(getImages($housing, 'images')) as $key => $image)
+                                        <div class="item carousel-item {{ $key == 0 ? 'active' : '' }}"
+                                            data-slide-number="{{ $key }}">
                                             <img src="{{ asset('housing_images/' . $image) }}" class="img-fluid"
                                                 alt="slider-listing">
-                                            @php
-                                                $i++;
-                                            @endphp
                                         </div>
                                     @endforeach
 
@@ -83,20 +79,15 @@
                                 </div>
                                 <!-- main slider carousel nav controls -->
                                 <ul class="carousel-indicators smail-listing list-inline">
-                                    @php
-                                        $j = 0;
-                                    @endphp
-                                    @foreach ($housing->images as $image)
-                                        <li class="list-inline-item {{ $i == 0 ? 'active' : '' }}">
-                                            <a id="carousel-selector-0" class="selected" data-slide-to="0"
-                                                data-target="#listingDetailsSlider">
-                                                <img src="{{ asset('storage/' . $image->imagepath) }}" class="img-fluid"
+
+                                    @foreach (json_decode(getImages($housing, 'images')) as $imageKey => $image)
+                                        <li class="list-inline-item {{ $imageKey == 0 ? 'active' : '' }}">
+                                            <a id="carousel-selector-{{ $imageKey }}" class="selected"
+                                                data-slide-to="{{ $imageKey }}" data-target="#listingDetailsSlider">
+                                                <img src="{{ asset('housing_images/' . $image) }}" class="img-fluid"
                                                     alt="listing-small">
                                             </a>
                                         </li>
-                                        @php
-                                            $j++;
-                                        @endphp
                                     @endforeach
 
                                 </ul>
@@ -108,7 +99,7 @@
 
 
                 </div>
-                <aside class="col-lg-4 col-md-12 car">
+                <aside class="col-md-5  car">
                     <div class="single widget">
                         <!-- Start: Schedule a Tour -->
                         <div class="schedule widget-boxed mt-33 mt-0" style="text-align: center;">
@@ -116,7 +107,8 @@
                                 <h4><i class="fa fa-calendar pr-3 padd-r-10"></i>İletişim Bilgileri</h4>
                             </div>
                             <div class="widget-boxed-body">
-                                <h5 class="" style="font-size: 20px; font-weight: 600;">{{$housing->brand?->title}}</h5>
+                                <h5 class="" style="font-size: 20px; font-weight: 600;">{{ $housing->brand?->title }}
+                                </h5>
                                 <p>0507 585 40 33</p>
                             </div>
                         </div>
@@ -148,55 +140,55 @@
                     <!-- title -->
                     <h5 class="mb-4">Bina Özellikleri</h5>
                     <ul class="homes-list clearfix">
-                        @foreach (json_decode($housing->housing_type_data) as $key=>$val)
-                          <li style="border: none !important;">
-                            <span class="font-weight-bold mr-1">{{$key}}:</span>
-                            <span class="det">{{json_encode($val[0])}}</span>
-                        </li>
+                        @foreach (json_decode($housing->housing_type_data) as $key => $val)
+                            <li style="border: none !important;">
+                                <span class="font-weight-bold mr-1">{{ $key }}:</span>
+                                <span class="det">{{ json_encode($val[0]) }}</span>
+                            </li>
                         @endforeach
 
                     </ul>
 
                     <!-- <h5 class="mt-5">Amenities</h5>
 
-                                                            <ul class="homes-list clearfix">
-                                                                <li>
-                                                                    <i class="fa fa-check-square" aria-hidden="true"></i>
-                                                                    <span>Air Cond</span>
-                                                                </li>
-                                                                <li>
-                                                                    <i class="fa fa-check-square" aria-hidden="true"></i>
-                                                                    <span>Balcony</span>
-                                                                </li>
-                                                                <li>
-                                                                    <i class="fa fa-check-square" aria-hidden="true"></i>
-                                                                    <span>Internet</span>
-                                                                </li>
-                                                                <li>
-                                                                    <i class="fa fa-check-square" aria-hidden="true"></i>
-                                                                    <span>Dishwasher</span>
-                                                                </li>
-                                                                <li>
-                                                                    <i class="fa fa-check-square" aria-hidden="true"></i>
-                                                                    <span>Bedding</span>
-                                                                </li>
-                                                                <li>
-                                                                    <i class="fa fa-check-square" aria-hidden="true"></i>
-                                                                    <span>Cable TV</span>
-                                                                </li>
-                                                                <li>
-                                                                    <i class="fa fa-check-square" aria-hidden="true"></i>
-                                                                    <span>Parking</span>
-                                                                </li>
-                                                                <li>
-                                                                    <i class="fa fa-check-square" aria-hidden="true"></i>
-                                                                    <span>Pool</span>
-                                                                </li>
-                                                                <li>
-                                                                    <i class="fa fa-check-square" aria-hidden="true"></i>
-                                                                    <span>Fridge</span>
-                                                                </li>
-                                                            </ul> -->
+                                                                                                    <ul class="homes-list clearfix">
+                                                                                                        <li>
+                                                                                                            <i class="fa fa-check-square" aria-hidden="true"></i>
+                                                                                                            <span>Air Cond</span>
+                                                                                                        </li>
+                                                                                                        <li>
+                                                                                                            <i class="fa fa-check-square" aria-hidden="true"></i>
+                                                                                                            <span>Balcony</span>
+                                                                                                        </li>
+                                                                                                        <li>
+                                                                                                            <i class="fa fa-check-square" aria-hidden="true"></i>
+                                                                                                            <span>Internet</span>
+                                                                                                        </li>
+                                                                                                        <li>
+                                                                                                            <i class="fa fa-check-square" aria-hidden="true"></i>
+                                                                                                            <span>Dishwasher</span>
+                                                                                                        </li>
+                                                                                                        <li>
+                                                                                                            <i class="fa fa-check-square" aria-hidden="true"></i>
+                                                                                                            <span>Bedding</span>
+                                                                                                        </li>
+                                                                                                        <li>
+                                                                                                            <i class="fa fa-check-square" aria-hidden="true"></i>
+                                                                                                            <span>Cable TV</span>
+                                                                                                        </li>
+                                                                                                        <li>
+                                                                                                            <i class="fa fa-check-square" aria-hidden="true"></i>
+                                                                                                            <span>Parking</span>
+                                                                                                        </li>
+                                                                                                        <li>
+                                                                                                            <i class="fa fa-check-square" aria-hidden="true"></i>
+                                                                                                            <span>Pool</span>
+                                                                                                        </li>
+                                                                                                        <li>
+                                                                                                            <i class="fa fa-check-square" aria-hidden="true"></i>
+                                                                                                            <span>Fridge</span>
+                                                                                                        </li>
+                                                                                                    </ul> -->
                 </div>
 
             </section>
@@ -206,42 +198,42 @@
 @endsection
 
 @section('scripts')
-<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
-<script>
-    var map = L.map('map').setView([{{ $housing->latitude }},{{ $housing->longitude }}], 13);
+    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
+    <script>
+        var map = L.map('map').setView([{{ $housing->latitude }}, {{ $housing->longitude }}], 13);
 
-    // OpenStreetMap katmanını haritaya ekleyin
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+        // OpenStreetMap katmanını haritaya ekleyin
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
 
-    // Yakınındaki alışveriş yerlerini Overpass API kullanarak getirin
-    var overpassUrl = 'https://overpass-api.de/api/interpreter';
-    var query = `[out:json];
+        // Yakınındaki alışveriş yerlerini Overpass API kullanarak getirin
+        var overpassUrl = 'https://overpass-api.de/api/interpreter';
+        var query = `[out:json];
     (
         node["amenity"="clinic"](around:12000,{{ $housing->latitude }},{{ $housing->longitude }});
         way["amenity"="clinic"](around:12000,{{ $housing->latitude }},{{ $housing->longitude }});
         relation["amenity"="clinic"](around:12000,{{ $housing->latitude }},{{ $housing->longitude }});
     );
     out center;`;
-    var url = `${overpassUrl}?data=${encodeURIComponent(query)}`;
+        var url = `${overpassUrl}?data=${encodeURIComponent(query)}`;
 
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            // Alışveriş yerlerini işaretle
-            console.log(data.elements);
-            data.elements.forEach(element => {
-                var lat = element.lat;
-                var lon = element.lon;
-                var marker = L.marker([lat, lon]).addTo(map);
-                marker.bindPopup(element.tags.name || 'Bilinmeyen Mağaza');
-            });
-        })
-        .catch(error => console.error('Hata:', error));
-</script>
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                // Alışveriş yerlerini işaretle
+                console.log(data.elements);
+                data.elements.forEach(element => {
+                    var lat = element.lat;
+                    var lon = element.lon;
+                    var marker = L.marker([lat, lon]).addTo(map);
+                    marker.bindPopup(element.tags.name || 'Bilinmeyen Mağaza');
+                });
+            })
+            .catch(error => console.error('Hata:', error));
+    </script>
 @endsection
 
 @section('styles')
-<link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
 @endsection
