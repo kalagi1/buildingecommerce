60b5645f531f80539d011763ae3dcf6af446d8f2
kaanylmz34 <kaanylmz34@icloud.com>
2023-10-10 19:10:00 +0300

    10/10/2023 3

--- resources/views/institutional/home/verification.blade.php
+++ resources/views/institutional/home/verification.blade.php
@@ -7,15 +7,19 @@
             @csrf
             <div class="p-5 bg-white border rounded-3 shadow-lg" style="width: 75%; overflow-y: scroll; max-height: 520px; margin: 0 auto;">
                 <div class="form-group">
-                    <div class="alert alert-success text-white">
+                    <div class="alert alert-outline-success">
+                        @if (!is_null(auth()->user()->tax_document) || !is_null(auth()->user()->record_document))
+                        Belgeleriniz gönderildi ve şu anda incelemede. Dilerseniz gönderdiğiniz belgeleri güncelleyebilirsiniz.
+                        @else
                         Sistemi kullanmaya devam edebilmeniz için hesabınızı doğrulamamız gerekiyor.<br/>
                         Lütfen aşağıda istenen belgeleri bize gönderin.
+                        @endif
                     </div>
                 </div>
                 <div class="form-group">
                     @if (auth()->user()->tax_document_approve)
-                    <div class="alert alert-info mt-3 text-white">
-                        <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path fill="#FFF" d="M7 5c-1.103 0-2 .897-2 2v10c0 1.103.897 2 2 2h10c1.103 0 2-.897 2-2V7c0-1.103-.897-2-2-2H7zm0 12V7h10l.002 10H7z"/><path fill="#FFF" d="M10.996 12.556 9.7 11.285l-1.4 1.43 2.704 2.647 4.699-4.651-1.406-1.422z"/></svg>
+                    <div class="alert alert-outline-info mt-3">
+                        <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path style="fill: var(--phoenix-info-500);" d="M7 5c-1.103 0-2 .897-2 2v10c0 1.103.897 2 2 2h10c1.103 0 2-.897 2-2V7c0-1.103-.897-2-2-2H7zm0 12V7h10l.002 10H7z"/><path style="fill: var(--phoenix-info-500);" d="M10.996 12.556 9.7 11.285l-1.4 1.43 2.704 2.647 4.699-4.651-1.406-1.422z"/></svg>
                         Vergi levhanız onaylandı.
                     </div>
                     @endif
@@ -24,8 +28,8 @@
                 </div>
                 <div class="form-group">
                     @if (auth()->user()->record_document_approve)
-                    <div class="alert alert-info mt-3 text-white">
-                        <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path fill="#FFF" d="M7 5c-1.103 0-2 .897-2 2v10c0 1.103.897 2 2 2h10c1.103 0 2-.897 2-2V7c0-1.103-.897-2-2-2H7zm0 12V7h10l.002 10H7z"/><path fill="#FFF" d="M10.996 12.556 9.7 11.285l-1.4 1.43 2.704 2.647 4.699-4.651-1.406-1.422z"/></svg>
+                    <div class="alert alert-outline-info mt-3">
+                        <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path style="fill: var(--phoenix-info-500);" d="M7 5c-1.103 0-2 .897-2 2v10c0 1.103.897 2 2 2h10c1.103 0 2-.897 2-2V7c0-1.103-.897-2-2-2H7zm0 12V7h10l.002 10H7z"/><path style="fill: var(--phoenix-info-500);" d="M10.996 12.556 9.7 11.285l-1.4 1.43 2.704 2.647 4.699-4.651-1.406-1.422z"/></svg>
                         Sicil belgeniz onaylandı.
                     </div>
                     @endif
@@ -34,11 +38,15 @@
                 </div>
                 @if ($user->corporate_account_note)
                 <div class="form-group">
-                    <div class="alert alert-warning text-white">{{$user->corporate_account_note}}</div>
+                    <div class="alert alert-outline-warning">{{$user->corporate_account_note}}</div>
                 </div>
                 @endif
                 <div class="form-group">
+                    @if (!is_null(auth()->user()->tax_document) || !is_null(auth()->user()->record_document))
+                    <button type="submit" class="btn btn-primary btn-lg">GÜNCELLE</button>
+                    @else
                     <button type="submit" class="btn btn-primary btn-lg">ONAYA GÖNDER</button>
+                    @endif
                 </div>
             </div>
         </form>
