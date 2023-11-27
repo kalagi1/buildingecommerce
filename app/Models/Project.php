ea5c3b9ca192605a8c74327f869cdf5b64dbef82
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2023-11-27 19:52:26 +0300

    update

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -17,11 +17,7 @@ class Project extends Model
         return $this->hasMany(ProjectHousings::class);
     }
 
-    public function blocks()
-    {
-        return $this->hasMany(Block::class);
-    }
-
+    
     public function images()
     {
         return $this->hasMany(ProjectImage::class, "project_id", "id");
