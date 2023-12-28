e2d553c187e3676ddd7abb4dd62150a7a8c8f104
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2023-12-28 17:09:40 +0300

    Changed the stroke color of the SVG icon from #dadada to #000000. (generated by blackbox.ai )

--- resources/views/client/housings/detail.blade.php
+++ resources/views/client/housings/detail.blade.php
@@ -542,7 +542,7 @@
                                                     <rect fill="none" height="50" width="50" />
                                                     <polygon fill="none"
                                                         points="25,3.553 30.695,18.321 46.5,19.173   34.214,29.152 38.287,44.447 25,35.848 11.712,44.447 15.786,29.152 3.5,19.173 19.305,18.321 "
-                                                        stroke="#dadada" stroke-miterlimit="10"
+                                                        stroke="#000000" stroke-miterlimit="10"
                                                         stroke-width="2" />
                                                 </svg>
                                                 <svg class="rating" enable-background="new 0 0 50 50"
