dd797f333c186769d9b936973d8d6bfd9527b4ed
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-01-09 15:25:18 +0300

    Update housing detail page to display parent title if it exists, otherwise display parent title (generated by blackbox.ai )

--- resources/views/client/housings/detail.blade.php
+++ resources/views/client/housings/detail.blade.php
@@ -639,7 +639,7 @@
                                                                     Günlük Kiralık
                                                                 @endif
                                                             @endif
-                                                            {{ $parent->title }}
+                                                            {{ $parent->title ?? $parent->title }}
                                                         @endif
                                                     </span>
                                                 </td>
