071db22d9ad39bce6f12985322458272aa315ae0
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-01-10 13:48:35 +0300

    Update footer table to remove the "Taksit Sayısı" column and add it after the "Aylık Ödenecek Tutar" column. (generated by blackbox.ai )

--- resources/views/client/layouts/partials/footer.blade.php
+++ resources/views/client/layouts/partials/footer.blade.php
@@ -424,9 +424,9 @@
                     <tr>
                         <th>Ödeme Türü</th>
                         <th>Fiyat</th>
-                        <th>Taksit Sayısı</th>
                         <th>Peşin Ödenecek Tutar</th>
                         <th>Aylık Ödenecek Tutar</th>
+                        <th>Taksit Sayısı</th>
                     </tr>
                 </thead>
                 <tbody>
