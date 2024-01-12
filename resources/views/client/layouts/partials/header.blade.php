deb5d68bac84771758472efadc8eaac4f8fcba16
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-01-12 15:43:12 +0300

    Changed the header link to open in a new tab. (generated by blackbox.ai )

--- resources/views/client/layouts/partials/header.blade.php
+++ resources/views/client/layouts/partials/header.blade.php
@@ -400,7 +400,7 @@
                                 </li>
                             @endforeach
                             <li>
-                                <a href="https://emlakkulup.emlaksepette.com/">
+                                <a href="https://emlakkulup.emlaksepette.com/" target="_blank">
                                  <b style="font-weight:700 !important;display:flex">
                                     <img style="height: 21px;" class="lazy entered loading" src="{{url('yeniler_2.svg')}}" alt="Yeniler" data-ll-status="loading">
                                     EMLAK KULÜP</b>
