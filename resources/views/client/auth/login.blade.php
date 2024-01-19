7157986544e4b4556f21681ac0f34e28fa395958
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-01-19 11:08:19 +0300

    Remove jQuery dependency (generated by blackbox.ai )

--- resources/views/client/auth/login.blade.php
+++ resources/views/client/auth/login.blade.php
@@ -691,7 +691,6 @@
             $(`.sub-plan-tab.${data[value]}`).removeClass('d-none');
         });
     </script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <script>
