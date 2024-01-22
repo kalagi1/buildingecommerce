de33b509f86f55fdccfa60a52a3bb9d257e092eb
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-01-22 15:51:59 +0300

    update

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Admin\BankAccountController;
 use App\Http\Controllers\Admin\ChangePasswordController;
 use App\Http\Controllers\Admin\DopingOrderController;
 use App\Http\Controllers\Admin\EmailTemplateController;
+use App\Http\Controllers\Admin\EstateClubController as AdminEstateClubController;
 use App\Http\Controllers\Admin\FooterLinkController;
 use App\Http\Controllers\Admin\HomeController as AdminHomeController;
 use App\Http\Controllers\Admin\HousingController;
@@ -207,6 +208,13 @@ Route::post('/institutional/login', [LoginController::class, 'login'])->name('in
 Route::post('/mark-notification-as-read/{id}', [InfoController::class, "markAsRead"]);
 
 Route::group(['prefix' => 'admin', "as" => "admin.", 'middleware' => ['admin']], function () {
+    Route::get('/estate_club_users', [AdminEstateClubController::class,"index"])->name('estate.club.users');
+    Route::get('/coupons', [AdminEstateClubController::class,"coupons"])->name('estate.coupons');
+    Route::get('/create_coupon/{user_id}', [AdminEstateClubController::class,"createCoupon"])->name('estate.create.coupon');
+    Route::post('/create_coupon/{user_id}', [AdminEstateClubController::class,"createCouponStore"])->name('estate.create.coupon.store');
+    Route::get('/edit_coupon/{coupon_id}', [AdminEstateClubController::class,"editCoupon"])->name('estate.edit.coupon');
+    Route::get('/coupon_destroy/{user_id}', [AdminEstateClubController::class,"destroy"])->name('estate.coupon.destroy');
+    
     Route::get('/real_estates',[AdminRealEstateController::class,"index"])->name('real.estates');
     Route::get('/real_estate/{id}',[AdminRealEstateController::class,"detail"])->name('real.estate.detail');
     Route::put('/users/{user}/block', [UserController::class, 'blockUser'])->name('users.block');
