4bb91dc5559cf1450c2cdcad32177b1c9ffdce16
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-01-22 15:23:41 +0300

    (generated by blackbox.ai )

--- resources/views/client/institutional/detail.blade.php
+++ resources/views/client/institutional/detail.blade.php
@@ -40,8 +40,9 @@
                                         fill: #FFFFFF;
                                     }
                                 </style>
-                                <svg id="Layer_1" style="enable-background:new 0 0 120 120;" version="1.1" width="24px"
-                                    height="24px" viewBox="0 0 120 120" xml:space="preserve"
+                                @if ($institutional->corporate_account_status )
+                                <svg id="Layer_1" style="enable-background:new 0 0 120 120;" version="1.1"
+                                    width="24px" height="24px" viewBox="0 0 120 120" xml:space="preserve"
                                     xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                                     <g>
                                         <path class="st0"
@@ -51,7 +52,8 @@
                                                 d="M43.4,93.5l-2.3-4.6c-0.8-1.5-2.1-2.7-3.7-3.2l-4.8-1.6c-2.9-1-4.7-3.8-4.4-6.8l0.5-5.1c0.2-1.7-0.3-3.4-1.4-4.7l-3.2-4    c-1.9-2.4-1.9-5.7,0-8.1l3.2-4c1.1-1.3,1.6-3,1.4-4.7l-0.5-5.1c-0.3-3,1.5-5.8,4.4-6.8l4.8-1.6c1.6-0.5,2.9-1.7,3.7-3.2l2.3-4.6    c0.8-1.6,2.2-2.7,3.7-3.2c-2.7-0.4-5.4,1-6.6,3.5l-2.3,4.6c-0.8,1.5-2.1,2.7-3.7,3.2l-4.8,1.6c-2.9,1-4.7,3.8-4.4,6.8l0.5,5.1    c0.2,1.7-0.3,3.4-1.4,4.7l-3.2,4c-1.9,2.4-1.9,5.7,0,8.1l3.2,4c1.1,1.3,1.6,3,1.4,4.7l-0.5,5.1c-0.3,3,1.5,5.8,4.4,6.8l4.8,1.6    c1.6,0.5,2.9,1.7,3.7,3.2l2.3,4.6c1.4,2.7,4.4,4.1,7.4,3.4l0.6-0.1C46.3,96.7,44.4,95.5,43.4,93.5z" />
                                             <path
                                                 d="M60.6,22.5l4.4-2.6c0.4-0.2,0.8-0.4,1.2-0.5c-1.4-0.2-2.9,0.1-4.1,0.8l-4.4,2.6c-0.4,0.2-0.8,0.4-1.2,0.5    C57.9,23.5,59.3,23.3,60.6,22.5z" />
-                                            <path d="M81,92c-0.5,0-1,0.1-1.4,0.2l3.6-0.2c0.5,0,0.9-0.1,1.4-0.2L81,92z" />
+                                            <path
+                                                d="M81,92c-0.5,0-1,0.1-1.4,0.2l3.6-0.2c0.5,0,0.9-0.1,1.4-0.2L81,92z" />
                                             <path
                                                 d="M65,98.9l-4.4-2.6c-1.5-0.9-3.2-1.1-4.9-0.7l-0.6,0.1c0.9,0.1,1.7,0.4,2.5,0.8l4.4,2.6c1.7,1,3.6,1.1,5.4,0.5    C66.6,99.6,65.8,99.4,65,98.9z" />
                                         </g>
@@ -60,6 +62,7 @@
                                             d="M53.5,75.3c-1.4,0-2.8-0.6-3.8-1.7L37.2,59.3c-1.8-2.1-1.6-5.2,0.4-7.1c2.1-1.8,5.2-1.6,7.1,0.4l9.4,10.7   l21.9-17.6c2.1-1.7,5.3-1.4,7,0.8c1.7,2.2,1.4,5.3-0.8,7L56.6,74.2C55.7,74.9,54.6,75.3,53.5,75.3z" />
                                     </g>
                                 </svg>
+                            @endif
                             </a>
                         </p>
                         <p class="brand-name"><i class="fa fa-angle-right"></i> </p>
@@ -122,6 +125,18 @@
                             </span>
                         </div>
                     </div>
+                    <div class="seller-info-container__wrapper"><img
+                        src="{{url('images/artboard-2.svg')}}" alt="icon"
+                        class="seller-info-container__wrapper__img">
+                    <div class="seller-info-container__wrapper__text-container"><span
+                            class="seller-info-container__wrapper__text-container__title">İletişim </span><span
+                            class="seller-info-container__wrapper__text-container__value">
+                      <span>     Telefon :  {{ $institutional->phone ? $institutional->phone : "Belirtilmedi."}} <br>
+                        E-Mail :  {{ $institutional->email ? $institutional->email : "Belirtilmedi"}}</span>
+
+                        </span>
+                    </div>
+                </div>
                     <div class="seller-info-container__wrapper"><img
                         src="{{url('images/artboard.svg')}}" alt="icon"
                             class="seller-info-container__wrapper__img">
