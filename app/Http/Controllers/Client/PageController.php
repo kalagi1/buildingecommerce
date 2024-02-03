73b78db49f0b8f859183279614b3874f7675980e
kalagi1 <islamoglu.abd@gmail.com>
2024-02-03 10:20:33 +0300

    apodev

--- app/Http/Controllers/Client/PageController.php
+++ app/Http/Controllers/Client/PageController.php
@@ -11,7 +11,6 @@ class PageController extends Controller
     public function index($slug)
     {
         $pageInfo = Page::where('slug', $slug)->first();
-    
         // Sayfa bulunamazsa 404 hatası döndür
         if (!$pageInfo) {
             abort(404);
