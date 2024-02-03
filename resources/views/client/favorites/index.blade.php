3e28a474eed257840fd4e0be63e43a5aefd52bbf
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-02-03 11:18:48 +0300

    Change the column used to check if a project has been sold in the cart_orders table from 'price[]' to 'squaremeters[]'. (generated by blackbox.ai )

--- resources/views/client/favorites/index.blade.php
+++ resources/views/client/favorites/index.blade.php
@@ -48,7 +48,7 @@
                                                 App\Models\Offer::where('type', 'project')->where('project_id', $item->project->id)->where('project_housings', 'LIKE', "%\"{$item->project->housing_type_id}\"%")->where('start_date', '<=', date('Y-m-d H:i:s'))->where('end_date', '>=', date('Y-m-d H:i:s'))->first()->discount_amount ?? 0
                                         )
 
-                                    @php($sold = DB::select('SELECT * FROM cart_orders WHERE JSON_UNQUOTE(JSON_EXTRACT(cart, "$.type")) = "project" AND JSON_UNQUOTE(JSON_EXTRACT(cart, "$.item.housing")) = ? AND JSON_UNQUOTE(JSON_EXTRACT(cart, "$.item.id")) = ? LIMIT 1', [getHouse($item->project, 'price[]', $item->housing_id)->room_order, $item->project->id]))
+                                    @php($sold = DB::select('SELECT * FROM cart_orders WHERE JSON_UNQUOTE(JSON_EXTRACT(cart, "$.type")) = "project" AND JSON_UNQUOTE(JSON_EXTRACT(cart, "$.item.housing")) = ? AND JSON_UNQUOTE(JSON_EXTRACT(cart, "$.item.id")) = ? LIMIT 1', [getHouse($item->project, 'squaremeters[]', $item->housing_id)->room_order, $item->project->id]))
 
                                     <tr>
                                         <td class="image myelist">
