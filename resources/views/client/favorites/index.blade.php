ed5a1646a2db2ed1a2a7e11202488c3cfd5449f1
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-02-03 15:26:10 +0300

    Update getHouse function to log housingId (generated by blackbox.ai )

--- resources/views/client/favorites/index.blade.php
+++ resources/views/client/favorites/index.blade.php
@@ -46,6 +46,7 @@
 
                                         $project = $item->project;
                                         $housingId = $item->housing_id;
+                                        dd($housingId);
                                         $projectDiscountAmount =
                                             App\Models\Offer::where('type', 'project')
                                                 ->where('project_id', $project->id)
@@ -54,7 +55,7 @@
                                                 ->where('end_date', '>=', now())
                                                 ->first()->discount_amount ?? 0;
 
-                                                dd($housingId);
+                                             
 
                                         $soldQuery = 'SELECT * FROM cart_orders WHERE JSON_UNQUOTE(JSON_EXTRACT(cart, "$.type")) = "project" AND JSON_UNQUOTE(JSON_EXTRACT(cart, "$.item.housing")) = ? AND JSON_UNQUOTE(JSON_EXTRACT(cart, "$.item.id")) = ? LIMIT 1';
                                         $sold = DB::select($soldQuery, [getHouse($project, 'price[]', $housingId)->room_order, $project->id]);
