6f8e5d86b0fb9d3caa81179b34caa938b5293c01
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-02-03 16:40:52 +0300

    Update project listing blade file. (generated by blackbox.ai )

--- resources/views/admin/projects/detail.blade.php
+++ resources/views/admin/projects/detail.blade.php
@@ -150,7 +150,7 @@
 
                                         <tr>
                                             <td>
-                                                <strong class="autoWidthTr">Telefon:</strong>
+                                                <strong class="autoWidthTr">İletişim No:</strong>
                                                 <span class="det" style="color: black;">{!! $project->user->phone ? $project->user->phone : 'Belirtilmedi' !!}</span>
                                             </td>
                                         </tr>
