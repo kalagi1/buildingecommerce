ecf1e691fac732447cdcbf97b79d0367837a205d
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-02-10 11:25:57 +0300

    Update meta tags if pageInfo is set (generated by blackbox.ai )

--- resources/views/client/layouts/partials/header.blade.php
+++ resources/views/client/layouts/partials/header.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-    @if (isset($pageInfo) && count($pageInfo) > 0)
+    @if (isset($pageInfo) )
         <meta name="keywords" content="{{ $pageInfo->meta_keywords }}">
         <meta name="description" content="{{ $pageInfo->meta_description }}">
         <meta name="author" content="{{ $pageInfo->meta_author }}">
