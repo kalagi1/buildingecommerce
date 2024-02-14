b5e01d3c8449194f75a9ae957b4575803dd3b908
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-02-14 16:13:29 +0300

    Changed the dd() statement to dd($blockHousingCount); (generated by blackbox.ai )

--- resources/views/client/projects/index.blade.php
+++ resources/views/client/projects/index.blade.php
@@ -674,7 +674,7 @@
                                                                 }
                                                                 $pageCount = $currentBlockHouseCount / 10;
                                                                 $count = $pageCount > 1 ? 10 : $blockHousingCount;
-                                                                dd($blockHousingCount)
+                                                                dd($blockHousingCount);
                                                             @endphp
 
                                                             <div class="mobile-hidden">
