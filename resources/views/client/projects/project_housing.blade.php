bf0363734f91062b7f5c7f1ae2da0d845f9f5f38
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-02-21 18:49:23 +0300

    Update sold button styling. (generated by blackbox.ai )

--- resources/views/client/projects/project_housing.blade.php
+++ resources/views/client/projects/project_housing.blade.php
@@ -408,7 +408,7 @@
                                     @else
                                         @if ($soldStatus && $soldStatus != '2')
                                             <button class="btn second-btn  soldBtn"
-                                                @if ($soldStatus == '0') style="background: orange !important;color:White" @else style="background: #EA2B2E !important;color:White;height: auto !important" @endif>
+                                                @if ($soldStatus == '0') style="background: orange !important;color:White" @else style="background: #EA2B2E !important;color:White" @endif>
                                                 @if ($soldStatus == '0')
                                                     <span class="text">Rezerve Edildi</span>
                                                 @else
