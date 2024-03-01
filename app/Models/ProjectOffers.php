851fe4f1ee7ffe7e68c9564eb1f7926da4e11703
Your Name <you@example.com>
2024-03-01 10:48:09 +0300

    teklif ver table update

--- app/Models/ProjectOffers.php
+++ app/Models/ProjectOffers.php
@@ -8,10 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class ProjectOffers extends Model
 {
     use HasFactory;
+    public function project(){
+        return $this->hasOne(Project::class,"id","project_id");
+    }
+
+    public function user(){
+        return $this->hasOne(User::class,"id","user_id");
+    }
     protected $fillable = [
         'user_id',
         'store_id',
-        'project_id',
+        'project_name',
         'room_id',
         'email',
         'offer_price_range',
