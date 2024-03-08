d6daf319894e852f3b54ab56e738c14701326e88
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-03-08 18:25:42 +0300

    Fix typo in if statement.

--- resources/views/admin/projects/housings2.blade.php
+++ resources/views/admin/projects/housings2.blade.php
@@ -259,7 +259,7 @@
                                                     @endif
                                                 </td>
                                                 <td class="sold">
-                                                    @if (isset($share_sale) && !empty($share_sale) && $share_sale ! = "[]")
+                                                    @if (isset($share_sale) && !empty($share_sale) && $share_sale != "[]")
                                                     <span class=" d-block mb-2">
                                                             @if (isset($sumCartOrderQt[$i + 1]) && isset($sumCartOrderQt[$i + 1]['qt_total']))
                                                                 {{ $sumCartOrderQt[$i + 1]['qt_total'] }}
@@ -608,7 +608,7 @@
                                             @endif
                                         </td>
                                         <td class="sold">
-                                            @if (isset($share_sale) && !empty($share_sale) && $share_sale ! = "[]")
+                                            @if (isset($share_sale) && !empty($share_sale) && $share_sale != "[]")
                                                 <span class=" d-block mb-2">
                                                     @if (isset($sumCartOrderQt[$i + 1]) && isset($sumCartOrderQt[$i + 1]['qt_total']))
                                                         {{ $sumCartOrderQt[$i + 1]['qt_total'] }}
