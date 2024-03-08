20cba09049c1f8fbdbbe4f8f9a5bb47066e59514
Your Name <you@example.com>
2024-03-08 11:46:59 +0300

    komşumu gör update  08/03 11:45

--- app/Http/Controllers/Institutional/ProjectController.php
+++ app/Http/Controllers/Institutional/ProjectController.php
@@ -77,6 +77,7 @@ class ProjectController extends Controller
 
     public function housings($project_id)
     {
+        print_r('yunusSs');die;
         $menu = Menu::getMenuItems();
         $project = Project::where('id', $project_id)->with("brand", "blocks", "roomInfo", "housingType", "county", "city", 'user.projects.housings', 'user.brands', 'user.housings', 'images')->firstOrFail();
         $project->roomInfo = $project->roomInfo;
