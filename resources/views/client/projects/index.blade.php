1d2c88dca1d7f8e1aae954a0c86bef806b54db5f
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-03-12 18:39:06 +0300

    Changed the active slide number in the carousel from 0 to 1, and changed the data-slide-number attributes of the carousel items accordingly. Also added a new active item to the carousel indicator navigation with the first image.

--- resources/views/client/projects/index.blade.php
+++ resources/views/client/projects/index.blade.php
@@ -206,7 +206,7 @@
                                 <a href="javascript:void()" style="color:White;">{{ $project->project_title }}</a>
                             </div>
                             <div class="carousel-inner">
-                                <div class="item carousel-item active" data-slide-number="0">
+                                <div class="item carousel-item active" data-slide-number="1">
                                     <a href="{{ URL::to('/') . '/project_housing_images/' . $projectHousingsList[1]['image[]'] }}"
                                         data-lightbox="image-gallery">
                                         <img src="{{ URL::to('/') . '/project_housing_images/' . $projectHousingsList[1]['image[]'] }}"
@@ -216,7 +216,7 @@
 
                                 {{-- Diğer Görseller --}}
                                 @foreach ($project->images as $key => $housingImage)
-                                    <div class="item carousel-item" data-slide-number="{{ $key + 1 }}">
+                                    <div class="item carousel-item" data-slide-number="{{ $key  }}">
                                         <a href="{{ URL::to('/') . '/' . str_replace('public/', 'storage/', $housingImage->image) }}"
                                             data-lightbox="image-gallery">
                                             <img src="{{ URL::to('/') . '/' . str_replace('public/', 'storage/', $housingImage->image) }}"
@@ -235,10 +235,17 @@
 
                             {{-- Küçük Resim Navigasyonu --}}
                             <div class="listingDetailsSliderNav mt-3">
+                                <div class="item active" style="margin: 10px; cursor: pointer">
+                                    <a id="carousel-selector-1" data-slide-to="1"
+                                        data-target="#listingDetailsSlider">
+                                        <img src="{{ URL::to('/') . '/project_housing_images/' . $projectHousingsList[1]['image[]'] }}"
+                                            class="img-fluid carousel-indicator-image" alt="listing-small">
+                                    </a>
+                                </div>
                                 @foreach ($project->images as $key => $housingImage)
                                     <div class="item" style="margin: 10px; cursor: pointer">
-                                        <a id="carousel-selector-{{ $key + 1 }}"
-                                            data-slide-to="{{ $key + 1 }}" data-target="#listingDetailsSlider">
+                                        <a id="carousel-selector-{{ $key  }}"
+                                            data-slide-to="{{ $key  }}" data-target="#listingDetailsSlider">
                                             <img src="{{ URL::to('/') . '/' . str_replace('public/', 'storage/', $housingImage->image) }}"
                                                 class="img-fluid carousel-indicator-image" alt="listing-small">
                                         </a>
