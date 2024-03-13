ca649662c2815c3777399bb88424b604248c3ac7
Your Name <you@example.com>
2024-03-13 14:54:04 +0300

    komşumu gör düzenleme2

--- resources/views/admin/projects/housings2.blade.php
+++ resources/views/admin/projects/housings2.blade.php
@@ -347,8 +347,8 @@
                                                     <a type="button" class="badge badge-phoenix badge-phoenix-warning" data-bs-toggle="modal" data-bs-target="#exampleModal{{$i+1}}">
                                                         Komşumu Gör
                                                       </a><br>
-                                                    <a href="{{ route('institutional.projects.edit.housing', ['project_id' => $project->id, 'room_order' => $i + 1]) }}"
-                                                        class="badge badge-phoenix badge-phoenix-primary">İlan Düzenle</a><br>
+                                                    {{-- <a href="{{ route('institutional.projects.edit.housing', ['project_id' => $project->id, 'room_order' => $i + 1]) }}"
+                                                        class="badge badge-phoenix badge-phoenix-primary">İlan Düzenle</a><br> --}}
                                                     <a href="{{ route('institutional.projects.delete.housing', ['project_id' => $project->id, 'room_order' => $i + 1]) }}"
                                                         class="badge badge-phoenix badge-phoenix-danger">Sil</a>
                                                 </td>
@@ -772,8 +772,8 @@
                                                             Komşumu Gör
                                                           </a><br>
                                                           
-                                                        <a href="{{ route('institutional.projects.edit.housing', ['project_id' => $project->id, 'room_order' => $i + 1]) }}"
-                                                            class="badge badge-phoenix badge-phoenix-primary">İlan Düzenle</a><br>
+                                                        {{-- <a href="{{ route('institutional.projects.edit.housing', ['project_id' => $project->id, 'room_order' => $i + 1]) }}"
+                                                            class="badge badge-phoenix badge-phoenix-primary">İlan Düzenle</a><br> --}}
                                                         <a href="{{ route('institutional.projects.delete.housing', ['project_id' => $project->id, 'room_order' => $i + 1]) }}"
                                                             class="badge badge-phoenix badge-phoenix-danger">Sil</a>
                                                     </td>
