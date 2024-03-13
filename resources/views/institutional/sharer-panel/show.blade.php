1aa125b0a1a6dfd34f07bab225677b275122f7c3
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-03-13 12:58:44 +0300

    update

--- resources/views/institutional/sharer-panel/show.blade.php
+++ resources/views/institutional/sharer-panel/show.blade.php
@@ -34,8 +34,6 @@
                                             $price = $item['project_values']['price[]'] - $item['discount_amount'];
                                             $discountedPrice = $price - ($price * $discountRate) / 100;
                                         }
-                                        $statusID = $item['project']->housingStatus->where('housing_type_id', '<>', 1)->first()->housing_type_id ?? 1;
-                                        $status = HousingStatus::find($statusID);
                                     @endphp
 
                                     <tr>
@@ -45,7 +43,6 @@
                                         </td>
 
                                         <td>
-                                                                                                                    {{-- ."-".$status->slug."-".$item['project']['step2_slug'."-".$item['project']->housingtype->slug    ]  --}}
                                             <a                                     
                                                 href="{{ $item['item_type'] == 1 ? route('project.housings.detail',['projectSlug'=>$item['project']['slug'],'projectID'=>$item['project']['id'], 'housingOrder'=>$item['room_order'] ]) :  route('housing.show', ['housingSlug' =>$item['housing']['step1_slug']. "-".$item['housing']['step2_slug']. "-" . $item['housing']['slug'], 'housingID' => [$item['housing']['id']] + 2000000]) }}">
                                                 <img src="{{ $item['item_type'] == 1 ? URL::to('/') . '/project_housing_images/' . $item['project_values']['image[]'] : URL::to('/') . '/housing_images/' . json_decode($item['housing']['housing_type_data'])->image }}"
