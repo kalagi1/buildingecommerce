6fcebe72108c48c422df6ec2870d43b2bca51154
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-03-14 22:37:25 +0300

    Update image path in the invoice index page

--- resources/views/admin/invoice/index.blade.php
+++ resources/views/admin/invoice/index.blade.php
@@ -77,8 +77,7 @@ function getHouse($project, $key, $roomOrder)
                                         </thead>
                                         <tbody>
                                             <tr>
-                                                <td class="tm_width_2"> <img src="{{ isset($cart['item']['image']) ?  $cart['item']['image'] : null}}"
-                                                    alt=""
+                                                <td class="tm_width_2"> <img src="{{ isset($cart['item']) ?  $cart['item']['image'] : null}}"
                                                     style="width:100px;height:100px;object-fit:cover"></td>
                                                 <td class="tm_width_5  tm_border_left" style="width: 100px">
                                                     <?php
