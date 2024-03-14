e9bcd70276f1eab1cf243a05ca6411d3ebbbbfb5
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-03-14 22:24:03 +0300

    Update phone number field name from mobile_phone to phone.

--- resources/views/components/project-item-mobile-card.blade.php
+++ resources/views/components/project-item-mobile-card.blade.php
@@ -312,12 +312,12 @@
                                             <li class="list-group-item" style="width:100%">İsim: {{ $sold->name }}
                                             </li>
                                             <li class="list-group-item" style="width:100%">Telefon:
-                                                {{  isset($sold->mobile_phone) ?? $sold->mobile_phone }}</li>
+                                                {{  isset($sold->phone) ?? $sold->phone }}</li>
 
                                         </ul>
                                     </div>
                                     <div class="modal-footer" style="justify-content: end !important">
-                                        <a href="tel:{{  isset($sold->mobile_phone) ?? $sold->mobile_phone }}"><button class="btn btn-success"
+                                        <a href="tel:{{  isset($sold->phone) ?? $sold->phone }}"><button class="btn btn-success"
                                                 style="width:100px">Ara</button></a>
 
                                         <button type="button" class="btn btn-danger" data-bs-dismiss="modal"
