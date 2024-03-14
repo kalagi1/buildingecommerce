f0d6cdf76d7519da4ba46cafaa5fb5eca13f94cf
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-03-14 23:05:37 +0300

    Update project address format in invoice index page

--- resources/views/admin/invoice/index.blade.php
+++ resources/views/admin/invoice/index.blade.php
@@ -83,9 +83,9 @@ function getHouse($project, $key, $roomOrder)
                                                 <td class="tm_width_5  tm_border_left" style="width: 100px">
                                                     <?php
                                                     if (isset($data['project']['project_title'])) {
-                                                        echo mb_convert_case($data['project']['project_title'], MB_CASE_TITLE, 'UTF-8') . ' ' . 'Projesinde ' . $cart['item']['housing'] . " No'lu " . $data['project']['step1_slug'] . '<br><span style="font-size: 11px;font-weight:700;color:black">' . $data['project']['city']['title'] . '/' . $data['project']['county']['ilce_title'] .  '</span>';
+                                                        echo mb_convert_case($data['project']['project_title'], MB_CASE_TITLE, 'UTF-8') . ' ' . 'Projesinde ' . $cart['item']['housing'] . " No'lu " . $data['project']['step1_slug'] . '<br><span style="font-size: 11px;font-weight:700;color:black">' . $data['project']['city']['title'] . '/' . $data['project']['county']['ilce_title'] . '/' . $data['project']['neighborhood']['mahalle_title'] . '</span>';
                                                     } else {
-                                                        echo $data['project']['title'] . '<br><span style="font-size: 11px;font-weight:700;color:black">' . $data['project']['city']['title'] . '/' . $data['project']['county']['title'] . '</span>';
+                                                        echo $data['project']['title'] . '<br><span style="font-size: 11px;font-weight:700;color:black">' . $data['project']['city']['title'] . '/' . $data['project']['county']['title'] . '/' . $data['project']['neighborhood']['mahalle_title'] . '</span>';
                                                     }
                                                     ?>
                                                 </td>
