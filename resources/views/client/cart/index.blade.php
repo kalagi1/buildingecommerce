f9e9fbc71ffd40d47afac6b5355feb16e886d08a
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-03-21 12:45:19 +0300

    update

--- resources/views/client/cart/index.blade.php
+++ resources/views/client/cart/index.blade.php
@@ -122,7 +122,14 @@
                                         } else {
                                             $projectOffer = App\Models\Offer::where('type', 'project')
                                                 ->where('project_id', $cart['item']['id'])
-                                                ->whereJsonContains('project_housings', $cart['item']['housing'])
+                                                ->where(function ($query) use ($i) {
+                                                                $query
+                                                                    ->orWhereJsonContains('project_housings', [$cart['item']['housing']])
+                                                                    ->orWhereJsonContains(
+                                                                        'project_housings',
+                                                                        (string) ($cart['item']['housing']),
+                                                                    ); // Handle as string as JSON might store values as strings
+                                                            })
                                                 ->where('start_date', '<=', now())
                                                 ->where('end_date', '>=', now())
                                                 ->first();
