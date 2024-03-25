d9367cfeece82a0d6bc73a5c08d4625e535907a8
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-03-25 09:35:50 +0300

    update

--- resources/views/client/institutional/project-detail.blade.php
+++ resources/views/client/institutional/project-detail.blade.php
@@ -2,34 +2,109 @@
 
 @section('content')
 
-<x-store-card :store="$institutional" />
+    <x-store-card :store="$institutional" />
 
 
 
-
-
-    <section class="properties-right featured portfolio blog pt-5 bg-white">
+    <section class="properties-right featured portfolio blog bg-white">
         <div class="container">
-                @if (count($institutional->projects))
-                    <div class="properties-right list featured portfolio blog pb-5 bg-white">
-                        <div class="container">
-                            <div class="row project-filter-reverse blog-pots finish-projects-web">
-                                @foreach ($institutional->projects as $project)
-                                    <x-project-card :project="$project" />
-                                @endforeach
+            @if (count($institutional->projects))
+                <div class="properties-right list featured portfolio blog pb-5 pt-3 bg-white">
+                    <div class="row">
+
+                        @php
+                            function addQueryParamToUrl($paramName, $paramValue)
+                            {
+                                $queryParams = request()->query();
+                                $queryParams[$paramName] = $paramValue;
+
+                                return request()->url() . '?' . http_build_query($queryParams);
+                            }
+
+                            $counts = [
+                                'devam-eden-projeler' => 0,
+                                'tamamlanan-projeler' => 0,
+                                'topraktan-projeler' => 0,
+                            ];
+                        @endphp
+                        @php
+                            $filter = request('filter', 'tumu');
+                        @endphp
+                        @foreach ($institutional->projects as $project)
+                            @php
+                                $statusID =
+                                    $project->housingStatus->where('housing_type_id', '<>', 1)->first()
+                                        ->housing_type_id ?? 1;
+
+                                $statusSlug = App\Models\HousingStatus::find($statusID)->slug;
+                            @endphp
+                            @if (in_array($statusSlug, ['devam-eden-projeler', 'tamamlanan-projeler', 'topraktan-projeler']))
+                                @php
+                                    $counts[$statusSlug]++;
+                                @endphp
+                            @endif
+                        @endforeach
+                        <div class="col-md-12">
+                            <div class="tabbed-content button-tabs mb-3">
+                                <ul class="tabs">
+                                    <li class="nav-item-block {{ $filter === 'tumu' ? 'active' : '' }}">
+                                        <a href="{{ addQueryParamToUrl('filter', 'tumu') }}">
+                                            <div class="tab-title">
+                                                <span>Tüm Projeler</span>
+                                            </div>
+                                        </a>
+                                    </li>
+                                    @foreach ($counts as $slug => $count)
+                                        <li class="nav-item-block {{ $filter === $slug ? 'active' : '' }}"
+                                            role="presentation">
+
+                                            <a href="{{ addQueryParamToUrl('filter', $slug) }}">
+                                                <div class="tab-title">
+                                                    <span>
+                                                        @if ($slug == 'devam-eden-projeler')
+                                                            Devam Eden Projeler
+                                                        @elseif($slug == 'tamamlanan-projeler')
+                                                            Tamamlanan Projeler
+                                                        @elseif($slug == 'topraktan-projeler')
+                                                            Topraktan Projeler
+                                                        @endif
+                                                        ({{ $count }})
+                                                    </span>
+                                                </div>
+                                            </a>
+                                        </li>
+                                    @endforeach
+                                </ul>
                             </div>
                         </div>
                     </div>
-                @else
-                <div class="container">
-                    <div class="row justify-content-center">
-                        <div class="col-md-8 text-center">
-                            <h2 class="mt-5 mb-3">Mağazaya ait proje kaydı bulunamadı.</h2>
-                            <p>Lütfen daha sonra tekrar deneyin veya başka bir arama yapın.</p>
-                        </div>
+                    <div class="row project-filter-reverse blog-pots finish-projects-web">
+                        @foreach ($institutional->projects as $project)
+                            @php
+                                $statusID =
+                                    $project->housingStatus->where('housing_type_id', '<>', 1)->first()
+                                        ->housing_type_id ?? 1;
+
+                                $statusSlug = App\Models\HousingStatus::find($statusID)->slug;
+                            @endphp
+                            @if (
+                                $filter === 'tumu' ||
+                                    ($filter === 'devam-eden-projeler' && $statusSlug === 'devam-eden-projeler') ||
+                                    ($filter === 'tamamlanan-projeler' && $statusSlug === 'tamamlanan-projeler') ||
+                                    ($filter === 'topraktan-projeler' && $statusSlug === 'topraktan-projeler'))
+                                <x-project-card :project="$project" />
+                            @endif
+                        @endforeach
                     </div>
                 </div>
-                @endif
+            @else
+                <div class="row justify-content-center">
+                    <div class="col-md-8 text-center">
+                        <h2 class="mt-5 mb-3">Mağazaya ait proje kaydı bulunamadı.</h2>
+                        <p>Lütfen daha sonra tekrar deneyin veya başka bir arama yapın.</p>
+                    </div>
+                </div>
+            @endif
 
         </div>
     </section>
