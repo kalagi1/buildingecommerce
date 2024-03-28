d673f62b6b02e87c4283ca0b32b3c934dfefe558
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-03-28 18:20:00 +0300

    "Updated routes for multiple mail feature in EmailTemplateController"
    This commit updates the routes for the multiple mail feature in the EmailTemplateController. The old routes with the prefix 'qR9zLp2xS6y/secured' have been changed to 'multiple-mail'. The updated routes are:
    
    * multiple-mail/create
    * multiple-mail/store
    * multiple-mail/get/users
    * multiple-mail/get/users/bireysel
    * multiple-mail/get/users/kurumsal
    
    The old routes with the prefix 'qR9zLp2xS6y/secured' have been removed.

--- routes/web.php
+++ routes/web.php
@@ -1132,6 +1132,6 @@ Route::get('/komsumu-sil/{id}', [ProjectController::class, 'komsumuSil'])->name(
 //Toplu Main Gönderimi
 Route::get('qR9zLp2xS6y/secured/multiple-mail/create', [EmailTemplateController::class, 'MultipleMail'])->name('admin.multiple_mail.create');
 Route::post('multiple_mail/store' , [EmailTemplateController::class, 'MultipleMailStore'])->name('admin.multiple_mail.store');
-Route::get('qR9zLp2xS6y/secured/multiple-mail/get/users',[EmailTemplateController::class,'MultipleMailGetUsers']);
-Route::get('qR9zLp2xS6y/secured/multiple-mail/get/users/bireysel',[EmailTemplateController::class,'MultipleMailGetUsersBireysel']);
-Route::get('qR9zLp2xS6y/secured/multiple-mail/get/users/kurumsal',[EmailTemplateController::class,'MultipleMailGetUsersKurumsal']);
+Route::get('multiple-mail/get/users',[EmailTemplateController::class,'MultipleMailGetUsers']);
+Route::get('multiple-mail/get/users/bireysel',[EmailTemplateController::class,'MultipleMailGetUsersBireysel']);
+Route::get('multiple-mail/get/users/kurumsal',[EmailTemplateController::class,'MultipleMailGetUsersKurumsal']);
