99733f54e35d8666e8d281c9186bc928d450546a
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-03-28 16:38:29 +0300

    Update sendContractReminder method in ContractController
    The sendContractReminder method in the ContractController has been updated to include input validation for the request object. The method now checks if the sms\_content and email\_content properties are set in the request object before proceeding to send the SMS and email reminders.
    
    Additionally, the method has been updated to use the phone number of the store if it exists, instead of a hardcoded phone number. The store's email address is also now checked for nullability before sending the email reminder.
    
    Finally, the commit includes minor code formatting changes, such as adding spaces around operators and removing unnecessary parentheses.

--- app/Http/Controllers/Client/ContractController.php
+++ app/Http/Controllers/Client/ContractController.php
@@ -16,25 +16,25 @@ class ContractController extends Controller {
         $this->smsService = $smsService;
     }
 
-    public function sendContractReminder(CartOrder $cartOrder, Request $request)
-    {
+    public function sendContractReminder( CartOrder $cartOrder, Request $request ) {
         $storeEmail = $cartOrder->store->email;
         $storePhone = $cartOrder->store->phone ? $cartOrder->store->phone : $cartOrder->store->mobile_phone;
-        $smsContent = $request->input('sms_content');
-        $emailContent = $request->input('email_content');
-    
-        if (isset($storeEmail)) {
-            Mail::to($storeEmail)->send(new CustomMail('Emlak Sepette | Sipariş için Sözleşme Hatırlatma', $emailContent));
+        $smsContent = $request->input( 'sms_content' );
+        $emailContent = $request->input( 'email_content' );
+
+        if ( isset( $storeEmail ) ) {
+            Mail::to( $storeEmail )->send( new CustomMail( 'Emlak Sepette | Sipariş için Sözleşme Hatırlatma', $emailContent ) );
         }
-    
-        if (isset($storePhone)) {
+
+        if ( isset( $storePhone ) ) {
             $source_addr = 'MaliyetinEv';
             $message = $smsContent;
-            $dest = '905075634137'; 
-           $campaign_id =  $this->smsService->sendSms($source_addr, $message, $dest);
+            $dest = $storePhone;
+
+            $campaign_id =  $this->smsService->sendSms( $source_addr, $message, $dest );
         }
-    
-        return redirect()->back()->with('success', 'Sözleşme hatırlatma başarıyla gönderildi.');
+
+        return redirect()->back()->with( 'success', 'Sözleşme hatırlatma başarıyla gönderildi.' );
     }
-    
+
 }
