67e2318e9a3a528270eb11bfb582e2dd4fdd1932
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-03-31 00:17:44 +0300

    "Update modal title in institutional swaps index view to include application number"
    This commit updates the modal title in the institutional swaps index view to include the number of the application being displayed in the modal. This is done by adding the loop index to the modal title.

--- resources/views/institutional/swaps/index.blade.php
+++ resources/views/institutional/swaps/index.blade.php
@@ -40,13 +40,13 @@
         </div>
 
         <!-- Modal -->
-        @foreach ($apps as $app)
+        @foreach ($apps as $key => $app)
             <div class="modal fade" id="detailModal{{ $app->id }}" tabindex="-1" role="dialog"
                 aria-labelledby="detailModalLabel{{ $app->id }}" aria-hidden="true">
                 <div class="modal-dialog modal-lg" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="detailModalLabel{{ $app->id }}">Başvuru Detayları</h5>
+                            <h5 class="modal-title" id="detailModalLabel{{ $app->id }}"> {{$key + 1}}. Başvuru Detayları</h5>
 
                         </div>
                         <div class="modal-body">
