edd63596c18a715700a6b5b349741604c49c4817
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-04-05 10:54:23 +0300

    Update text on payment button in project-item-card component
    This commit updates the text on the payment button in the `project-item-card` component from "Ödeme Detayı" to "Ödeme Detayı asd".

--- resources/views/components/project-item-card.blade.php
+++ resources/views/components/project-item-card.blade.php
@@ -389,7 +389,7 @@
                                             data-sold="{{ ($sold_check &&  $share_sale == "[]") || (isset($sumCartOrderQt[$keyIndex]) && $sumCartOrderQt[$keyIndex]['qt_total'] == $number_of_share) || ( !$sold_check && isset($projectHousingsList[$keyIndex]['off_sale']) && $projectHousingsList[$keyIndex]['off_sale'] != '[]') ? '1' : '0' }}"
                                             order="{{ $keyIndex }}" data-block="{{ $blockName }}"
                                             data-payment-order="{{ $projectOrder }}">
-                                            Ödeme Detayı
+                                            Ödeme Detayı asd
                                         </button>
 
                                     @endif
