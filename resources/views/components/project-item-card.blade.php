32e78492aa804e8dc370a4c599ce287fc20553f4
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-04-05 11:59:24 +0300

    Change 'printr_' to 'print_r' in project-item-card.blade.php
    This commit changes the function name 'printr_' to the correct function name 'print_r' in the project-item-card blade template file.

--- resources/views/components/project-item-card.blade.php
+++ resources/views/components/project-item-card.blade.php
@@ -307,7 +307,7 @@
                                                 ->first();
                                         }
                                     @endphp
-                                    {{printr_($neighborView)}}
+                                    {{print_r($neighborView)}}
 
                                     @if (!$neighborView && $sold->status == '1' && isset($sold->is_show_user) && $sold->is_show_user == 'on' && !$isUserSame)
                                         <span class="first-btn see-my-neighbor"
