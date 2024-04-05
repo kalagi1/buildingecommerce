5f9246306843146c1d066eb3390ee2a8e7147dc3
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-04-05 11:50:31 +0300

    Update project-item-card component: add ternary expression to button element
    The changes include:
    
    * Adding a new ternary expression to the data-payment-order attribute of the button element with the class "first-btn payment-plan-button".
    * The new expression checks if the project is not sold and if the project housings list at the current key index has an "off\_sale" value that is not an empty array.
    * The existing ternary expression is updated to include the new condition and checks if the sum of the cart order quantity at the current key index is equal to the number of shares.
    
    The purpose of these changes is to conditionally set the data-payment-order attribute based on the project's sale status and the sum of the cart order quantity.

--- resources/views/components/project-item-card.blade.php
+++ resources/views/components/project-item-card.blade.php
@@ -390,6 +390,7 @@
                                             order="{{ $keyIndex }}" data-block="{{ $blockName }}"
                                             data-payment-order="{{ $projectOrder }}">
                                             Ödeme Detayı
+                                            {{(!$sold && isset($projectHousingsList[$keyIndex]['off_sale']) && $projectHousingsList[$keyIndex]['off_sale'] != '[]') ? 1 : 0}}
                                             {{($sold && $sold->status != 2 && $share_sale_empty) || (isset($sumCartOrderQt[$keyIndex]) && $sumCartOrderQt[$keyIndex]['qt_total'] == $number_of_share) || (!$sold && isset($projectHousingsList[$keyIndex]['off_sale']) && $projectHousingsList[$keyIndex]['off_sale'] != '[]') ? 1 : 0}}
 
                                         </button>
