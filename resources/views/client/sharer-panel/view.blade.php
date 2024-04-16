407c4b12ab44263f962566917ea253001232185e
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-04-16 12:32:55 +0300

    Change height of home5-right-slider and recently-16 in sharer-panel view
    The height of the home5-right-slider and recently-16 have been changed from 550px to 35

--- resources/views/client/sharer-panel/view.blade.php
+++ resources/views/client/sharer-panel/view.blade.php
@@ -98,10 +98,10 @@
        
     @endif
 
-    <section class="recently portfolio bg-white homepage-5 emlak-kulup-slider recently2">
+    <section class="recently portfolio bg-white homepage-5 emlak-kulup-slider mt-5 recently2">
         <div class="container recently-slider">
             <div class="portfolio right-slider">
-                <div class="owl-carousel home5-right-slider" style="height: 550px">
+                <div class="owl-carousel home5-right-slider" style="height: 350px">
                     <a href="javascript:void()" class="recent-16" data-aos="fade-up" data-aos-delay="150">
                         <div class="recent-img16 sliderSize img-fluid img-center mobile-hidden"
                             style="background-image: url(images/emlak-kulup-banner.png)"></div>
@@ -613,7 +613,7 @@
         .home5-right-slider,
         .home5-right-slider .owl-stage-outer,
         .recent-16 {
-            height: 550px !important;
+            height: 350px !important;
         }
 
 
