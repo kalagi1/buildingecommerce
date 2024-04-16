b199c1110f9278aaedd960bc8dba8b9a785b7a2d
kalagi1 <islamoglu.abd@gmail.com>
2024-04-16 11:33:43 +0300

    apodev

--- app/Http/Controllers/Api/Client/AddressController.php
+++ app/Http/Controllers/Api/Client/AddressController.php
@@ -5,11 +5,12 @@ namespace App\Http\Controllers\Api\Client;
 use App\Http\Controllers\Controller;
 use App\Models\City;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AddressController extends Controller
 {
     public function cities(){
-        $cities = City::get();
+        $cities = City::select(DB::raw('title as label , id as value'))->get();
 
         return json_encode([
             "data" => $cities
