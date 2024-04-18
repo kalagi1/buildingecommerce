e07d87dee50628e58ac42665b8fe8b1f6596103b
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-04-18 13:47:11 +0300

    "Removed 'pt-1' class from payment option radio button labels in payment/index.blade.php"
    This commit message describes the change in the code diff, which removes the 'pt-1' class from two

--- resources/views/payment/index.blade.php
+++ resources/views/payment/index.blade.php
@@ -757,7 +757,7 @@
                                                                 <input class="form-check-input mt-0" type="radio"
                                                                     name="payment_option" id="option1" value="option1"
                                                                     checked>
-                                                                <label class="form-check-label pt-1 ml-2  mb-2 offset-md-1"
+                                                                <label class="form-check-label  ml-2  mb-2 offset-md-1"
                                                                     for="option1">
                                                                     Kredi Kartı ile Ödeme
                                                                 </label>
@@ -767,7 +767,7 @@
                                                             <div class="form-check">
                                                                 <input class="form-check-input mt-0" type="radio"
                                                                     name="payment_option" id="option2" value="option2">
-                                                                <label class="form-check-label pt-1 ml-2 mb-2 offset-md-1"
+                                                                <label class="form-check-label  ml-2 mb-2 offset-md-1"
                                                                     for="option2">
                                                                     EFT / Havale ile Ödeme
                                                                 </label>
