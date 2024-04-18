6e707db9aef3d0969d8e84dbd375b02863c31d13
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-04-18 17:10:53 +0300

    "Remove echo of deposit rate in payment index view"
    This commit removes the echo statement that displays the deposit rate in the payment index view. The variable $deposit\_rate is still being calculated and assigned, but it is no

--- resources/views/payment/index.blade.php
+++ resources/views/payment/index.blade.php
@@ -20,7 +20,6 @@
                 $project = \App\Models\Project::where('id', $cart['item']['id'])->first();
                 $saleType = $project->step2_slug;
                 $deposit_rate = $project->deposit_rate / 100;
-                echo $deposit_rate;
             }
     @endphp
     <section class="payment-method notfound">
