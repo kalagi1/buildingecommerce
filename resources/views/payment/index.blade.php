f222215542e32bcf56e1d2d68bf7c7d8b6415b47
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-04-18 13:48:05 +0300

    "Adjust payment form layout: move son kullanma ayı input left and add margin"
    This commit adjusts the layout of the payment form by moving the "Son Kullanma Ayı" input field to the left

--- resources/views/payment/index.blade.php
+++ resources/views/payment/index.blade.php
@@ -841,7 +841,7 @@
                                                             </div>
                                                         </div>
                                                         <div class="row mrg-bot-20" style="width:100% !important">
-                                                            <div class="col-sm-6 col-md-6 p-0">
+                                                            <div class="col-sm-6 col-md-6 p-0 pl-0">
                                                                 <label>Son Kullanma Ayı</label>
                                                                 {{-- <input type="number" class="form-control" id="month"
                                                             name="month" placeholder="09"> --}}
@@ -861,7 +861,7 @@
                                                                     <option value="12">Aralık</option>
                                                                 </select>
                                                             </div>
-                                                            <div class="col-sm-6 col-md-6 pr-0">
+                                                            <div class="col-sm-6 col-md-6 pr-0 pl-0">
                                                                 <label>Son Kullanma Yılı</label>
                                                                 <select class="form-control" id="year"
                                                                     name="year">
