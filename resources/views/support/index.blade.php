e8a6daddf51ce184373e27dd255c09cb34939400
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-04-20 16:55:23 +0300

    "Updated 'resources/views/support/index.blade.php' to adjust the 'Destek Talebi Oluştur' button styling"
    This commit message describes the changes made to the 'index.bl

--- resources/views/support/index.blade.php
+++ resources/views/support/index.blade.php
@@ -5,7 +5,8 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-3" id="sozlesmeLink">
-                    <a href="{{ url('/destek') }}"><button type="button" class=" newButtonStyle w-100">
+                    <a href="{{ url('/destek') }}"><button type="button" class=" newButtonStyle w-100"
+                            style="justify-content: start !important;padding:0 10px;">
                             <span class="buyUserRequest__text newButtonStyle__text"> Destek Talebi
                                 Oluştur</span>
                         </button></a>
