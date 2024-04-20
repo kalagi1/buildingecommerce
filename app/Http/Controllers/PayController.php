9a0fc1208e895d40ad1f2344c95617587b49a5fe
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-04-20 17:31:30 +0300

    "Refactor Housing query in PayController; replace complex query with simpler, more direct query"
    This commit refactors the code in the `index` method of the `PayController` class, specifically the query that retrieves housing

--- app/Http/Controllers/PayController.php
+++ app/Http/Controllers/PayController.php
@@ -83,39 +83,45 @@ class PayController extends Controller
 
         if (isset($cart) && !empty($cart)) {
             if ($cart['type'] == 'housing') {
-                $housing = Housing::with('images')
-                    ->select(
-                        'housings.id',
-                        'housings.slug',
-                        'housings.title AS housing_title',
-                        'housings.created_at',
-                        'housings.step1_slug',
-                        'housings.step2_slug',
-                        'housing_types.title as housing_type_title',
-                        'housings.housing_type_data',
-                        'project_list_items.column1_name as column1_name',
-                        'project_list_items.column2_name as column2_name',
-                        'project_list_items.column3_name as column3_name',
-                        'project_list_items.column4_name as column4_name',
-                        'project_list_items.column1_additional as column1_additional',
-                        'project_list_items.column2_additional as column2_additional',
-                        'project_list_items.column3_additional as column3_additional',
-                        'project_list_items.column4_additional as column4_additional',
-                        'housings.address',
-                        \Illuminate\Support\Facades\DB::raw('(SELECT status FROM cart_orders WHERE JSON_EXTRACT(cart, "$.type") = "housing" AND JSON_EXTRACT(cart, "$.item.id") = housings.id) AS sold'),
-                        'cities.title AS city_title',
-                        'districts.ilce_title AS county_title',
-                        'neighborhoods.mahalle_title AS neighborhood_title',
-                        DB::raw('(SELECT discount_amount FROM offers WHERE housing_id = housings.id AND type = "housing" AND start_date <= "' . date('Y-m-d H:i:s') . '" AND end_date >= "' . date('Y-m-d H:i:s') . '") as discount_amount'),
-                    )
-                    ->leftJoin('housing_types', 'housing_types.id', '=', 'housings.housing_type_id')
-                    ->leftJoin('project_list_items', 'project_list_items.housing_type_id', '=', 'housings.housing_type_id')
-                    ->leftJoin('cities', 'cities.id', '=', 'housings.city_id')
-                    ->leftJoin('districts', 'districts.ilce_key', '=', 'housings.county_id')
-                    ->leftJoin('neighborhoods', 'neighborhoods.mahalle_id', '=', 'housings.neighborhood_id')
-                    ->where("housings.id", $cart['item']['id'])
-                    ->where('project_list_items.item_type', 2)
-                    ->first();
+
+                $housing = Housing::with( 'neighborhood', 'images', 'reservations', 'user.housings', 'user.banners', 'brand', 'city', 'county' )
+                ->where( 'id', $cart['item']['id'] )
+                ->where( 'status', 1 )->first();
+
+
+                // $housing = Housing::with('images')
+                //     ->select(
+                //         'housings.id',
+                //         'housings.slug',
+                //         'housings.title AS housing_title',
+                //         'housings.created_at',
+                //         'housings.step1_slug',
+                //         'housings.step2_slug',
+                //         'housing_types.title as housing_type_title',
+                //         'housings.housing_type_data',
+                //         'project_list_items.column1_name as column1_name',
+                //         'project_list_items.column2_name as column2_name',
+                //         'project_list_items.column3_name as column3_name',
+                //         'project_list_items.column4_name as column4_name',
+                //         'project_list_items.column1_additional as column1_additional',
+                //         'project_list_items.column2_additional as column2_additional',
+                //         'project_list_items.column3_additional as column3_additional',
+                //         'project_list_items.column4_additional as column4_additional',
+                //         'housings.address',
+                //         \Illuminate\Support\Facades\DB::raw('(SELECT status FROM cart_orders WHERE JSON_EXTRACT(cart, "$.type") = "housing" AND JSON_EXTRACT(cart, "$.item.id") = housings.id) AS sold'),
+                //         'cities.title AS city_title',
+                //         'districts.ilce_title AS county_title',
+                //         'neighborhoods.mahalle_title AS neighborhood_title',
+                //         DB::raw('(SELECT discount_amount FROM offers WHERE housing_id = housings.id AND type = "housing" AND start_date <= "' . date('Y-m-d H:i:s') . '" AND end_date >= "' . date('Y-m-d H:i:s') . '") as discount_amount'),
+                //     )
+                //     ->leftJoin('housing_types', 'housing_types.id', '=', 'housings.housing_type_id')
+                //     ->leftJoin('project_list_items', 'project_list_items.housing_type_id', '=', 'housings.housing_type_id')
+                //     ->leftJoin('cities', 'cities.id', '=', 'housings.city_id')
+                //     ->leftJoin('districts', 'districts.ilce_key', '=', 'housings.county_id')
+                //     ->leftJoin('neighborhoods', 'neighborhoods.mahalle_id', '=', 'housings.neighborhood_id')
+                //     ->where("housings.id", $cart['item']['id'])
+                //     ->where('project_list_items.item_type', 2)
+                //     ->first();
                 $saleType = $housing->step2_slug;
             } else {
                 $project = Project::where('id', $cart['item']['id'])->first();
