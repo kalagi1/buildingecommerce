f4b7bf355498c2a90764658bfe30a33487584294
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-04-26 13:49:35 +0300

    Update whatsapp button SVG dimensions in footer
    The SVG element for the whatsapp button in the footer has been updated with new dimensions (18x18) while maintaining the same viewBox.

--- resources/views/client/layouts/partials/footer.blade.php
+++ resources/views/client/layouts/partials/footer.blade.php
@@ -165,7 +165,7 @@
         <div class="my-properties">
             <table class="payment-plan table">
                 <a id="whatsappButton" class="btn btn-outline-light"><svg xmlns="http://www.w3.org/2000/svg" x="0px"
-                        y="0px" width="30" height="30" viewBox="0 0 48 48">
+                        y="0px" width="18" height="18" viewBox="0 0 48 48">
                         <path fill="#fff"
                             d="M4.9,43.3l2.7-9.8C5.9,30.6,5,27.3,5,24C5,13.5,13.5,5,24,5c5.1,0,9.8,2,13.4,5.6	C41,14.2,43,18.9,43,24c0,10.5-8.5,19-19,19c0,0,0,0,0,0h0c-3.2,0-6.3-0.8-9.1-2.3L4.9,43.3z">
                         </path>
