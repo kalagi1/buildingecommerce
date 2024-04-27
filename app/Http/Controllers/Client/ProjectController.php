ea060120484eb7408bb950035b379cea6dc1b374
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-04-27 17:41:03 +0300

    Change variable name in return statement for consistency
    In the `index` method of the `ProjectController`, the variable name in the return statement has been changed from `$has_same_quantity` to `$total_quantity`

--- app/Http/Controllers/Client/ProjectController.php
+++ app/Http/Controllers/Client/ProjectController.php
@@ -237,7 +237,7 @@ class ProjectController extends Controller
 
 
                 if ($i == 1) {
-                   return  $has_same_quantity;
+                   return  $total_quantity;
                 }
 
                 if (!$has_share_sale && !empty($total_quantity->total_quantity) && isset($total_quantity) && !$has_same_quantity || $has_share_sale  && $has_same_quantity) {
