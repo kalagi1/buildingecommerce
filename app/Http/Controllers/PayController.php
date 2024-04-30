d5395ec77894111fcf160481edddefa746013de4
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-04-30 17:18:23 +0300

    "Change in PayController: added return statement and housing type image handling"
    This commit message describes the changes made to the PayController.php file. It mentions the addition of a return statement and the handling of housing type images. The

--- app/Http/Controllers/PayController.php
+++ app/Http/Controllers/PayController.php
@@ -698,7 +698,6 @@ class PayController extends Controller
         $o = json_decode($cartOrder);
         $productDetails = json_decode($o->cart)->item;
         if (json_decode($o->cart)->type == 'housing') {
-            return "asasas";
             $housingTypeImage = asset('housing_images/' . json_decode(Housing::find($productDetails->id ?? 0)->housing_type_data ?? '[]')->image ?? null);
             $city = Housing::find($productDetails->id ?? 0)->city->title;
             $county = Housing::find($productDetails->id ?? 0)->county->title;
