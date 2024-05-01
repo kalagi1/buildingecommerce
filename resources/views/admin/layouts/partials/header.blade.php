199cdc29d0712d0e8333991232db68e24f55128e
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-05-01 14:23:38 +0300

    update

--- resources/views/admin/layouts/partials/header.blade.php
+++ resources/views/admin/layouts/partials/header.blade.php
@@ -19,9 +19,9 @@
     <!-- ===============================================-->
     <meta name="csrf-token" content="{{ csrf_token() }}">
                                                                             <!-- Canonical URL için bölüm -->
-                                                                            {{-- @if(isset($canonicalUrl))
+                                                                            @if(isset($canonicalUrl))
                                                                                 <link rel="canonical" href="{{ $canonicalUrl }}" />
-                                                                            @endif --}}
+                                                                            @endif
     <link rel="apple-touch-icon" sizes="180x180"
         href="{{ URL::to('/') }}/favicon.png">
     <link rel="icon" type="image/png" sizes="32x32"
--- resources/views/client/layouts/partials/header.blade.php
+++ resources/views/client/layouts/partials/header.blade.php
@@ -23,10 +23,10 @@
 
 
     <!-- FAVICON -->
-                                                                                <!-- Canonical URL için bölüm -->
-                                                                                {{-- @if(isset($canonicalUrl))
-                                                                                        <link rel="canonical" href="{{ $canonicalUrl }}" />
-                                                                                @endif --}}
+    <!-- Canonical URL için bölüm -->
+    @if (isset($canonicalUrl))
+        <link rel="canonical" href="{{ $canonicalUrl }}" />
+    @endif
     <link rel="shortcut icon" type="image/x-icon" href="{{ URL::to('/') }}/favicon.png">
     <link rel="stylesheet" href="{{ URL::to('/') }}/css/jquery-ui.css">
     <!-- GOOGLE FONTS -->
@@ -334,19 +334,30 @@
             }
         }
     </style>
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-55Q6HGHL');</script>
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-55Q6HGHL');
+    </script>
     <!-- End Google Tag Manager -->
 </head>
 
 <body class="m0a homepage-2 the-search hd-white inner-pages">
     <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-55Q6HGHL"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-55Q6HGHL" height="0" width="0"
+            style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
     <!-- Wrapper -->
     <div id="wrapper">
