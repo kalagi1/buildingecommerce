8593038aec784e8555d2be643ed5e06e5c381d7e
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-05-02 12:07:21 +0300

    Update cookie consent design and message
    - Changes to app.css:
      - Adjusted position, width, background color, padding, and box-shadow of the cookie consent banner.
    - Changes to resources/lang/vendor

--- resources/lang/vendor/cookie-consent/tr/texts.php
+++ resources/lang/vendor/cookie-consent/tr/texts.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'message' => 'Bu sitedeki deneyiminizi çerezlere izin vererek geliştirebilirsiniz.',
+    'message' => 'İnternet sitemizin çalışması, analitik çalışmaların yürütülmesi ve tercihleriniz doğrultusunda sitenin kişiselleştirilmesi için çerezler kullanmaktayız. Ayrıca açık rıza vermeniz halinde size yönelik reklam/pazarlama faaliyetlerinin gerçekleştirilmesi kapsamında birinci ve üçüncü taraf çerezler kullanılacaktır.',
     'agree' => 'Çerezlere izin ver',
 ];
