63cf286e5c7d4366ff242271d85d931247a8d271
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-05-03 09:21:18 +0300

    Update footer script references
    The commit updates the script references in the footer by commenting out the first script reference and uncommenting the second one. It also adds a new script reference for "/build/assets/app-a0

--- resources/views/institutional/layouts/partials/footer.blade.php
+++ resources/views/institutional/layouts/partials/footer.blade.php
@@ -96,8 +96,8 @@
 <script src="https://formbuilder.online/assets/js/form-builder.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jQuery-formBuilder/3.4.2/form-render.min.js"></script>
 
-<script src="{{URL::to('/')}}/build/assets/app-eca8790f.js"></script>
-{{-- <script src="{{URL::to('/')}}/build/assets/app-a00207ef.js"></script>  --}}
+{{-- <script src="{{URL::to('/')}}/build/assets/app-eca8790f.js"></script> --}}
+<script src="{{URL::to('/')}}/build/assets/app-a00207ef.js"></script> 
 
 
 
