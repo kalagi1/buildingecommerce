5eca8fd0544af881fb36525f6ff047aa57f681cc
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-05-03 18:28:56 +0300

    Update text in homes-price div to "Paylaşımlı Emlak" in housing-card component
    This commit updates the text in the homes-price div from "" to "Paylaşımlı Emlak

--- resources/views/components/housing-card.blade.php
+++ resources/views/components/housing-card.blade.php
@@ -20,7 +20,7 @@
                                 Satılık
                             @endif
                         </div>
-                        <div class="homes-price"><i class="fa fa-handshake-o"></i></div>
+                        <div class="homes-price"><i class="fa fa-handshake-o"></i> Paylaşımlı Emlak</div>
                         <img loading="lazy"
                             src="{{ URL::to('/') . '/housing_images/' . json_decode($housing->housing_type_data)->image }}"
                             alt="{{ $housing->housing_title }}" class="img-responsive">
