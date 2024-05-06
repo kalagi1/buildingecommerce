7660a0f8508e098c0568875ef882b5219cff94cc
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-05-06 15:17:03 +0300

    "Updated search result view to display projects with a modified project-card layout"
    This commit updates the 'resources/views/client/search/result.blade.php' file by modifying the way projects are displayed in the search

--- resources/views/client/search/result.blade.php
+++ resources/views/client/search/result.blade.php
@@ -615,7 +615,22 @@
                         ({{ count($results['projects']) }})</div>
                     <div class="row mt-2">
                         @foreach ($results['projects'] as $result)
-                        <x-project-card :project="$result" />
+                            <div class="col-sm-12 col-md-4 col-lg-4 col-12 projectMobileMargin" data-aos="zoom-in"
+                                data-aos-delay="150" style="height:200px">
+                                <div class="project-single no-mb aos-init aos-animate" style="height:100%"
+                                    data-aos="zoom-in" data-aos-delay="150">
+                                    <div class="listing-item compact" style="height:100%">
+                                        <a href="{{ route('project.detail', ['slug' => $result['slug'], 'id' => $result['id'] +1000000]) }}"
+                                            class="listing-img-container">
+                                            <div class="listing-img-content" style="padding-left:10px;text-transform:uppercase;background-color: rgba({{ mt_rand(0, 255) }}, {{ mt_rand(0, 255) }}, {{ mt_rand(0, 255) }}, 0.8);">
+                                                <span class="badge badge-phoenix text-left">{{ $project->project_title }}</span>
+                                            </div>
+                                            <img src="{{ URL::to('/') . '/' . str_replace('public/', 'storage/', $result['photo']) }}"
+                                                alt="" style="height:100%;object-fit:contain">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
                         @endforeach
                     </div>
                 @endif
