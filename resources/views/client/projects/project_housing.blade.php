906bfc5368e10ef763c32ee70d7bcf8aaf6b98ab
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-05-07 16:25:59 +0300

    "Updated index calculation in project\_housing.blade.php to start counting from 1 instead of 0"
    This commit message describes the change in the code diff by stating that the calculation of the index in the project\_h

--- resources/views/client/projects/project_housing.blade.php
+++ resources/views/client/projects/project_housing.blade.php
@@ -1685,7 +1685,7 @@
         function updateIndex() {
             var totalSlides = $('#listingDetailsSlider .carousel-item').length; // Toplam slayt sayısını al
             var index = $('#listingDetailsSlider .carousel-item.active').index(); // Aktif slaydın indeksini al
-            $('.pagination .page-item-middle .page-link').text((index == 0 ? 1 : index + 1) + '/' +
+            $('.pagination .page-item-middle .page-link').text((index + 1) + '/' +
                 totalSlides); // Ortadaki li etiketinin metnini güncelle
         }
 
