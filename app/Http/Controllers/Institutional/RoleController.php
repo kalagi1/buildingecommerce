582ed365b566b2f663eddf3cc8cc26d3118092d9
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-05-08 15:31:25 +0300

    Update RoleController: remove unnecessary code, add filtered permissions in create method, and pass mainRole permissions to edit view
    The diff removes unnecessary code in the RoleController's create and edit methods, adds filtered permissions in the create method,

--- app/Http/Controllers/Institutional/RoleController.php
+++ app/Http/Controllers/Institutional/RoleController.php
@@ -9,82 +9,85 @@ use App\Models\Role;
 use App\Models\RolePermission;
 use Illuminate\Support\Facades\Auth;
 
-class RoleController extends Controller
-{
+class RoleController extends Controller {
 
-    public function index()
-    {
-        $roles = Role::where("parent_id", auth()->user()->parent_id ?? auth()->user()->id)->get();
-        return view('institutional.roles.index', compact('roles'));
+    public function index() {
+        $roles = Role::where( 'parent_id', auth()->user()->parent_id ?? auth()->user()->id )->get();
+        return view( 'institutional.roles.index', compact( 'roles' ) );
     }
 
-    public function create()
-    {
-        $role = Role::where("id", "2")->with("rolePermissions.permissions")->first();
-        $permissions = $role->rolePermissions->pluck('permissions')->flatten();
+    public function create() {
+        $role = Role::where( 'id', '2' )->with( 'rolePermissions.permissions' )->first();
+        $permissions = $role->rolePermissions->pluck( 'permissions' )->flatten();
+        $specialPermissions = [
+            'Projects',
+            'CreateProject',
+            'GetProjects',
+            'DeleteProject',
+            'UpdateProject',
+        ];
+        $filteredPermissions = $permissions->reject( function (
+            $permission,
+        ) use ( $specialPermissions ) {
+            return in_array( $permission->key, $specialPermissions );
+        }
+    );
 
-        return $permissions;
-        
-        $groupedPermissions = $permissions->groupBy('permission_group_id');
+    $groupedPermissions = $filteredPermissions->groupBy( 'permission_group_id' );
 
-        return view('institutional.roles.create', compact('groupedPermissions'));
-    }
-    
-    
+    return view( 'institutional.roles.create', compact( 'groupedPermissions' ) );
+}
 
-    public function edit(Role $role)
-    {
-        $role = Role::where("id", $role->id)->with("rolePermissions.permissions")->first();
-        $mainRole = Role::where("id", "2")->with("rolePermissions.permissions")->first();
-        $permissions = $mainRole->rolePermissions->pluck('permissions')->flatten();
-         $groupedPermissions = $permissions->groupBy('permission_group_id');
-        return view('institutional.roles.edit', compact('role', 'groupedPermissions'));
-    }
+public function edit( Role $role ) {
+    $role = Role::where( 'id', $role->id )->with( 'rolePermissions.permissions' )->first();
+    $mainRole = Role::where( 'id', '2' )->with( 'rolePermissions.permissions' )->first();
+    $permissions = $mainRole->rolePermissions->pluck( 'permissions' )->flatten();
+    $groupedPermissions = $permissions->groupBy( 'permission_group_id' );
+    return view( 'institutional.roles.edit', compact( 'role', 'groupedPermissions' ) );
+}
 
-    public function store(CreateRoleRequest $request)
-    {
-        $permissions = $request->input('permissions');
+public function store( CreateRoleRequest $request ) {
+    $permissions = $request->input( 'permissions' );
 
-        $role = Role::create([
-            'name' => $request->input('name'),
-            "parent_id" => auth()->user()->parent_id ?? auth()->user()->id,
-        ]);
+    $role = Role::create( [
+        'name' => $request->input( 'name' ),
+        'parent_id' => auth()->user()->parent_id ?? auth()->user()->id,
+    ] );
 
-        if (!empty($permissions)) {
-            foreach ($permissions as $permissionId) {
-                RolePermission::create([
-                    "role_id" => $role->id,
-                    "permission_id" => $permissionId]);
+    if ( !empty( $permissions ) ) {
+        foreach ( $permissions as $permissionId ) {
+            RolePermission::create( [
+                'role_id' => $role->id,
+                'permission_id' => $permissionId ] );
             }
         }
 
-        return redirect()->route('institutional.roles.index')->with('success', 'Role created successfully');
+        return redirect()->route( 'institutional.roles.index' )->with( 'success', 'Role created successfully' );
     }
 
-    public function update(UpdateRoleRequest $request, Role $role)
-    {
-        $permissions = $request->input('permissions');
+    public function update( UpdateRoleRequest $request, Role $role ) {
+        $permissions = $request->input( 'permissions' );
 
-        $role->update([
-            'name' => $request->input('name'),
-        ]);
+        $role->update( [
+            'name' => $request->input( 'name' ),
+        ] );
 
-        RolePermission::where('role_id', $role->id)->delete();
+        RolePermission::where( 'role_id', $role->id )->delete();
 
-        if (!empty($permissions)) {
-            foreach ($permissions as $permissionId) {
-                RolePermission::create([
-                    "role_id" => $role->id,
-                    "permission_id" => $permissionId,
-                ]);
+        if ( !empty( $permissions ) ) {
+            foreach ( $permissions as $permissionId ) {
+                RolePermission::create( [
+                    'role_id' => $role->id,
+                    'permission_id' => $permissionId,
+                ] );
             }
         }
 
-        return redirect()->route('institutional.roles.index')->with('success', 'Role updated successfully');
+        return redirect()->route( 'institutional.roles.index' )->with( 'success', 'Role updated successfully' );
     }
-    public function destroy(Role $role)
-    {
+
+    public function destroy( Role $role ) {
         $role->delete();
-        return redirect()->route('institutional.roles.index')->with('success', 'Role deleted successfully');
+        return redirect()->route( 'institutional.roles.index' )->with( 'success', 'Role deleted successfully' );
     }
 }
