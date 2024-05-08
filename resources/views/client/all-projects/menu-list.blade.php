35d89bd0f4c11f653500d4859d381448a3167794
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-05-08 11:37:51 +0300

    Update homes img alt text in menu-list.blade.php
    Change the variable name from `open_sharing1` to `open_share1` in the alt attribute of the homes img element. Also, add a fall

--- resources/views/client/all-projects/menu-list.blade.php
+++ resources/views/client/all-projects/menu-list.blade.php
@@ -1455,8 +1455,7 @@
                                                                 <!-- homes img -->
                                                                 <div class="homes-img h-100 d-flex align-items-center"
                                                                     style="width: 110px; height: 128px;">
-                                                                    ${res.housing_type.open_sharing1}
-                                                                    ${res.housing_type.open_sharing1 ??
+                                                                    ${res.housing_type.open_share1 ??
                                                                         ` <div class="homes-price"style="bottom: 0;left:0;z-index:9"><i class="fa fa-handshake-o"></i></div> `
                                                                     }
                                                                     <img src="${res.image}" alt="${res.title}" class="img-responsive"
