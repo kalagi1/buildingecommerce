882641775cd51af915154a9ecfd42a44273eb05d
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-05-08 15:10:23 +0300

    Update role creation view: filter special permissions from permission groups
    This commit updates the institutional/roles/create.blade.php view by filtering special permissions from permission groups. It iterates through the grouped permissions, rejects any

--- resources/views/institutional/roles/create.blade.php
+++ resources/views/institutional/roles/create.blade.php
@@ -47,22 +47,28 @@
 
                                         {{-- Özel izinleri listeden çıkar --}}
                                         @foreach ($groupedPermissions as $groupId => $groupPermissions)
-                                            @if (count($groupPermissions) > 0)
+                                            @php
+                                            // Grup içindeki özel izinleri çıkartarak izin listesini güncelle
+                                            $filteredPermissions = $groupPermissions->reject(function ($permission) use ($specialPermissions) {
+                                                return in_array($permission->key, $specialPermissions);
+                                            });
+                                            @endphp
+
+                                            {{-- Eğer grup içinde izin kalmadıysa bu grubu atla --}}
+                                            @if ($filteredPermissions->isNotEmpty())
                                                 <div class="col-12">
                                                     @php
                                                         $groupTitle = \App\Models\PermissionGroup::find($groupId)->desc;
                                                     @endphp
                                                     <h4 class="mt-3" style="margin-bottom:10px;">{{ $groupTitle }}</h4>
                                                     <div class="mb-3">
-                                                        @foreach ($groupPermissions->reject(function ($permission) use ($specialPermissions) {
-            return in_array($permission->key, $specialPermissions);
-        }) as $permission)
-                                                            @if ($permission->description != 'Modülün menüde etkin olması için bu seçeneği işaretlemeniz gerekmektedir.')
-                                                                <div class="form-check form-control px-3"
-                                                                    style="cursor: pointer">
+                                                        @foreach ($filteredPermissions as $permission)
+                                                            @if ($permission->description !== 'Modülün menüde etkin olması için bu seçeneği işaretlemeniz gerekmektedir.')
+                                                                <div class="form-check form-control px-3" style="cursor: pointer">
                                                                     <input class="form-check-input" type="checkbox"
                                                                         id="permission-{{ $permission->id }}"
-                                                                        name="permissions[]" value="{{ $permission->id }}">
+                                                                        name="permissions[]" 
+                                                                        value="{{ $permission->id }}">
                                                                     <label class="form-check-label"
                                                                         for="permission-{{ $permission->id }}">
                                                                         {{ $permission->description }}
