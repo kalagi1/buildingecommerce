0bfd477a17a2fcdbd783031afc4fec97a2d578e5
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-05-09 15:01:58 +0300

    "Updated FormController and added a new route for showing swap application details"
    This diff has made the following changes:
    
    1. In `FormController`, the `swapApplications()` function has been updated to return a JSON response

--- app/Http/Controllers/Api/Institutional/FormController.php
+++ app/Http/Controllers/Api/Institutional/FormController.php
@@ -7,18 +7,22 @@ use App\Models\Form;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class FormController extends Controller
-{
-    public function swapApplications()
-    {
-        $apps = Form::with("city", "county")->where('store_id', Auth::user()->id)->orderBy('created_at', 'desc')->get();
+class FormController extends Controller {
+    public function swapApplications() {
+        $apps = Form::with( 'city', 'county' )->where( 'store_id', Auth::user()->id )->orderBy( 'created_at', 'desc' )->get();
 
-        if ($apps->isNotEmpty()) {
+        if ( $apps->isNotEmpty() ) {
             // Eğer veri bulunduysa, veriyi dön
-            return response()->json($apps, 200);
+            return response()->json( $apps, 200 );
         }
 
         // Eğer veri bulunamadıysa, uygun bir mesaj dön
-        return response()->json(['message' => 'Not Found.'], 404);
+        return response()->json( [ 'message' => 'Not Found.' ], 404 );
+    }
+
+    public function showSwapApplication( Request $request, Form $form ) {
+        return json_encode( [
+            'form' => $form
+        ] );
     }
 }
