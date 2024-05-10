512189f716cfe5fa4986ba1bd0e9053e42569050
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-05-10 18:21:13 +0300

    Update footer script source for production build
    This commit updates the script source for the production build of the application in the footer from "<https://build/assets/app-a7103fa4.js>" to "<https

--- resources/views/institutional/layouts/partials/footer.blade.php
+++ resources/views/institutional/layouts/partials/footer.blade.php
@@ -97,7 +97,7 @@
 <script src="https://formbuilder.online/assets/js/form-builder.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jQuery-formBuilder/3.4.2/form-render.min.js"></script>
 
-<script src="{{URL::to('/')}}/build/assets/app-a7103fa4.js"></script>
+<script src="{{URL::to('/')}}/build/assets/app-1345241c.js"></script>
 
 
 
