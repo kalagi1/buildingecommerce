08d783058853d299323e4d502c582cc7bbad0e56
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-05-10 18:16:34 +0300

    Update project title Blade template
    The Blade template for the project title has been updated to remove the <h3> tag and move the variable interpolations outside of it. The variables used are:
    
    * `$advert

--- resources/views/client/layouts/partials/project_title.blade.php
+++ resources/views/client/layouts/partials/project_title.blade.php
@@ -1,4 +1,4 @@
 
-<h3>{{ $advertiseTitle ? $advertiseTitle : " " }}  {{ $housingOrder }} {{ "No'lu" }} {{ $step1Slug }}</h3>
+{{ $advertiseTitle ? $advertiseTitle : " " }}  {{ $housingOrder }} {{ "No'lu" }} {{ $step1Slug }}
 
 
