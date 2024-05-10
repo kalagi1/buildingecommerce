6c5d9d462568bfbe9677888cd04d700997e9c3a6
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-05-10 12:59:37 +0300

    Update position and style of favorite button in project\_housing view
    The position and style of the favorite button in the project\_housing view have been updated. The button-effect-div now has a position of absolute, a z-

--- resources/views/client/projects/project_housing.blade.php
+++ resources/views/client/projects/project_housing.blade.php
@@ -233,13 +233,16 @@
                     <div class="row">
                         <div class="col-md-12">
                             <div id="listingDetailsSlider" class="carousel listing-details-sliders slide mb-30">
-                                <div class="button-effect-div">
-                                    <div class="button-effect toggle-project-favorite"
-                                        style="margin-left:13px;width:40px !important"
+                                <div class="button-effect-div"
+                                    style="    position: absolute;
+                                z-index: 9;
+                                margin-top: 20px;
+                                right: 40px;">
+                                    <div class="button-effect toggle-project-favorite" style="width:35px !important"
                                         data-project-housing-id="{{ $projectHousingsList[$housingOrder]['squaremeters[]'] }}"
                                         data-project-id={{ $project->id }}>
                                         <i class="fa fa-heart-o"></i>
-                                    </div> 
+                                    </div>
                                 </div>
                                 <div class="carousel-inner">
 
