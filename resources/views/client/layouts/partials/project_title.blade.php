3ec3826d929d33b579a03e18294c2f3a01529967
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-05-10 18:14:51 +0300

    Update project title Blade template
    * Remove unnecessary div wrapper
    * Simplify spacing using PHP alternative syntax for inline if statement

--- resources/views/client/layouts/partials/project_title.blade.php
+++ resources/views/client/layouts/partials/project_title.blade.php
@@ -1,8 +1,5 @@
-<div>
-    <h3 class="mobile-hidden">{{ $title }}</h3>
-    <span class="title-fsp" style="margin-top: 5px">{{ $advertiseTitle ? $advertiseTitle : ' ' }} {{ $housingOrder }}
-        {{ "No'lu" }} {{ $step1Slug }}</span>
+
+<h3 class="mobile-hidden">{{$title}}</h3>
+<span class="title-fsp" style="margin-top: 5px">{{ $advertiseTitle ? $advertiseTitle : " " }}  {{ $housingOrder }} {{ "No'lu" }} {{ $step1Slug }}</span>
 
 
-
-</div>
