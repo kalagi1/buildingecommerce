2116f48f59535df4dc953ebd775f817497f4e384
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-05-10 15:56:03 +0300

    Update footer.blade.php: remove old app.js, add new app.js
    This commit updates the footer.blade.php file by removing the old app-5bdb4125.js and

--- resources/views/institutional/layouts/partials/footer.blade.php
+++ resources/views/institutional/layouts/partials/footer.blade.php
@@ -89,7 +89,7 @@
 </script>
 
 <script src="{{ URL::to('/') }}/adminassets/assets//js/ecommerce-dashboard.js"></script>
-<script src="{{URL::to('/')}}/build/assets/app-5bdb4125.js"></script> 
+
 
 <!--FormBuilder-->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
@@ -97,6 +97,7 @@
 <script src="https://formbuilder.online/assets/js/form-builder.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jQuery-formBuilder/3.4.2/form-render.min.js"></script>
 
+<script src="{{URL::to('/')}}/build/assets/app-a7103fa4.js"></script>
 
 
 
