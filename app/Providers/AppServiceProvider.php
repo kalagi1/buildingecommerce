95348a9ba456f39ba2b46cf0d903a7d577b276c0
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-05-13 15:00:48 +0300

    "Changed `return $permissions;` to `dd($permissions);` in `setMenuVisibility` method of `AppServiceProvider`"

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -151,7 +151,7 @@ class AppServiceProvider extends ServiceProvider
 
     private function setMenuVisibility(&$menuItem, $permissions)
     {
-        return $permissions;
+      dd($permissions);
         if (isset($menuItem['subMenu'])) {
             // Alt menü anahtarlarını pluck et ve kontrol et
             $subMenuKeys = collect($menuItem['subMenu'])->pluck('key');
