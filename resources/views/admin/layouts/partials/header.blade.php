8d2e4456e078cc0c55b2255e2f2f376e7a79835c
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-05-13 17:38:54 +0300

    Change variable name from 'text' to 'key' in header.blade.php for GetProjects and GetHousings menu items
    This commit changes the variable name from 'text' to 'key' in the header.bl

--- resources/views/admin/layouts/partials/header.blade.php
+++ resources/views/admin/layouts/partials/header.blade.php
@@ -209,7 +209,7 @@
                                                 {{ $commentCount != null ? "(". $commentCount->count() .")" : null }}     
                                                 {{ $sharerCount != null ? "(". $sharerCount->count() .")" : null }}   
 
-                                                @if ($menuItem['text'] == "GetProjects" || $menuItem['text'] == "GetHousings")
+                                                @if ($menuItem['key'] == "GetProjects" || $menuItem['key'] == "GetHousings")
                                                 <svg class="svg-inline--fa fa-circle text-info ms-1 new-page-indicator" style="font-size: 6px;" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256z"></path></svg>
                                                 @endif
                                             </span>
