c51df52ffce6f57bdc08be35e51a3cfb3835bc2c
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-05-14 14:21:28 +0300

    "Changed Collection query in DashboardController to limit results to 6 and ordered by id in descending order. Added if statement in index.blade.php to conditionally display real estate section for certain user types."

--- app/Http/Controllers/Institutional/DashboardController.php
+++ app/Http/Controllers/Institutional/DashboardController.php
@@ -386,7 +386,7 @@ class DashboardController extends Controller
             ->where("status", "2")
             ->sum('balance');
 
-        $collections = Collection::with("links")->where("user_id", Auth::user()->id)->get();
+        $collections = Collection::with("links")->where("user_id", Auth::user()->id)->orderBy("id","desc")->limit(6)->get();
         $totalStatus1Count = $balanceStatus1Lists->count();
         $successPercentage = $totalStatus1Count > 0 ? ($totalStatus1Count / ($totalStatus1Count + $balanceStatus0Lists->count() + $balanceStatus2Lists->count())) * 100 : 0;
 
