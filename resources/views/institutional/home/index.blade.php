af3f5766b779be983638ec0550b4011fe62fc2ef
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-05-14 13:41:09 +0300

    "Adjusted layout of invoices section in institutional home index view"
    This commit message accurately describes the code diff by mentioning that it involves adjusting the layout of the invoices section in the institutional home index view. It is

--- resources/views/institutional/home/index.blade.php
+++ resources/views/institutional/home/index.blade.php
@@ -35,7 +35,7 @@
                     </div>
                     <div class="row align-items-center g-4">
 
-                        <div class="col-12 col-sm-6 col-md-3 col-lg-6 col-xl-3 col-xxl-12">
+                        <div class="col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
                             <div class="d-flex align-items-center"><span
                                     class="fs-4 lh-1 uil uil-invoice text-warning-dark"></span>
                                 <div class="ms-2">
@@ -46,25 +46,32 @@
                                     <p class="text-body-secondary fs-9 mb-0">Soon to be cleared</p>
                                 </div>
                             </div>
-                            <div class="d-flex align-items-center"><span
-                                class="fs-4 lh-1 uil uil-invoice text-warning-dark"></span>
-                            <div class="ms-2">
-                                <div class="d-flex align-items-end">
-                                    <h2 class="mb-0 me-2">23</h2><span
-                                        class="fs-7 fw-semibold text-body">Invoices</span>
-                                </div>
-                                <p class="text-body-secondary fs-9 mb-0">Soon to be cleared</p>
-                            </div>
-                        </div> <div class="d-flex align-items-center"><span
-                            class="fs-4 lh-1 uil uil-invoice text-warning-dark"></span>
-                        <div class="ms-2">
-                            <div class="d-flex align-items-end">
-                                <h2 class="mb-0 me-2">23</h2><span
-                                    class="fs-7 fw-semibold text-body">Invoices</span>
-                            </div>
-                            <p class="text-body-secondary fs-9 mb-0">Soon to be cleared</p>
+                            
                         </div>
-                    </div>
+                        <div class="col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                            <div class="d-flex align-items-center"><span
+                                    class="fs-4 lh-1 uil uil-invoice text-warning-dark"></span>
+                                <div class="ms-2">
+                                    <div class="d-flex align-items-end">
+                                        <h2 class="mb-0 me-2">23</h2><span
+                                            class="fs-7 fw-semibold text-body">Invoices</span>
+                                    </div>
+                                    <p class="text-body-secondary fs-9 mb-0">Soon to be cleared</p>
+                                </div>
+                            </div>
+                            
+                        </div><div class="col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                            <div class="d-flex align-items-center"><span
+                                    class="fs-4 lh-1 uil uil-invoice text-warning-dark"></span>
+                                <div class="ms-2">
+                                    <div class="d-flex align-items-end">
+                                        <h2 class="mb-0 me-2">23</h2><span
+                                            class="fs-7 fw-semibold text-body">Invoices</span>
+                                    </div>
+                                    <p class="text-body-secondary fs-9 mb-0">Soon to be cleared</p>
+                                </div>
+                            </div>
+                            
                         </div>
                         
                     </div>
