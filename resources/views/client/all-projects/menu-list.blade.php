3f0fec8bc3cffdc3213a4b0b3e8629fd7b593b39
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-05-18 18:34:00 +0300

    "Removed unnecessary option appends in county and neighborhood dropdowns"

--- resources/views/client/all-projects/menu-list.blade.php
+++ resources/views/client/all-projects/menu-list.blade.php
@@ -868,13 +868,9 @@
                 url: "{{ url('get-counties') }}/" + $(this).val(),
                 success: function(res) {
                     $('#county').empty();
-                    $('#county').append(
-                            `<option value="#">İlçe Seçiniz</option>`
-                        );
+                   
                     $("#neighborhood").empty();
-                    $('#neighborhood').append(
-                            `<option value="#">Mahalle</option>`
-                        );
+                   
                     $(".hiddenCountyName").removeClass("d-flex").addClass("d-none");
                     $(".hiddenNeighborhoodName").removeClass("d-flex").addClass("d-none");
 
@@ -925,9 +921,7 @@
                 success: function(res) {
                     console.log(res);
                     $("#neighborhood").empty();
-                    $('#neighborhood').append(
-                            `<option value="#">Mahalle Seçiniz</option>`
-                        );
+                
                     // İlçe adını slug formatına çevir
                     var countySlug = res.countySlug;
 
