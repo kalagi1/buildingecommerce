ada7e4d9d4cbef6e5b1186241d8f34f618ad9b3b
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-05-20 16:39:34 +0300

    "Added new `give_offer` method to `ProjectController` and corresponding route, updated existing methods and routes in `ProjectController`, and made minor formatting changes."

--- routes/api.php
+++ routes/api.php
@@ -170,6 +170,7 @@ Route::group(['middleware' => 'auth:api'], function () {
 
         Route::get('/get_boughts', [BoughtController::class, 'bougths'])->name('react.bougths');
         Route::get('/get_solds', [BoughtController::class, 'solds'])->name('react.solds');
+        Route::post('give_offer', [ProjectController::class, 'give_offer'])->name('give_offer');
 
         Route::get('/swap_applications', [InstitutionalFormController::class, 'swapApplications'])->name('react.swap.applications');
         Route::get('/swap_applications/{form}', [InstitutionalFormController::class, 'showSwapApplication'])->name('react.show.swap.applications');
