4b32d305aaf089b31d64c617d035cc1e6ebc640e
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-05-21 12:33:13 +0300

    update

--- app/Http/Controllers/Client/CartController.php
+++ app/Http/Controllers/Client/CartController.php
@@ -387,8 +387,9 @@ class CartController extends Controller
                 }
             } else {
                 $housing = Housing::where('id', $cart['item']['id'])->first();
+                return $lastClick ? $lastClick : "yok";
                 $user = User::where('id', $housing->user_id)->first();
-                if (isset($lastClick) && $lastClick->user_id != Auth::user()->id) {
+                if (isset($lastClick)) {
                     $collection = Collection::where('id', $lastClick->collection_id)->first();
                     $newAmount = $amountWithoutDiscount - ($amountWithoutDiscount * ($discountRate / 100));
                     $rates = Rate::where('housing_id', $cart['item']['id'])->get();
