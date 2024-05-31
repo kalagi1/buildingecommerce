0ca73e41765d3663d0f1e484e3fda68b5d6a1416
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-05-31 13:30:07 +0300

    "Removed auth middleware from CartController constructor"

--- app/Http/Controllers/Client/CartController.php
+++ app/Http/Controllers/Client/CartController.php
@@ -34,9 +34,6 @@ use Illuminate\Support\Facades\Response;
 
 class CartController extends Controller {
 
-    public function __construct() {
-        $this->middleware( 'auth' );
-    }
 
     public function payCart( Request $request ) {
 
