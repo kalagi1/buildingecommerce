c05a2b076c88a424ca4b4736fad2333b91cb429b
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-05-31 17:36:07 +0300

    "Removed 'web' middleware from API route group and reformatted middleware declaration"

--- routes/api.php
+++ routes/api.php
@@ -119,7 +119,7 @@ Route::post('password/email', [AuthController::class, "sendResetLinkEmail"])->na
 
 Route::get('/emlak-kulup/{userid}/koleksiyonlar/{id}', [SharerController::class, "showClientLinks"])->name('sharer.links.showClientLinks');
 
-Route::middleware(['auth:api', 'web'])->group(function () {
+Route::group(['middleware' => 'auth:api'], function () {
     Route::group(['prefix' => 'institutional', "as" => "institutional.", 'middleware' => ['institutional', 'checkCorporateAccount', "checkHasClubAccount"]], function () {
         Route::get('/collections/{id}', [SharerController::class, "show"])->name('collection.show');
         Route::get('my-cart', [CartController::class, 'index'])->name('cart');
