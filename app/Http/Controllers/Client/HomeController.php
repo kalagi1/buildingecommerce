6cc4e15fe061174fc8eb2116845010ec2192615a
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-06-06 16:46:33 +0300

    "Refactored HomeController's index method to remove unnecessary query and reformatted secondhandHousings query with array syntax and added IFNULL to sold column"

--- app/Http/Controllers/Client/HomeController.php
+++ app/Http/Controllers/Client/HomeController.php
@@ -74,44 +74,45 @@ class HomeController extends Controller
     public function index()
     {
 
-        $housings = Housing::with("images")->limit(1)->get();
-        return $housings;
-        $secondhandHousings =  Housing::with('images')
-            ->select(
-                'housings.id',
-                'housings.slug',
-                'housings.title AS housing_title',
-                'housings.created_at',
-                'housings.step1_slug',
-                'housings.step2_slug',
-                'housing_types.title as housing_type_title',
-                'housings.housing_type_data',
-                'project_list_items.column1_name as column1_name',
-                'project_list_items.column2_name as column2_name',
-                'project_list_items.column3_name as column3_name',
-                'project_list_items.column4_name as column4_name',
-                'project_list_items.column1_additional as column1_additional',
-                'project_list_items.column2_additional as column2_additional',
-                'project_list_items.column3_additional as column3_additional',
-                'project_list_items.column4_additional as column4_additional',
-                'housings.address',
-                DB::raw('(SELECT status FROM cart_orders WHERE JSON_EXTRACT(cart, "$.type") = "housing" AND JSON_EXTRACT(cart, "$.item.id") = housings.id ORDER BY created_at DESC LIMIT 1) AS sold'),
-                'cities.title AS city_title',
-                'districts.ilce_title AS county_title',
-                'neighborhoods.mahalle_title AS neighborhood_title',
-                DB::raw('(SELECT discount_amount FROM offers WHERE housing_id = housings.id AND type = "housing" AND start_date <= "' . date('Y-m-d H:i:s') . '" AND end_date >= "' . date('Y-m-d H:i:s') . '" LIMIT 1) as discount_amount'),
-            )
-            ->leftJoin('housing_types', 'housing_types.id', '=', 'housings.housing_type_id')
-            ->leftJoin('project_list_items', 'project_list_items.housing_type_id', '=', 'housings.housing_type_id')
-            ->leftJoin('housing_status', 'housings.status_id', '=', 'housing_status.id')
-            ->leftJoin('cities', 'cities.id', '=', 'housings.city_id')
-            ->leftJoin('districts', 'districts.ilce_key', '=', 'housings.county_id')
-            ->leftJoin('neighborhoods', 'neighborhoods.mahalle_id', '=', 'housings.neighborhood_id')
-            ->where('housings.status', 1)
-            ->where('project_list_items.item_type', 2)
-            ->orderByDesc('housings.created_at')
-            ->limit(1)
-            ->get();
+        $secondhandHousings = Housing::with('images')
+        ->select([
+            'housings.id',
+            'housings.slug',
+            'housings.title AS housing_title',
+            'housings.created_at',
+            'housings.step1_slug',
+            'housings.step2_slug',
+            'housing_types.title as housing_type_title',
+            'housings.housing_type_data',
+            'project_list_items.column1_name',
+            'project_list_items.column2_name',
+            'project_list_items.column3_name',
+            'project_list_items.column4_name',
+            'project_list_items.column1_additional',
+            'project_list_items.column2_additional',
+            'project_list_items.column3_additional',
+            'project_list_items.column4_additional',
+            'housings.address',
+            DB::raw('IFNULL((SELECT status FROM cart_orders WHERE JSON_EXTRACT(cart, "$.type") = "housing" AND JSON_EXTRACT(cart, "$.item.id") = housings.id ORDER BY created_at DESC LIMIT 1), 0) AS sold'),
+            'cities.title AS city_title',
+            'districts.ilce_title AS county_title',
+            'neighborhoods.mahalle_title AS neighborhood_title',
+            DB::raw('(SELECT discount_amount FROM offers WHERE housing_id = housings.id AND type = "housing" AND start_date <= NOW() AND end_date >= NOW() LIMIT 1) as discount_amount'),
+        ])
+        ->leftJoin('housing_types', 'housing_types.id', '=', 'housings.housing_type_id')
+        ->leftJoin('project_list_items', function ($join) {
+            $join->on('project_list_items.housing_type_id', '=', 'housings.housing_type_id')
+                ->where('project_list_items.item_type', 2);
+        })
+        ->leftJoin('housing_status', 'housings.status_id', '=', 'housing_status.id')
+        ->leftJoin('cities', 'cities.id', '=', 'housings.city_id')
+        ->leftJoin('districts', 'districts.ilce_key', '=', 'housings.county_id')
+        ->leftJoin('neighborhoods', 'neighborhoods.mahalle_id', '=', 'housings.neighborhood_id')
+        ->where('housings.status', 1)
+        ->orderByDesc('housings.created_at')
+        ->limit(1)
+        ->get();
+    
 
             dd($secondhandHousings);
 
