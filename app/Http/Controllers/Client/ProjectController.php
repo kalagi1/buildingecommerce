f96a520500d3c4a5c99283394edc76dad4c69458
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-06-12 13:19:12 +0300

    "Modified ProjectController: removed JSON_EXTRACT condition, added whereNotNull('housings.owner_id') condition"

--- app/Http/Controllers/Client/ProjectController.php
+++ app/Http/Controllers/Client/ProjectController.php
@@ -597,7 +597,8 @@ class ProjectController extends Controller
                 ->leftJoin('districts', 'districts.ilce_key', '=', 'housings.county_id')
                 ->leftJoin('neighborhoods', 'neighborhoods.mahalle_id', '=', 'housings.neighborhood_id')
                 ->where('housings.status', 1)
-                ->whereRaw('JSON_EXTRACT(housings.housing_type_data, "$.open_sharing1") IS NOT NULL')
+                ->whereNotNull('housings.owner_id') 
+                // ->whereRaw('JSON_EXTRACT(housings.housing_type_data, "$.open_sharing1") IS NOT NULL')
                 ->where('project_list_items.item_type', 2)
                 ->orderByDesc('housings.created_at')
                 ->get();
