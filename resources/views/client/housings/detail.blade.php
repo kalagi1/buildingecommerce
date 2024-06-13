e8b4165fe0d7e8c2f6eee2337b328ed1fa8e1d90
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-06-13 09:58:51 +0300

    "Updated modal title text from 'Onayla' to 'Fiyatı Onayla' in detail.blade.php"

--- resources/views/client/housings/detail.blade.php
+++ resources/views/client/housings/detail.blade.php
@@ -757,7 +757,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="confirmationModalLabel">Onayla</h5>
+                <h5 class="modal-title" id="confirmationModalLabel">Fiyatı Onayla</h5>
                 <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
