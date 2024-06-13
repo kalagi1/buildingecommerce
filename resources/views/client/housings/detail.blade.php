5ab9e45ee742950febd8eca47e1ade1bf801a8c6
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-06-13 16:20:43 +0300

    "Updated button style and added CSS for 'Pazarlık Yap' button in housing detail page"

--- resources/views/client/housings/detail.blade.php
+++ resources/views/client/housings/detail.blade.php
@@ -635,13 +635,16 @@
 
                         @endif
 
-                        <!-- Teklif Gönderme Butonu -->
-                        <button type="button" class="btn btn-primary" data-bs-toggle="modal"
-                            data-bs-target="#bidModal">
-                            Pazarlık Yap
-                            <i class="fa fa-handshake"></i>
-                        </button>
 
+                        <button class="cta"  data-bs-toggle="modal"
+                        data-bs-target="#bidModal">
+                            <span>      Pazarlık Yap   <i class="fa fa-handshake"></i></span>
+                            <svg width="15px" height="10px" viewBox="0 0 13 10">
+                              <path d="M1,5 L11,5"></path>
+                              <polyline points="8 1 12 5 8 9"></polyline>
+                            </svg>
+                          </button>
+                          
                         @if (isset(json_decode($housing->housing_type_data)->open_sharing1[0]))
                             <div class="add-to-collections-wrapper addCollection" data-type='housing'
                                 data-id="{{ $housing->id }}">
@@ -3288,5 +3291,64 @@
                 margin-bottom: 30px !important;
             }
         }
+
+        .cta {
+  position: relative;
+  margin: auto;
+  padding: 12px 18px;
+  transition: all 0.2s ease;
+  border: none;
+  background: none;
+  cursor: pointer;
+}
+
+.cta:before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: block;
+  border-radius: 50px;
+  background: #b1dae7;
+  width: 45px;
+  height: 45px;
+  transition: all 0.3s ease;
+}
+
+.cta span {
+  position: relative;
+  font-family: "Ubuntu", sans-serif;
+  font-size: 18px;
+  font-weight: 700;
+  letter-spacing: 0.05em;
+  color: #234567;
+}
+
+.cta svg {
+  position: relative;
+  top: 0;
+  margin-left: 10px;
+  fill: none;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  stroke: #234567;
+  stroke-width: 2;
+  transform: translateX(-5px);
+  transition: all 0.3s ease;
+}
+
+.cta:hover:before {
+  width: 100%;
+  background: #b1dae7;
+}
+
+.cta:hover svg {
+  transform: translateX(0);
+}
+
+.cta:active {
+  transform: scale(0.95);
+}
+
     </style>
 @endsection
