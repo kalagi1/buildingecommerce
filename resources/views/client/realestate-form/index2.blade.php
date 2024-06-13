b03a09eb3b9086de4afd3733e08dadcf39c1bba9
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-06-13 11:21:52 +0300

    "Updated modal ID and label from 'sellTypeModal' to 'priceUpdateModal' in realestate-form/index2.blade.php"

--- resources/views/client/realestate-form/index2.blade.php
+++ resources/views/client/realestate-form/index2.blade.php
@@ -78,12 +78,12 @@
     </section>
 
         <!-- Onay Modal -->
-        <div class="modal fade" id="sellTypeModal" tabindex="-1" role="dialog" aria-labelledby="sellTypeModalLabel"
+        <div class="modal fade" id="#priceUpdateModal" tabindex="-1" role="dialog" aria-labelledby="#priceUpdateModalLabel"
         aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="sellTypeModalLabel">Satış Tipi Seçimi</h5>
+                    <h5 class="modal-title" id="priceUpdateModalLabel">Satış Tipi Seçimi</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
