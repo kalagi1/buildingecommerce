6fb53728d139b573ff2c3ccaa5e26f565b1d3b0c
MusaKartal <musakartal71@gmail.com>
2024-06-13 12:50:38 +0300

    google map yönlendirilmesi tamamlandı

--- resources/views/client/projects/index.blade.php
+++ resources/views/client/projects/index.blade.php
@@ -1025,7 +1025,13 @@
                         </div>
                         <div class="tab-pane fade  blog-info details mb-30" id="map" role="tabpanel"
                             aria-labelledby="contact-tab">
-                            <div id="mapContainer" style="height: 100%"></div>
+                            <iframe
+                            width="100%"
+                            height="100%"
+                            frameborder="0" style="border:0;"
+                            src="https://www.google.com/maps/embed/v1/place?key=AIzaSyB-ip8tV3D9tyRNS8RMUwxU8n7mCJ9WCl0&q={{ explode(',', $project->location)[0] }},{{ explode(',', $project->location)[1] }}"
+                            allowfullscreen="">
+                        </iframe>
                         </div>
                         <div class="tab-pane fade blog-info details mb-30" id="situation" role="tabpanel"
                             aria-labelledby="situation-tab">
