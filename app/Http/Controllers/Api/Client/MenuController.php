6ac7ec2bb35f8a1f57c7089ef8a489c1ba08c7e6
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-06-20 17:10:48 +0300

    "Refactored MenuController to use Menu model instead of HousingTypeParent, updated getMenuList and getSubmenus methods accordingly."

--- app/Http/Controllers/Api/Client/MenuController.php
+++ app/Http/Controllers/Api/Client/MenuController.php
@@ -4,37 +4,35 @@ namespace App\Http\Controllers\Api\Client;
 
 use App\Http\Controllers\Controller;
 use App\Models\HousingStatus;
-use App\Models\HousingTypeParent;
 use App\Models\Menu;
 use App\Models\Project;
 use Illuminate\Http\Request;
 
 class MenuController extends Controller
 {
-    public function getMenuList()
-    {
-
-        $items = HousingTypeParent::with("connections.housingType")->where("parent_id", null)->get();
-        return $items;
+    public function getMenuList(){
+        
+        $menuList = Menu::all();
         $formattedMenu = [];
-
-        foreach ($items as $menu) {
-            $formattedMenu[$menu->id] = [
-                'id' => $menu->id,
-                'href' => $menu->href,
-                'text' => $menu->text,
-                'target' => $menu->target,
-                'submenus' => $this->getSubmenus($items, $menu->id)
-            ];
+    
+        foreach ($menuList as $menu) {
+            if ($menu->parent_id === null) {
+                $formattedMenu[$menu->id] = [
+                    'id' => $menu->id,
+                    'href' => $menu->href,
+                    'text' => $menu->text,
+                    'target' => $menu->target,
+                    'submenus' => $this->getSubmenus($menuList, $menu->id)
+                ];
+            }
         }
-
+    
         return response()->json(array_values($formattedMenu));
     }
-
-    private function getSubmenus($menuList, $parentId)
-    {
+    
+    private function getSubmenus($menuList, $parentId) {
         $submenus = [];
-
+    
         foreach ($menuList as $menu) {
             if ($menu->parent_id == $parentId) {
                 $submenus[] = [
@@ -42,12 +40,13 @@ class MenuController extends Controller
                     'href' => $menu->href,
                     'text' => $menu->text,
                     'target' => $menu->target,
-                    'parent_id' => $menu->parent_id,
+                    'parent_id' =>$menu->parent_id,
                     'submenus' => $this->getSubmenus($menuList, $menu->id)
                 ];
             }
         }
-
+    
         return $submenus;
     }
+    
 }
