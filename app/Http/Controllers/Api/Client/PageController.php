4baece0836e564d45897499b72921b7b47f549ea
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-06-21 16:35:47 +0300

    "Added return statement to output SQL query in PageController.php"

--- app/Http/Controllers/Api/Client/PageController.php
+++ app/Http/Controllers/Api/Client/PageController.php
@@ -921,11 +921,12 @@ class PageController extends Controller
 
                         if (isset($values['max'])) {
                             $maxValue = str_replace('.', '', $values['max']); 
-                            return $maxValue;
                             $query->whereRaw('CAST(JSON_UNQUOTE(JSON_EXTRACT(housing_type_data, "$.' . $maxValue . '[0]")) AS FLOAT) <= ?', [$request->input($key)]);
                         }
                     }
                 }
+
+                return $query->toSql();
 
                 if ($countyID || $request->input("selectedCounty")) {
                     $query->where('county_id', $countyID ?? $request->input("selectedCounty"));
