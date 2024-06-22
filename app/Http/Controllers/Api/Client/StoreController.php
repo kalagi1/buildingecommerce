412c54adcf3aa24cee690ef271da2d6da4f58469
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-06-22 16:46:20 +0300

    "Added 'corporate_type' filter to getFeaturedStores method in StoreController"

--- app/Http/Controllers/Api/Client/StoreController.php
+++ app/Http/Controllers/Api/Client/StoreController.php
@@ -7,12 +7,10 @@ use App\Models\Project;
 use App\Models\User;
 use Illuminate\Http\Request;
 
-class StoreController extends Controller
-{
-    public function getFeaturedStores()
-    {
-        $brands = User::where("type", "2")->where("status", "1")->where("is_show", "yes")->where("corporate_account_status", "1")->orderBy("order", "asc")->get();
+class StoreController extends Controller {
+    public function getFeaturedStores() {
+        $brands = User::where( 'type', '2' )->where( 'corporate_type', 'İnşaat Ofisi' )->where( 'status', '1' )->where( 'is_show', 'yes' )->where( 'corporate_account_status', '1' )->orderBy( 'order', 'asc' )->get();
 
-        return response()->json($brands);
+        return response()->json( $brands );
     }
 }
