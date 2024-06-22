2db75ffce6a077a16f8833b170c2e86d233069ae
kalagi1 <islamoglu.abd@gmail.com>
2024-06-22 15:21:22 +0300

    apodev

---
+++ app/Http/Controllers/Api/Client/PayController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api\Client;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PayController extends Controller
+{
+    public function pay(){
+        return "asd";
+    }
+}
