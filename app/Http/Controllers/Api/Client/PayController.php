79d04f1b860163ef03ab3c56e50e2ac69a0104c0
kalagi1 <islamoglu.abd@gmail.com>
2024-06-22 17:45:31 +0300

    apodev

--- app/Http/Controllers/Api/Client/PayController.php
+++ app/Http/Controllers/Api/Client/PayController.php
@@ -37,6 +37,7 @@ class PayController extends Controller
         $transaction = $this->createTransaction();
 
         $data = $this->preparePaymentData($requestData, 1, $amount, $transaction);
+        dd($data);
         return view('payment.pay-mobil', $data);
     }
 
