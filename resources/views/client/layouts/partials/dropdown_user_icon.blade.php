2202a2a2fdc9f2357e53765bedf58221624545a4
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-06-27 11:32:42 +0300

    "Commented out two lines in dropdown_user_icon.blade.php"

--- resources/views/client/layouts/partials/dropdown_user_icon.blade.php
+++ resources/views/client/layouts/partials/dropdown_user_icon.blade.php
@@ -2,8 +2,8 @@
 
 <div class="dropdown hover">
     <a href="javascript:void()" class="userIcon">
-        @include('client.layouts.partials.user_icon', ['text' => $mainLink])
-        <i class="fa fa-angle-down pl-1"></i>
+        {{-- @include('client.layouts.partials.user_icon', ['text' => $mainLink]) --}}
+        {{-- <i class="fa fa-angle-down pl-1"></i> --}}
 
         <ul>
             @foreach ($links as $link)
