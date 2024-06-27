cd0561236f477679db579269bf1ae085ea413e53
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-06-27 10:21:04 +0300

    Remove unnecessary return statement in show method of UserController.

--- app/Http/Controllers/Api/Institutional/UserController.php
+++ app/Http/Controllers/Api/Institutional/UserController.php
@@ -29,7 +29,6 @@ class UserController extends Controller
 
     public function show(Request $request, User $user)
     {
-        return auth()->user()->id;
         // Kullanıcının rollerine ve izinlerine göre permissions dizisini oluştur
         $permissions = $user->role->rolePermissions->flatMap(function ($rolePermission) {
             return $rolePermission->permissions->pluck('key');
