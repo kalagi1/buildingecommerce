2760c58d54039451dd5c939ef250bc5ef4539e99
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-06-27 11:33:14 +0300

    "Uncommented user icon include and added icon to dropdown links"

--- resources/views/client/layouts/partials/dropdown_user_icon.blade.php
+++ resources/views/client/layouts/partials/dropdown_user_icon.blade.php
@@ -2,12 +2,12 @@
 
 <div class="dropdown hover">
     <a href="javascript:void()" class="userIcon">
-        {{-- @include('client.layouts.partials.user_icon', ['text' => $mainLink]) --}}
-        {{-- <i class="fa fa-angle-down pl-1"></i> --}}
+        @include('client.layouts.partials.user_icon', ['text' => $mainLink])
+        <i class="fa fa-angle-down pl-1"></i>
 
         <ul>
             @foreach ($links as $link)
-                <li><a href="{{ $link['url'] }}"> {{ $link['text'] }}</a></li>
+                <li><a href="{{ $link['url'] }}"><i class="{{ $link['icon'] }}"></i> {{ $link['text'] }}</a></li>
             @endforeach
         </ul>
     </a>
