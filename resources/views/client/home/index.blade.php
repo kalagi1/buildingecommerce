0bcb770560bdd819273487973db20ca9e9b82896
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-07-08 15:34:30 +0300

    update

--- resources/views/client/home/index.blade.php
+++ resources/views/client/home/index.blade.php
@@ -114,7 +114,7 @@
                 </div>
                 <div class="slick-lancers">
                     <div class="agents-grid" data-aos="fade-up" data-aos-delay="150">
-                        <a href="https://emlaksepette.com/kategori/al-sat-acil" class="homes-img">
+                        <a href="https://test.emlaksepette.com/kategori/al-sat-acil" class="homes-img">
                             <div class="landscapes">
                                 <div class="project-single">
                                     <div class="project-inner project-head">
@@ -300,7 +300,7 @@
                     <div class="section-title">
                         <h2>Öne Çıkan Projeler</h2>
                     </div>
-                    <a href="https://emlaksepette.com/kategori/tum-projeler" style="font-size: 11px;">
+                    <a href="https://test.emlaksepette.com/kategori/tum-projeler" style="font-size: 11px;">
                         <button style="background-color: #ea2a28; color: white; padding: 5px 10px; border: none;"
                             class="w-100">
                             Tüm Projeleri Gör
@@ -334,7 +334,7 @@
                             <h2>Emlak İlanları</h2>
                         </div>
                         <div>
-                            <a href="https://emlaksepette.com/kategori/emlak-ilanlari" style="font-size: 11px;">
+                            <a href="https://test.emlaksepette.com/kategori/emlak-ilanlari" style="font-size: 11px;">
                                 <button style="background-color: #ea2a28; color: white; padding: 5px 10px; border: none;"
                                     class="w-100">
                                     Tümünü Gör
