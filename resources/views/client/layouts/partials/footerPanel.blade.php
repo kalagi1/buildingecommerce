6668d576b2f97ff4cad9b4d78c58e007a86a255c
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-07-08 15:46:23 +0300

    update

--- resources/views/client/layouts/partials/footerPanel.blade.php
+++ resources/views/client/layouts/partials/footerPanel.blade.php
@@ -2031,7 +2031,9 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.6.0/leaflet.js"></script>
 <script src="https://www.jqueryscript.net/demo/leaflet-location-picker/src/leaflet-locationpicker.js"></script>
 
-@vite('resources/js/app.jsx')
+<script src="{{URL::to('/')}}/build/assets/app-8c284c27.js"></script>
+
+{{-- @vite('resources/js/app.jsx') --}}
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.6.0/leaflet.js"></script>
 <script src="https://www.jqueryscript.net/demo/leaflet-location-picker/src/leaflet-locationpicker.js"></script>
