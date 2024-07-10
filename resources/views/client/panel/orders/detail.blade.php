008ce84d39aee611621b208b636987d29f877588
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-07-10 11:38:39 +0300

    "Updated SVG width and height in order-header-image, and fixed CSS syntax errors in order-header styles"

--- resources/views/client/panel/orders/detail.blade.php
+++ resources/views/client/panel/orders/detail.blade.php
@@ -62,7 +62,7 @@
                 <div class="order-details">
                     <div class="order-header">
                         <div class="order-header-image">
-                            <svg viewBox="0 0 24 24" width="18" height="18" stroke="#FFFFFF" stroke-width="2" fill="#FFFFFF" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
+                            <svg viewBox="0 0 24 24" width="16" height="16" stroke="#FFFFFF" stroke-width="2" fill="#FFFFFF" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
                         </div>
                         <h3>#{{ $order->id }} Nolu Sipariş Detayı</h3>
                     </div>
@@ -1277,11 +1277,11 @@
             height:30px;
             display:flex;
             align-items:center;
-            justify-content: : center;
-    margin-right: 10px;
-    color: white;
-    background-color: black;
-    border-radius: 50%;
+            justify-content: center;
+            margin-right: 10px;
+            color: white;
+            background-color: black;
+            border-radius: 50%;
         }
         .order-header h3 {
             margin: 0;
