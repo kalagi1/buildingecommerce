0d3f6fd2d0c57fb69d57fda07efbeafca4f41a50
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-07-10 09:52:25 +0300

    "Refactored orders list template, updated filter button and spinner styles, and modified JavaScript code for filtering and popover functionality."

--- resources/views/client/panel/orders/get.blade.php
+++ resources/views/client/panel/orders/get.blade.php
@@ -5,7 +5,7 @@
         <div class="table-breadcrumb">
             <ul>
                 <li>Hesabım</li>
-                <li> Siparişlerim</li>
+                <li>Siparişlerim</li>
             </ul>
         </div>
     </div>
@@ -20,156 +20,19 @@
             <div class="d-flex align-items-center">
                 <input type="date" id="startDate" class="form-control mr-3">
                 <input type="date" id="endDate" class="form-control">
-                <button id="filterButton" class="btn btn-primary ml-3">Filtrele</button>
+                <button id="filterButton" class="btn btn-outline-primary ml-3">
+                    <svg viewBox="0 0 24 24" width="18" height="18" stroke="currentColor" stroke-width="2"
+                        fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1">
+                        <polygon points="22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3"></polygon>
+                    </svg>Filtrele</button>
             </div>
         </div>
 
         <div id="user-list-table-body">
             <div class="spinner-border text-danger d-none" role="status">
-                <span class="visually-hidden"></span>
+                <span class="visually-hidden">Loading...</span>
             </div>
             @include('client.panel.orders_list', ['cartOrders' => $cartOrders])
-
-            {{-- @foreach ($cartOrders as $order)
-                @php
-                    $o = json_decode($order->cart);
-                    $project =
-                        $o->type == 'project'
-                            ? App\Models\Project::with('roomInfo')
-                                ->where('id', $o->item->id)
-                                ->first()
-                            : null;
-                    $tarih = date('d F Y', strtotime($order->created_at));
-                    $tarih = str_replace(
-                        [
-                            'January',
-                            'February',
-                            'March',
-                            'April',
-                            'May',
-                            'June',
-                            'July',
-                            'August',
-                            'September',
-                            'October',
-                            'November',
-                            'December',
-                        ],
-                        [
-                            'Ocak',
-                            'Şubat',
-                            'Mart',
-                            'Nisan',
-                            'Mayıs',
-                            'Haziran',
-                            'Temmuz',
-                            'Ağustos',
-                            'Eylül',
-                            'Ekim',
-                            'Kasım',
-                            'Aralık',
-                        ],
-                        $tarih,
-                    );
-                @endphp
-                <div class="project-table-content user-item">
-                    <ul @if ($order->refund && $order->refund->status == 1) class="table-danger" @endif>
-                        @php
-                            $orderCart = json_decode($order->cart, true);
-                        @endphp
-
-                        <li class="no"><span>#{{ $order->id }}</span></li>
-                        @php
-                        if ($order->store->profile_image) {
-                            $storeImage = url('storage/profile_images/' . $order->store->profile_image);
-                        } else {
-                            $initial = $order->store->name ? sultoupper(subsul($order->store->name, 0, 1)) : '';
-                            $storeImage = $initial;
-                        }
-                    @endphp
-
-                    <li class="sales_person align-middle white-space-nowrap">
-                        <a target="_blank"
-                            href="{{ route('institutional.dashboard', ['slug' => $order->store->name, 'userID' => $order->store->id]) }}"
-                            class="d-flex align-items-center text-body">
-                            <div class="avatar avatar-m"><img class="rounded-circle" src="{{ $storeImage }}" alt=""></div>
-                        </a>
-                    </li>
-
-                        <li class="order_amount">{{ number_format(floatval(str_replace('.', '', $order->amount)), 0, ',', '.') }} ₺</li>
-                        <li class="order_date">{{ $tarih }}</li>
-
-                        <li class="order_project">
-                            <span>
-                                <strong>İlan No:
-                                    <a target="_blank"
-                                        href="{{ $orderCart['type'] == 'housing'
-                                            ? route('housing.show', [
-                                                'housingSlug' => $orderCart['item']['slug'],
-                                                'housingID' => $orderCart['item']['id'] + 2000000,
-                                            ])
-                                            : route('project.housings.detail', [
-                                                'projectSlug' =>
-                                                    optional(App\Models\Project::find($orderCart['item']['id']))->slug .
-                                                    '-' .
-                                                    optional(App\Models\Project::find($orderCart['item']['id']))->step2_slug .
-                                                    '-' .
-                                                    optional(App\Models\Project::find($orderCart['item']['id']))->housingtype->slug,
-                                                'projectID' => optional(App\Models\Project::find($orderCart['item']['id']))->id + 1000000,
-                                                'housingOrder' => $orderCart['item']['housing'],
-                                            ]) }}">
-                                        {{ $order->key }}
-                                    </a>
-                                </strong>
-                            </span>
-                        </li>
-
-                        @if ($order->refund != null)
-                            <li class="order_status">
-                                <span class="text-success">
-                                    {!! [
-                                        '0' => '<span class="badge badge-phoenix fs-10 badge-phoenix-warning"><span class="badge-label">İade Talebi Oluşturuldu</span><span class="ms-1" data-feather="alert-octagon" style="height:12.8px;width:12.8px;"></span></span>',
-                                        '1' => '<span class="badge badge-phoenix fs-10 badge-phoenix-info"><span class="badge-label">İade Talebi Onaylandı</span><span class="ms-1" data-feather="check" style="height:12.8px;width:12.8px;"></span></span>',
-                                        '2' => '<span class="badge badge-phoenix fs-10 badge-phoenix-danger"><span class="badge-label">İade Talebi Reddedildi</span><span class="ms-1" data-feather="x" style="height:12.8px;width:12.8px;"></span></span>',
-                                        '3' => '<span class="badge badge-phoenix fs-10 badge-phoenix-success"><span class="badge-label">Geri Ödeme Yapıldı</span><span class="ms-1" data-feather="check" style="height:12.8px;width:12.8px;"></span></span>',
-                                    ][$order->refund->status] !!}
-                                </span>
-                            </li>
-                        @else
-                            <li class="order_status">
-                                <span class="text-success">
-                                    {!! [
-                                        '0' => '<span class="badge badge-phoenix fs-10 badge-phoenix-warning"><span class="badge-label">Onay Bekleniyor</span><span class="ms-1" data-feather="alert-octagon" style="height:12.8px;width:12.8px;"></span></span>',
-                                        '1' => '<span class="badge badge-phoenix fs-10 badge-phoenix-success"><span class="badge-label">Ödeme Onaylandı</span><span class="ms-1" data-feather="check" style="height:12.8px;width:12.8px;"></span></span>',
-                                        '2' => '<span class="badge badge-phoenix fs-10 badge-phoenix-danger"><span class="badge-label">Ödeme Reddedildi</span><span class="ms-1" data-feather="x" style="height:12.8px;width:12.8px;"></span></span>',
-                                    ][$order->status] !!}
-                                </span>
-                                @if ($order->invoice && $order->status == 1)
-                                    <span class="badge badge-phoenix fs-10 badge-phoenix-success">
-                                        <a href="{{ route('institutional.invoice.show', $order->id) }}">
-                                            Faturayı Görüntüle
-                                        </a>
-                                    </span>
-                                @endif
-                            </li>
-                        @endif
-
-                        <li style="width: 5%;"><span class="project-table-content-actions-button" data-toggle="popover-{{ $order->id }}"><i class="fa fa-chevron-down"></i></span></li>
-                    </ul>
-                    <div class="popover-project-actions d-none" id="popover-{{ $order->id }}">
-                        <ul>
-                            @if ($order->invoice && $order->status == 1)
-                                <li>
-                                    <a href="{{ route('institutional.invoice.show', $order->id) }}">Faturayı Görüntüle</a>
-                                </li>
-                            @endif
-                            <li>
-                                <a href="{{ route('institutional.order.detail', ['order_id' => hash_id($order->id)]) }}">Sipariş Detayı</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            @endforeach --}}
         </div>
     </section>
 @endsection
@@ -181,8 +44,13 @@
                 filterOrders();
             });
 
+            $('#searchInput').on('input', function() {
+                filterOrders();
+            });
+
             function filterOrders() {
                 $(".spinner-border").removeClass("d-none");
+
                 var searchInput = $('#searchInput').val();
                 var startDate = $('#startDate').val();
                 var endDate = $('#endDate').val();
@@ -198,18 +66,17 @@
                     },
                     success: function(response) {
                         $(".spinner-border").addClass("d-none");
-
                         $('#user-list-table-body').html(response.html);
                     },
                     error: function(xhr) {
                         console.log(xhr.responseText);
+                        $(".spinner-border").addClass("d-none");
                     }
                 });
             }
-        });
-        $(document).ready(function() {
+
             // Toggle popovers
-            $('.project-table-content-actions-button').on('click', function() {
+            $(document).on('click', '.project-table-content-actions-button', function() {
                 var targetId = $(this).data('toggle');
                 var $popover = $('#' + targetId);
 
@@ -225,17 +92,6 @@
                 if (!$(event.target).closest('.project-table-content').length) {
                     $('.popover-project-actions').addClass('d-none');
                 }
-            });
-
-            // Filter orders
-            $('#searchInput').on('input', function() {
-                $('.project-table-content').each(function() {
-                    var orderText = $(this).text().toLowerCase();
-
-                    var isSearchMatch = orderText.includes(searchInput);
-
-                    $(this).toggle(isSearchMatch);
-                });
             });
         });
     </script>
@@ -286,14 +142,26 @@
         }
 
         #filterButton {
+            justify-content: center;
             height: 40px;
+            display: flex;
+            align-items: center;
         }
 
         .spinner-border.text-danger {
             margin: 0 auto;
             text-align: center;
-            position: relative;
+            position: absolute;
             left: 50%;
+            transform: translateX(-50%);
+        }
+
+        .popover-project-actions {
+            display: none;
+        }
+
+        .project-table-content-actions-button {
+            cursor: pointer;
         }
     </style>
 @endsection
