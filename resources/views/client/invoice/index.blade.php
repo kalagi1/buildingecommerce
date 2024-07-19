3c160529b05b3c9b229323f8279088568e8e4c83
zehrasenakgul <zehrasenakgul@gmail.com>
2024-07-19 14:45:37 +0300

    update

--- resources/views/client/invoice/index.blade.php
+++ resources/views/client/invoice/index.blade.php
@@ -158,7 +158,7 @@
                                                     </li>
                                                     <li><strong>Ödeme Şekli:</strong> EFT/HAVALE</li>
                                                     <li><strong>Web Adresi:</strong> <a
-                                                            href="http://127.0.0.1:8000">http://127.0.0.1:8000</a>
+                                                            href="https://private.emlaksepette.com">https://private.emlaksepette.com</a>
                                                     </li>
 
                                                 </ul>
