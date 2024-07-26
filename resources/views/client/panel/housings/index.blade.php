ade83086767ea79c757ed2235db8a15eb797929e
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-07-26 17:53:41 +0300

    update

--- resources/views/client/panel/housings/index.blade.php
+++ resources/views/client/panel/housings/index.blade.php
@@ -116,7 +116,7 @@
 
                                         <!-- Actions -->
                                         <li style="width: 5%">
-                                            <span class="project-table-content-actions-button" data-bs-content="#popover-{{ $housingType->id }}">
+                                            <span class="project-table-content-actions-button" data-toggle="#popover-{{ $housingType->id }}">
                                                 <i class="fa fa-chevron-down"></i>
                                             </span>
                                         </li>
@@ -204,6 +204,8 @@
             $('.project-table-content-actions-button').on('click', function() {
                 var targetId = $(this).data('toggle');
                 var $popover = $('#' + targetId);
+
+                console.log("sas");
 
                 // Hide other popovers
                 $('.popover-project-actions').not($popover).addClass('d-none');
