3077ae92df332ef867c9fbd1803257e220d9a10b
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-07-26 13:13:41 +0300

    update

--- resources/views/client/layouts/partials/dropdown_user_icon.blade.php
+++ resources/views/client/layouts/partials/dropdown_user_icon.blade.php
@@ -1,7 +1,6 @@
 <!-- partials/dropdown_user_icon.blade.php -->
 
 <div class="dropdown hover">
-    <a href="javascript:void()" class="userIcon">
         @include('client.layouts.partials.user_icon', ['text' => $mainLink])
         <i class="fa fa-angle-down pl-1"></i>
 
@@ -10,5 +9,4 @@
                 <li><a href="{{ $link['url'] }}"><i class="{{ $link['icon'] }}"></i> {{ $link['text'] }}</a></li>
             @endforeach
         </ul>
-    </a>
 </div>
