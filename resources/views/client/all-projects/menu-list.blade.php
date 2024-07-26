b29d8935a71e7404d2fc6cc09cd2dc3bd606ec8f
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-07-27 00:47:41 +0300

    update

--- resources/views/client/all-projects/menu-list.blade.php
+++ resources/views/client/all-projects/menu-list.blade.php
@@ -143,6 +143,14 @@
         }
     @endphp
 
+    @if ($secondhandHousings)
+    <div id="map-container" class="fullwidth-home-map header-map google-maps pull-top map-leaflet-wrapper">
+        <div id="map-leaflet"></div>
+    
+    </div>
+        
+    @endif
+
     <section class="properties-right list featured portfolio blog pt-5 bg-white">
         <div class="container">
 
@@ -2432,4 +2440,23 @@
             color: black;
         }
     </style>
+@endsection
+
+@section('scripts')
+<script src="{{asset('js/map-style2.js')}}"></script>
+<script src="{{ asset('js/leaflet.js') }}"></script>
+<script src="{{ asset('js/leaflet-gesture-handling.min.js') }}"></script>
+<script src="{{ asset('js/leaflet-providers.js') }}"></script>
+<script src="{{ asset('js/leaflet.markercluster.js') }}"></script>
+
+@endsection
+
+@section('styles')
+<link rel="stylesheet" href="{{asset('css/maps.css')}}">
+   <!-- LEAFLET MAP -->
+   <link rel="stylesheet" href="{{asset('css/leaflet.css')}}">
+   <link rel="stylesheet" href="{{asset('css/leaflet-gesture-handling.min.css')}}">
+   <link rel="stylesheet" href="{{asset('css/leaflet.markercluster.css')}}">
+   <link rel="stylesheet" href="{{asset('css/leaflet.markercluster.default.css')}}">
+
 @endsection
