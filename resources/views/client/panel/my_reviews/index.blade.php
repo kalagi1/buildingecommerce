12a060801d2b131d81400408b05bde3f3995d8a6
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-07-26 13:04:50 +0300

    update

--- resources/views/client/panel/my_reviews/index.blade.php
+++ resources/views/client/panel/my_reviews/index.blade.php
@@ -11,84 +11,80 @@
             </ul>
         </div>
     </div>
-        <div class="text-header p-0" id="yeniMusteriler" style="border-radius:7px;">
-            <div class="d-flex" style="justify-content: space-between;">
-                <input type="text" id="search-input-yeni" placeholder="Ara..." class="search-input form-control">
-            </div>
-
-            <div class="project-table-content user-item">
-                <ul style="gap: 20px">
-                    <li style="width: 0%;">No.</li>
-                    <li style="width: 10%;">İlan Adı</li>
-                    <li style="width: 10%;">İlan Numarası</li>
-                    <li style="width: 15%;">Yorum</li>
-                    <li style="width: 10%;">Değerlendirme</li>
-                    <li style="width: 10%;">Emlak / Proje</li>
-                    <li style="width: 10%;">Durum</li>
-                    <li style="width: 15%;">Tarih</li>
-                </ul>
-            </div>
-
-            <div id="commentTable">
-                @foreach ($all_comments as $key => $comment)
-                    <div class="project-table-content user-item">
-                        <ul style="gap: 20px">
-                            <li style="width: 0%;">{{ $key + 1 }}</li>
-                            <li style="width: 10%; align-items: flex-start;">
-                                @if ($comment->type == 'Emlak')
-                                    <span>{{ $comment->housing->name ?? 'N/A' }}</span>
-                                @else
-                                    <span>{{ $comment->project->project_title ?? 'N/A' }}</span>
-                                @endif
-                            </li>
-                            <li style="width: 10%; align-items: flex-start;">
-                                @if ($comment->type == 'Emlak')
-                                    <a
-                                        href="{{ route('project.housing.detail', ['slug' => $comment->housing->slug]) }}">{{ $comment->housing->id + 2000000 }}</a>
-                                @else
-                                    <a
-                                        href="{{ route('project.housing.detail', ['slug' => $comment->project->slug]) }}">{{ $comment->project->id + 1000000 }}</a>
-                                @endif
-                            </li>
-                            <li style="width: 15%; align-items: flex-start; word-wrap: break-word; word-break: break-all;">
-                                <span>{{ $comment->comment }}</span>
-                            </li>
-                            <li style="width: 10%; align-items: flex-start;">
-                                @if ($comment->rate)
-                                    @for ($i = 0; $i < $comment->rate; $i++)
-                                        <svg viewBox="0 0 14 14" class="widget-svg"
-                                            style="width: 10.89px; height: 10.89px;">
-                                            <path class="star"
-                                                d="M13.668 5.014a.41.41 0 0 1 .21.695l-3.15 3.235.756 4.53a.4.4 0 0 1-.376.5.382.382 0 0 1-.179-.046l-3.91-2.14-3.9 2.164a.372.372 0 0 1-.408-.03.41.41 0 0 1-.155-.397l.733-4.557-3.17-3.217a.415.415 0 0 1-.1-.415.396.396 0 0 1 .313-.277l4.368-.68L6.64.229A.386.386 0 0 1 6.986 0c.146 0 .281.087.348.226L9.3 4.364l4.368.65z"
-                                                style="fill: rgb(255, 192, 0);"></path>
-                                        </svg>
-                                    @endfor
-                                @endif
-                            </li>
-                            <li style="width: 10%; align-items: flex-start;">
-                                <span>{{ $comment->type }}</span>
-                            </li>
-                            <li style="width: 10%; align-items: flex-start;">
-                                @if ($comment->status == '1')
-                                    <span class="badge badge-success">Aktif</span>
-                                @else
-                                    <span class="badge badge-danger">Pasif</span>
-                                @endif
-                            </li>
-                            <li style="width: 15%; align-items: flex-start;">
-                                <span>{{ $comment->created_at->locale('tr')->isoFormat('D MMM, HH:mm') }}</span>
-                            </li>
-                        </ul>
-                    </div>
-                @endforeach
-            </div>
-            <div id="pagination-controls" class="d-flex"
-                style="margin-top: 20px; justify-content: space-between !important;">
-                <button id="prev-page" class="btn btn-primary" disabled>Önceki</button>
-                <span id="page-info"></span>
-                <button id="next-page" class="btn btn-primary">Sonraki</button>
-            </div>
+    <div class="text-header p-0" id="yeniMusteriler" style="border-radius:7px;">
+        <div class="d-flex" style="justify-content: space-between;">
+            <input type="text" id="search-input-yeni" placeholder="Ara..." class="search-input form-control">
         </div>
+
+        <div class="project-table-content user-item">
+            <ul style="gap: 20px">
+                <li style="width: 0%;">No.</li>
+                <li style="width: 10%;">İlan Adı</li>
+                <li style="width: 10%;">İlan Numarası</li>
+                <li style="width: 15%;">Yorum</li>
+                <li style="width: 10%;">Değerlendirme</li>
+                <li style="width: 10%;">Emlak / Proje</li>
+                <li style="width: 10%;">Durum</li>
+                <li style="width: 15%;">Tarih</li>
+            </ul>
+        </div>
+
+        <div id="commentTable">
+            @foreach ($all_comments as $key => $comment)
+                <div class="project-table-content user-item">
+                    <ul style="gap: 20px">
+                        <li style="width: 0%;">{{ $key + 1 }}</li>
+                        <li style="width: 10%; align-items: flex-start;">
+                            @if ($comment->type == 'Emlak')
+                                <span>{{ $comment->housing->name ?? 'N/A' }}</span>
+                            @else
+                                <span>{{ $comment->project->project_title ?? 'N/A' }}</span>
+                            @endif
+                        </li>
+                        <li style="width: 10%; align-items: flex-start;">
+                            @if ($comment->type == 'Emlak')
+                                {{ $comment->housing->id + 2000000 }}
+                            @else
+                                {{ $comment->project->id + 1000000 }}
+                            @endif
+                        </li>
+                        <li style="width: 15%; align-items: flex-start; word-wrap: break-word; word-break: break-all;">
+                            <span>{{ $comment->comment }}</span>
+                        </li>
+                        <li style="width: 10%; align-items: flex-start;">
+                            @if ($comment->rate)
+                                @for ($i = 0; $i < $comment->rate; $i++)
+                                    <svg viewBox="0 0 14 14" class="widget-svg" style="width: 10.89px; height: 10.89px;">
+                                        <path class="star"
+                                            d="M13.668 5.014a.41.41 0 0 1 .21.695l-3.15 3.235.756 4.53a.4.4 0 0 1-.376.5.382.382 0 0 1-.179-.046l-3.91-2.14-3.9 2.164a.372.372 0 0 1-.408-.03.41.41 0 0 1-.155-.397l.733-4.557-3.17-3.217a.415.415 0 0 1-.1-.415.396.396 0 0 1 .313-.277l4.368-.68L6.64.229A.386.386 0 0 1 6.986 0c.146 0 .281.087.348.226L9.3 4.364l4.368.65z"
+                                            style="fill: rgb(255, 192, 0);"></path>
+                                    </svg>
+                                @endfor
+                            @endif
+                        </li>
+                        <li style="width: 10%; align-items: flex-start;">
+                            <span>{{ $comment->type }}</span>
+                        </li>
+                        <li style="width: 10%; align-items: flex-start;">
+                            @if ($comment->status == '1')
+                                <span class="badge badge-success">Aktif</span>
+                            @else
+                                <span class="badge badge-danger">Pasif</span>
+                            @endif
+                        </li>
+                        <li style="width: 15%; align-items: flex-start;">
+                            <span>{{ $comment->created_at->locale('tr')->isoFormat('D MMM, HH:mm') }}</span>
+                        </li>
+                    </ul>
+                </div>
+            @endforeach
+        </div>
+        <div id="pagination-controls" class="d-flex" style="margin-top: 20px; justify-content: space-between !important;">
+            <button id="prev-page" class="btn btn-primary" disabled>Önceki</button>
+            <span id="page-info"></span>
+            <button id="next-page" class="btn btn-primary">Sonraki</button>
+        </div>
+    </div>
 @endsection
 
 @section('scripts')
