2e1c247ecb2ce9b442a57d56de74651f11e01b76
zehrasenakgul <zehrasenakgul@gmail.com>
2024-07-27 12:14:51 +0300

    update

--- resources/views/client/all-projects/menu-list.blade.php
+++ resources/views/client/all-projects/menu-list.blade.php
@@ -302,10 +302,13 @@
                                     </div>
                                     <div class="mt-md-2">
                                         <select id="county" class="bg-white filter-now mobile-button" >
+                                            <option value="#" class="selected" selected disabled>İlçe</option>
+
                                         </select>
                                     </div>
                                     <div class="mt-md-2">
-                                        <select id="neighborhood" class="bg-white filter-now mobile-button">
+                                        <select id="neighborhood" class="bg-white filter-now mobile-button">                                            <option value="#" class="selected" selected disabled>Mahalle</option>
+
                                         </select>
                                     </div>
                                 </div>
