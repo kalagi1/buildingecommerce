cfd87ad13c82beb81c18787be5f0222fb7abf23e
zehrasenakgul <zehrasenakgul@gmail.com>
2024-07-27 15:40:56 +0300

    update

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -2,53 +2,56 @@
 
 @section('content')
 <div style="background-color: ghostwhite;">
-<div class="container pb-5 pt-5">
-<div class="d-flex justify-content-between align-items-center mb-5">
-    <div class="table-breadcrumb">
-        <ul>
-            <li>Emlak Sepette</li>
-            <li> Şifremi Unuttum</li>
-        </ul>
-    </div>
-</div>
-<section>
-    <div class="single homes-content details mb-30">
-
-            @if (session('status'))
-            <div class="alert alert-success text-white" role="alert">
-                <span>{{ session('status') }}</span>
+    <div class="container pb-5 pt-5">
+        <div class="d-flex justify-content-between align-items-center mb-5">
+            <div class="table-breadcrumb">
+                <ul>
+                    <li>Emlak Sepette</li>
+                    <li>Şifremi Unuttum</li>
+                </ul>
             </div>
-        @endif
-        
-            <form method="POST" action="{{ route('password.email') }}">
-                @csrf
+        </div>
+        <section>
+            <div class="single homes-content details mb-30">
+                @if (session('status'))
+                    <div class="alert alert-success text-white" role="alert">
+                        <span>{{ session('status') }}</span>
+                    </div>
+                @endif
 
-                <div class=" mb-3">
-                    <label for="email" class="col-md-12 col-form-label text-md-end"><span>E-posta Adresi</span></label>
+                <form method="POST" action="{{ route('password.email') }}">
+                    @csrf
 
-                    <div class="col-md-12">
-                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">
+                            <span>E-posta Adresi</span>
+                        </label>
+                        <input 
+                            id="email" 
+                            type="email" 
+                            class="form-control @error('email') is-invalid @enderror" 
+                            name="email" 
+                            value="{{ old('email') }}" 
+                            required 
+                            autocomplete="email" 
+                            autofocus
+                        >
 
                         @error('email')
-                            <span class="invalid-feedback" role="alert">
+                            <div class="invalid-feedback">
                                 <strong>{{ $message }}</strong>
-                            </span>
+                            </div>
                         @enderror
                     </div>
-                </div>
 
-                <div class="mb-0">
-                    <div class="col-md-12">
+                    <div class="mb-0">
                         <button type="submit" class="btn btn-primary">
                             <span>Şifre Sıfırlama Bağlantısı Gönder</span>
                         </button>
                     </div>
-                </div>
-            </form>
-        </div>
+                </form>
+            </div>
+        </section>
     </div>
-</section>
 </div>
-</div>
-
 @endsection
