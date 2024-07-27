7fe0d753d7fc043deeb59fcb4ae912e883d98d2a
zehrasenakgul <zehrasenakgul@gmail.com>
2024-07-27 15:43:12 +0300

    update

--- resources/lang/tr/validation.php
+++ resources/lang/tr/validation.php
@@ -14,4 +14,9 @@ return [
             'max' => 'IBAN en fazla :max karakter olabilir.',
         ],
     ],
+    'reset' => 'Şifreniz sıfırlandı!',
+    'sent' => 'Şifre sıfırlama bağlantınız e-posta ile gönderildi!',
+    'throttled' => 'Lütfen tekrar denemeden önce bekleyin.',
+    'token' => 'Bu şifre sıfırlama kodu geçersiz.',
+    'user' => 'Bu e-posta adresine sahip bir kullanıcı bulunamadı.',
 ];
