54b9e29e11e95eed9efd2b5f4e6bb98602591bb5
zehrasenakgul <zehrasenakgul@gmail.com>
2024-07-27 15:36:41 +0300

    update

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,7 +1,7 @@
 @extends('client.layouts.master')
 
 @section('content')
-<div class="container">
+<div class="container pb-5 pt-5">
 <div class="d-flex justify-content-between align-items-center mb-5">
     <div class="table-breadcrumb">
         <ul>
