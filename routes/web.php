536ac82c09f9776ac3bd703bd579032230279f51
zehrasenakgul <zehrasenakgul@gmail.com>
2024-07-29 17:08:53 +0300

    update

--- routes/web.php
+++ routes/web.php
@@ -855,6 +855,8 @@ Route::put('/project/{id}/{room}/update-price', [ApiClientProjectController::cla
 
 Route::group(['prefix' => 'hesabim', "as" => "institutional.", 'middleware' => ['institutional', 'checkCorporateAccount', "checkHasClubAccount"]], function () {
     // Route::get('/danismana/musteri/atama',[InstitutionalCrmController::class,'assignConsultantCustomer'])->name('assign.consultant.customer');
+    Route::post('/reservation/approve/{reservation}', [AdminHomeController::class, 'approveReservation'])->name('approve-reservation');
+        Route::post('/reservation/unapprove/{reservation}', [AdminHomeController::class, 'unapproveReservation'])->name('unapprove-reservation');
 
 
     //sıfırdan crm rotaları
