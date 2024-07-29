ba33a8cae9059482c1cb37d0d0e1561bf8242a56
zehrasenakgul <zehrasenakgul@gmail.com>
2024-07-29 17:48:24 +0300

    update

--- resources/views/client/layouts/partials/headerPanel.blade.php
+++ resources/views/client/layouts/partials/headerPanel.blade.php
@@ -538,7 +538,7 @@
     <!-- Wrapper -->
     <div id="wrapper">
         <div class="slick-lancersl">
-            <div class="home-top-banner d-xl-block d-none d-lg-block" style="height:50px">
+            <div class="home-top-banner d-xl-block d-none d-lg-block" style="height:50px;background-color:#D32729">
                 <video autoplay loop muted style="width:100%;height:50px">
                     <source src="{{ asset('/emlaksepettegif.mp4') }}" type="video/mp4" style="height:50px;display:block">
                 </video>
