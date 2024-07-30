684f9ff408840feb86331acc6aa43e487f2cdd43
zehrasenakgul <zehrasenakgul@gmail.com>
2024-07-30 15:23:07 +0300

    update

--- app/Http/Controllers/Client/ProjectController.php
+++ app/Http/Controllers/Client/ProjectController.php
@@ -1568,7 +1568,6 @@ class ProjectController extends Controller
         $active = isset($active) ? 'active' : null;
         
  
-return $projectHousing;
         return view('client.projects.project_housing', compact('pageInfo', "blockName", "blockHousingOrder", "towns", "cities", "sumCartOrderQt", "bankAccounts", 'projectHousingsList', 'blockIndex', "parent", 'lastHousingCount', 'projectCartOrders', 'offer', 'endIndex', 'startIndex', 'currentBlockHouseCount', 'menu', 'project', 'housingOrder', 'projectHousingSetting', 'projectHousing', "statusSlug", "active"));
     }
 
