af61d4e8644e5cf9306993c06c581544d6517ead
zehrasenakgul <zehrasenakgul@gmail.com>
2024-07-30 13:37:08 +0300

    update

--- app/Http/Controllers/MarkerController.php
+++ app/Http/Controllers/MarkerController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\URL;
 
 class MarkerController extends Controller
 {
-    public function index()
+    public function index(Reqeust $request)
     {   function convertMonthToTurkishCharacter($date)
         {
             $aylar = [
