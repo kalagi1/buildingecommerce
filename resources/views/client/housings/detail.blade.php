63c9822b633b183da518b28498fc2c4b4cd3f200
zehrasenakgul <zehrasenakgul@gmail.com>
2024-07-31 15:28:31 +0300

    update

--- resources/views/client/housings/detail.blade.php
+++ resources/views/client/housings/detail.blade.php
@@ -597,7 +597,7 @@
                                                     <rect fill="none" height="50" width="50" />
                                                     <polygon fill="none"
                                                         points="25,3.553 30.695,18.321 46.5,19.173   34.214,29.152 38.287,44.447 25,35.848 11.712,44.447 15.786,29.152 3.5,19.173 19.305,18.321 "
-                                                        stroke="#000000" stroke-miterlimit="10" stroke-width="2" />
+                                                        stroke="#ebebeb" stroke-miterlimit="10" stroke-width="2" />
                                                 </svg>
                                                 <svg class="rating" enable-background="new 0 0 50 50" height="24px"
                                                     id="Layer_1" version="1.1" viewBox="0 0 50 50" width="24px"
@@ -606,7 +606,7 @@
                                                     <rect fill="none" height="50" width="50" />
                                                     <polygon fill="none"
                                                         points="25,3.553 30.695,18.321 46.5,19.173   34.214,29.152 38.287,44.447 25,35.848 11.712,44.447 15.786,29.152 3.5,19.173 19.305,18.321 "
-                                                        stroke="#000000" stroke-miterlimit="10" stroke-width="2" />
+                                                        stroke="#ebebeb" stroke-miterlimit="10" stroke-width="2" />
                                                 </svg>
                                                 <svg class="rating" enable-background="new 0 0 50 50" height="24px"
                                                     id="Layer_1" version="1.1" viewBox="0 0 50 50" width="24px"
@@ -615,7 +615,7 @@
                                                     <rect fill="none" height="50" width="50" />
                                                     <polygon fill="none"
                                                         points="25,3.553 30.695,18.321 46.5,19.173   34.214,29.152 38.287,44.447 25,35.848 11.712,44.447 15.786,29.152 3.5,19.173 19.305,18.321 "
-                                                        stroke="#000000" stroke-miterlimit="10" stroke-width="2" />
+                                                        stroke="#ebebeb" stroke-miterlimit="10" stroke-width="2" />
                                                 </svg>
                                                 <svg class="rating" enable-background="new 0 0 50 50" height="24px"
                                                     id="Layer_1" version="1.1" viewBox="0 0 50 50" width="24px"
@@ -624,7 +624,7 @@
                                                     <rect fill="none" height="50" width="50" />
                                                     <polygon fill="none"
                                                         points="25,3.553 30.695,18.321 46.5,19.173   34.214,29.152 38.287,44.447 25,35.848 11.712,44.447 15.786,29.152 3.5,19.173 19.305,18.321 "
-                                                        stroke="#000000" stroke-miterlimit="10" stroke-width="2" />
+                                                        stroke="#ebebeb" stroke-miterlimit="10" stroke-width="2" />
                                                 </svg>
                                                 <svg class="rating" enable-background="new 0 0 50 50" height="24px"
                                                     id="Layer_1" version="1.1" viewBox="0 0 50 50" width="24px"
@@ -633,7 +633,7 @@
                                                     <rect fill="none" height="50" width="50" />
                                                     <polygon fill="none"
                                                         points="25,3.553 30.695,18.321 46.5,19.173   34.214,29.152 38.287,44.447 25,35.848 11.712,44.447 15.786,29.152 3.5,19.173 19.305,18.321 "
-                                                        stroke="#000000" stroke-miterlimit="10" stroke-width="2" />
+                                                        stroke="#ebebeb" stroke-miterlimit="10" stroke-width="2" />
                                                 </svg>
                                             </div>
                                             <div class="ml-auto">
