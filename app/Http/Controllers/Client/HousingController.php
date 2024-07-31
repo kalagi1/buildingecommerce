1c86b055edbb6bad74b85eb1e5353942c0b14d66
zehrasenakgul <zehrasenakgul@gmail.com>
2024-07-31 16:40:51 +0300

    update

--- app/Http/Controllers/Client/HousingController.php
+++ app/Http/Controllers/Client/HousingController.php
@@ -17,51 +17,54 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Validator;
 
-class HousingController extends Controller {
-    public function alert() {
+class HousingController extends Controller
+{
+    public function alert()
+    {
 
-        $secondhandHousings =  Housing::with( 'images' )
-        ->select(
-            'housings.id',
-            'housings.slug',
-            'housings.title AS housing_title',
-            'housings.created_at',
-            'housings.step1_slug',
-            'housings.step2_slug',
-            'housing_types.title as housing_type_title',
-            'housings.housing_type_data',
-            'project_list_items.column1_name as column1_name',
-            'project_list_items.column2_name as column2_name',
-            'project_list_items.column3_name as column3_name',
-            'project_list_items.column4_name as column4_name',
-            'project_list_items.column1_additional as column1_additional',
-            'project_list_items.column2_additional as column2_additional',
-            'project_list_items.column3_additional as column3_additional',
-            'project_list_items.column4_additional as column4_additional',
-            'housings.address',
-            \Illuminate\Support\Facades\DB::raw( '(SELECT status FROM cart_orders WHERE JSON_EXTRACT(cart, "$.type") = "housing" AND JSON_EXTRACT(cart, "$.item.id") = housings.id) AS sold' ),
-            'cities.title AS city_title',
-            'districts.ilce_title AS county_title',
-            'neighborhoods.mahalle_title AS neighborhood_title',
-            DB::raw( '(SELECT discount_amount FROM offers WHERE housing_id = housings.id AND type = "housing" AND start_date <= "' . date( 'Y-m-d H:i:s' ) . '" AND end_date >= "' . date( 'Y-m-d H:i:s' ) . '") as discount_amount' ),
-        )
-        ->leftJoin( 'housing_types', 'housing_types.id', '=', 'housings.housing_type_id' )
-        ->leftJoin( 'project_list_items', 'project_list_items.housing_type_id', '=', 'housings.housing_type_id' )
-        ->leftJoin( 'housing_status', 'housings.status_id', '=', 'housing_status.id' )
-        ->leftJoin( 'cities', 'cities.id', '=', 'housings.city_id' )
-        ->leftJoin( 'districts', 'districts.ilce_key', '=', 'housings.county_id' )
-        ->leftJoin( 'neighborhoods', 'neighborhoods.mahalle_id', '=', 'housings.neighborhood_id' )
-        ->where( 'housings.status', 1 )
-        ->where( 'project_list_items.item_type', 2 )
-        ->orderByDesc( 'housings.created_at' )
-        ->get();
+        $secondhandHousings =  Housing::with('images')
+            ->select(
+                'housings.id',
+                'housings.slug',
+                'housings.title AS housing_title',
+                'housings.created_at',
+                'housings.step1_slug',
+                'housings.step2_slug',
+                'housing_types.title as housing_type_title',
+                'housings.housing_type_data',
+                'project_list_items.column1_name as column1_name',
+                'project_list_items.column2_name as column2_name',
+                'project_list_items.column3_name as column3_name',
+                'project_list_items.column4_name as column4_name',
+                'project_list_items.column1_additional as column1_additional',
+                'project_list_items.column2_additional as column2_additional',
+                'project_list_items.column3_additional as column3_additional',
+                'project_list_items.column4_additional as column4_additional',
+                'housings.address',
+                \Illuminate\Support\Facades\DB::raw('(SELECT status FROM cart_orders WHERE JSON_EXTRACT(cart, "$.type") = "housing" AND JSON_EXTRACT(cart, "$.item.id") = housings.id) AS sold'),
+                'cities.title AS city_title',
+                'districts.ilce_title AS county_title',
+                'neighborhoods.mahalle_title AS neighborhood_title',
+                DB::raw('(SELECT discount_amount FROM offers WHERE housing_id = housings.id AND type = "housing" AND start_date <= "' . date('Y-m-d H:i:s') . '" AND end_date >= "' . date('Y-m-d H:i:s') . '") as discount_amount'),
+            )
+            ->leftJoin('housing_types', 'housing_types.id', '=', 'housings.housing_type_id')
+            ->leftJoin('project_list_items', 'project_list_items.housing_type_id', '=', 'housings.housing_type_id')
+            ->leftJoin('housing_status', 'housings.status_id', '=', 'housing_status.id')
+            ->leftJoin('cities', 'cities.id', '=', 'housings.city_id')
+            ->leftJoin('districts', 'districts.ilce_key', '=', 'housings.county_id')
+            ->leftJoin('neighborhoods', 'neighborhoods.mahalle_id', '=', 'housings.neighborhood_id')
+            ->where('housings.status', 1)
+            ->where('project_list_items.item_type', 2)
+            ->orderByDesc('housings.created_at')
+            ->get();
 
         return $secondhandHousings;
     }
 
-    public function sendComment( Request $request, $id ) {
-        
-        $housing = Housing::where( 'id', $id )->with( 'user' )->first();
+    public function sendComment(Request $request, $id)
+    {
+
+        $housing = Housing::where('id', $id)->with('user')->first();
         $validator = Validator::make(
             $request->all(),
             [
@@ -70,17 +73,17 @@ class HousingController extends Controller {
             ]
         );
 
-        if ( $validator->fails() ) {
-            return redirect()->back()->withErrors( $validator->errors() );
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator->errors());
         }
 
-        $rate = $request->input( 'rate' );
-        $comment = $request->input( 'comment' );
+        $rate = $request->input('rate');
+        $comment = $request->input('comment');
 
         $images = [];
-        if ( is_array( $request->images ) ) {
-            foreach ( $request->images as $image ) {
-                $images[] = $image->store( 'public/housing-comment-images' );
+        if (is_array($request->images)) {
+            foreach ($request->images as $image) {
+                $images[] = $image->store('public/housing-comment-images');
             }
         }
 
@@ -90,7 +93,7 @@ class HousingController extends Controller {
                 'housing_id' => $id,
                 'comment' => $comment,
                 'rate' => $rate,
-                'images' => json_encode( $images ),
+                'images' => json_encode($images),
                 'owner_id' => $housing->user_id,
             ]
         );
@@ -98,46 +101,51 @@ class HousingController extends Controller {
         return redirect()->back();
     }
 
-    public function show( $housingSlug, $housingID ) {
+    public function show($housingSlug, $housingID)
+    {
         $menu = Menu::getMenuItems();
         $bankAccounts = BankAccount::all();
         $cities = City::all();
 
         $realHousingID  = $housingID;
 
-        if ( intval( $housingID ) > 2000000 ) {
-            $realHousingID = intval( $housingID ) - 2000000;
-
+        if (intval($housingID) > 2000000) {
+            $realHousingID = intval($housingID) - 2000000;
         }
 
-        $housing = Housing::with( 'neighborhood', 'consultant.role', 'images', 'reservations', 'user.housings', 'user.banners', 'brand', 'city', 'county' )
-        ->where( 'id', $realHousingID )
-        ->where( 'status', 1 )->first();
+        $housing = Housing::with('neighborhood', 'consultant.role', 'images', 'reservations', 'user.housings', 'user.banners', 'brand', 'city', 'county')
+            ->where('id', $realHousingID)
+            ->where('status', 1)->first();
 
-        if ( $housing ) {
-            $housing->increment( 'views_count' );
+        if ($housing) {
+
+            if ($housing->is_sold != NULL) {
+                return redirect('/')
+                    ->with('error', 'İLAN SATILDI');
+            }
+            $housing->increment('views_count');
 
             $housingSetting = ProjectHouseSetting::all();
-            $housingComments = HousingComment::where( 'housing_id', $realHousingID )->where( 'status', 1 )->with( 'user' )->get();
+            $housingComments = HousingComment::where('housing_id', $realHousingID)->where('status', 1)->with('user')->get();
 
             $labels = [];
-            $housingTypeData = json_decode( $housing->housing_type_data, true );
+            $housingTypeData = json_decode($housing->housing_type_data, true);
 
-            $housingType = HousingType::find( $housing->housing_type_id );
+            $housingType = HousingType::find($housing->housing_type_id);
             foreach ($housingTypeData as $key => $value) {
                 if ($housingType) {
                     $formJsonItems = json_decode($housingType->form_json, true) ?? [];
-            
+
                     foreach ($formJsonItems as $formJsonItem) {
                         // Check if $formJsonItem is an array
                         if (is_array($formJsonItem)) {
                             // Proceed with operations on $formJsonItem
                             if (isset($formJsonItem['name'])) {
                                 $formJsonItemName = rtrim($formJsonItem['name'], '[]');
-                                
+
                                 // Remove the last character '1' if it exists in the key
                                 $keyWithoutLastCharacter = rtrim($key, '1');
-            
+
                                 // Check for equality after removing the last character
                                 if ($formJsonItemName === $keyWithoutLastCharacter) {
                                     // Ensure $formJsonItem has 'label' key before accessing it
@@ -151,8 +159,8 @@ class HousingController extends Controller {
                     }
                 }
             }
-            
-            
+
+
 
             $pageInfo = [
                 'meta_title' => $housing->title,
@@ -160,72 +168,73 @@ class HousingController extends Controller {
                 'meta_description' => 'Emlak Kulüpte ' . $housing->title . ' ile hayallerinizdeki konutu bulabilirsiniz. Geniş seçenekler, uygun fiyatlar ve konforlu yaşam sizi bekliyor!
                     Şimdi alım yapın.Geleceğe yatırım yapın.',
                 'meta_author' => 'Emlak Sepette',
-                'meta_image' => URL::to( '/' ) . '/' . 'housing_images/' .  json_decode( $housing->housing_type_data )->image,
+                'meta_image' => URL::to('/') . '/' . 'housing_images/' .  json_decode($housing->housing_type_data)->image,
 
             ];
 
-            $pageInfo = json_encode( $pageInfo );
-            $pageInfo = json_decode( $pageInfo );
-            $parent = HousingTypeParent::where( 'slug', $housing->step1_slug )->first();
+            $pageInfo = json_encode($pageInfo);
+            $pageInfo = json_decode($pageInfo);
+            $parent = HousingTypeParent::where('slug', $housing->step1_slug)->first();
 
-            return view( 'client.housings.detail', compact( 'pageInfo', 'housing', 'bankAccounts', 'parent', 'menu', 'housingSetting', 'housingID', 'housingComments', 'labels', 'cities' ) );
+            return view('client.housings.detail', compact('pageInfo', 'housing', 'bankAccounts', 'parent', 'menu', 'housingSetting', 'housingID', 'housingComments', 'labels', 'cities'));
         } else {
-            return redirect( '/' )
-            ->with( 'error', 'İlan yayından kaldırıldı veya bulunamadı.' );
+            return redirect('/')
+                ->with('error', 'İlan yayından kaldırıldı veya bulunamadı.');
         }
     }
 
-    public function list( Request $request ) {
+    public function list(Request $request)
+    {
         $housings = Housing::query();
-        if ( $request->input( 'search' ) ) {
-            $housings = $housings->where( 'title', 'LIKE', '%' . $request->input( 'search' ) . '%' );
+        if ($request->input('search')) {
+            $housings = $housings->where('title', 'LIKE', '%' . $request->input('search') . '%');
         }
 
-        if ( $request->input( 'city' ) ) {
-            $housings = $housings->where( 'city_id', $request->input( 'city' ) );
+        if ($request->input('city')) {
+            $housings = $housings->where('city_id', $request->input('city'));
         }
 
-        if ( $request->input( 'housing_type' ) ) {
-            $housings = $housings->where( 'housing_type_id', $request->input( 'housing_type' ) );
+        if ($request->input('housing_type')) {
+            $housings = $housings->where('housing_type_id', $request->input('housing_type'));
         }
 
         $housings = $housings->get();
-        if ( $request->input( 'min-price' ) != '' && $request->input( 'max-price' ) != '' ) {
+        if ($request->input('min-price') != '' && $request->input('max-price') != '') {
             $housingsTemp = [];
-            $maxPrice = intval( str_replace( ',', '', $request->input( 'max-price' ) ) );
-            $minPrice = intval( str_replace( ',', '', $request->input( 'min-price' ) ) );
-            foreach ( $housings as $housing ) {
-                $housingTypeData = json_decode( $housing->housing_type_data );
-                $housingPrice = floatval( $housingTypeData->price[ 0 ] );
-                if ( $housingPrice >= $minPrice && $housingPrice <= $maxPrice ) {
-                    array_push( $housingsTemp, $housing );
+            $maxPrice = intval(str_replace(',', '', $request->input('max-price')));
+            $minPrice = intval(str_replace(',', '', $request->input('min-price')));
+            foreach ($housings as $housing) {
+                $housingTypeData = json_decode($housing->housing_type_data);
+                $housingPrice = floatval($housingTypeData->price[0]);
+                if ($housingPrice >= $minPrice && $housingPrice <= $maxPrice) {
+                    array_push($housingsTemp, $housing);
                 }
             }
 
             $housings = $housingsTemp;
         }
 
-        if ( $request->input( 'min-square-meters' ) != '' && $request->input( 'max-square-meters' ) != '' ) {
+        if ($request->input('min-square-meters') != '' && $request->input('max-square-meters') != '') {
             $housingsTemp = [];
-            $maxSquareMeters = intval( str_replace( ',', '', $request->input( 'max-square-meters' ) ) );
-            $minSquareMeters = intval( str_replace( ',', '', $request->input( 'min-square-meters' ) ) );
-            foreach ( $housings as $housing ) {
-                $housingTypeData = json_decode( $housing->housing_type_data );
-                $housingSquareMeters = floatval( $housingTypeData->squaremeters[ 0 ] );
-                if ( $housingSquareMeters >= $minSquareMeters && $housingSquareMeters <= $maxSquareMeters ) {
-                    array_push( $housingsTemp, $housing );
+            $maxSquareMeters = intval(str_replace(',', '', $request->input('max-square-meters')));
+            $minSquareMeters = intval(str_replace(',', '', $request->input('min-square-meters')));
+            foreach ($housings as $housing) {
+                $housingTypeData = json_decode($housing->housing_type_data);
+                $housingSquareMeters = floatval($housingTypeData->squaremeters[0]);
+                if ($housingSquareMeters >= $minSquareMeters && $housingSquareMeters <= $maxSquareMeters) {
+                    array_push($housingsTemp, $housing);
                 }
             }
             $housings = $housingsTemp;
         }
 
-        if ( $request->input( 'room_count' ) != '' ) {
+        if ($request->input('room_count') != '') {
             $housingsTemp = [];
-            foreach ( $housings as $housing ) {
-                $housingTypeData = json_decode( $housing->housing_type_data );
-                $housingRoomCount = $housingTypeData->room_count[ 0 ];
-                if ( $request->input( 'room_count' ) == $housingRoomCount ) {
-                    array_push( $housingsTemp, $housing );
+            foreach ($housings as $housing) {
+                $housingTypeData = json_decode($housing->housing_type_data);
+                $housingRoomCount = $housingTypeData->room_count[0];
+                if ($request->input('room_count') == $housingRoomCount) {
+                    array_push($housingsTemp, $housing);
                 }
             }
             $housings = $housingsTemp;
@@ -235,42 +244,44 @@ class HousingController extends Controller {
         $cities = City::get();
         $housingTypes = HousingType::get();
 
-        return view( 'client.housings.list', compact( 'housings', 'menu', 'cities', 'housingTypes' ) );
+        return view('client.housings.list', compact('housings', 'menu', 'cities', 'housingTypes'));
     }
 
-    public function updatePrice( Request $request, $id ) {
-        $housing = Housing::findOrFail( $id );
+    public function updatePrice(Request $request, $id)
+    {
+        $housing = Housing::findOrFail($id);
 
-        if ( Auth::check() && Auth::user()->id == $housing->user_id ) {
+        if (Auth::check() && Auth::user()->id == $housing->user_id) {
             // Request'ten gelen 'new_price' değerini al ve noktalardan arındır
             $newPrice = $request->input('new_price');
             $newPrice = str_replace('.', '', $newPrice);
-    
+
             $housingTypeData = json_decode($housing->housing_type_data, true);
             if ($housing->step2_slug == 'gunluk-kiralik') {
                 $housingTypeData['daily_rent'][0] = $newPrice;
             } else {
                 $housingTypeData['price'][0] = $newPrice;
             }
-                $housing->housing_type_data = json_encode($housingTypeData);
-                $housing->save();
+            $housing->housing_type_data = json_encode($housingTypeData);
+            $housing->save();
 
-                return redirect()->back()->with('success', 'Fiyat başarıyla güncellendi.İlanınız onay süreci için emlak sepette yönetimine iletilmiştir.');
-            } else {
-                return redirect()->back()->with('error', 'Fiyat güncellenirken bir hata oluştu.' );
+            return redirect()->back()->with('success', 'Fiyat başarıyla güncellendi.İlanınız onay süreci için emlak sepette yönetimine iletilmiştir.');
+        } else {
+            return redirect()->back()->with('error', 'Fiyat güncellenirken bir hata oluştu.');
         }
-
     }
 
-    public function getComment($id){
+    public function getComment($id)
+    {
         $comment = HousingComment::find($id);
         if (!$comment) {
             return response()->json(['error' => 'Yorum bulunamadı.'], 404);
         }
         return response()->json(['data' => $comment]);
-    }//End
+    } //End
 
-    public function updateComment(Request $request){
+    public function updateComment(Request $request)
+    {
         $validator = Validator::make($request->all(), [
             'id' => 'required|exists:project_comments,id',
             // 'rate' => 'required|integer|min:1|max:5',
@@ -292,6 +303,6 @@ class HousingController extends Controller {
         $comment->save();
 
         return response()->json(['message' => 'Yorumunuz admin onayladıktan sonra yayınlanacaktır.']);
-    }//End
+    } //End
 
 }
