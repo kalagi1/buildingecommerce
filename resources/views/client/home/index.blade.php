4dff42a398c14472911d1790364bf5f027d7f002
zehrasenakgul <zehrasenakgul@gmail.com>
2024-08-01 10:17:17 +0300

    update

--- resources/views/client/home/index.blade.php
+++ resources/views/client/home/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <meta name="description" content="Emlak Sepette">
     <meta name="author" content="Master Girişim">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
 
     <title>Emlak Sepette</title>
     <style>
