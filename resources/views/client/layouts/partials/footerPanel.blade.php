ebae7643b8a8921f940f1cab36faf63c9468c24f
zehrasenakgul <zehrasenakgul@gmail.com>
2024-08-01 11:00:37 +0300

    update

--- resources/views/client/layouts/partials/footerPanel.blade.php
+++ resources/views/client/layouts/partials/footerPanel.blade.php
@@ -261,6 +261,11 @@
 
 
 @include('cookie-consent::index')
+<script>
+    $(".accordion li").click(function () {
+    $(this).closest(".accordion").hasClass("one-open") ? ($(this).closest(".accordion").find("li").removeClass("active"), $(this).addClass("active")) : $(this).toggleClass("active"), "undefined" != typeof window.mr_parallax && setTimeout(mr_parallax.windowLoad, 500)
+});
+</script>
 
 <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
