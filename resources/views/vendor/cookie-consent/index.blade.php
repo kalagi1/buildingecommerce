a43b62d4bd0b0b503c3372e72de5e8e7cab2f06b
zehrasenakgul <zehrasenakgul@gmail.com>
2024-08-01 10:18:54 +0300

    update

--- resources/views/vendor/cookie-consent/index.blade.php
+++ resources/views/vendor/cookie-consent/index.blade.php
@@ -3,9 +3,7 @@
     @include('cookie-consent::dialogContents')
 
     <script>
-
         window.laravelCookieConsent = (function () {
-
             const COOKIE_VALUE = 1;
             const COOKIE_DOMAIN = '{{ config('session.domain') ?? request()->getHost() }}';
 
@@ -14,13 +12,21 @@
                 hideCookieDialog();
             }
 
+            function declineCookies() {
+                setCookie('{{ $cookieConsentConfig['cookie_name'] }}', 'declined', {{ $cookieConsentConfig['cookie_lifetime'] }});
+                hideCookieDialog();
+            }
+
+            function manageCookies() {
+                window.location.href = '{{ url('sayfa/cerez-yönetimi') }}'; // Çerez yönetim sayfası URL'si
+            }
+
             function cookieExists(name) {
                 return (document.cookie.split('; ').indexOf(name + '=' + COOKIE_VALUE) !== -1);
             }
 
             function hideCookieDialog() {
                 const dialogs = document.getElementsByClassName('js-cookie-consent');
-
                 for (let i = 0; i < dialogs.length; ++i) {
                     dialogs[i].style.display = 'none';
                 }
@@ -40,14 +46,25 @@
                 hideCookieDialog();
             }
 
-            const buttons = document.getElementsByClassName('js-cookie-consent-agree');
+            const agreeButtons = document.getElementsByClassName('js-cookie-consent-agree');
+            for (let i = 0; i < agreeButtons.length; ++i) {
+                agreeButtons[i].addEventListener('click', consentWithCookies);
+            }
 
-            for (let i = 0; i < buttons.length; ++i) {
-                buttons[i].addEventListener('click', consentWithCookies);
+            const declineButtons = document.getElementsByClassName('js-cookie-consent-decline');
+            for (let i = 0; i < declineButtons.length; ++i) {
+                declineButtons[i].addEventListener('click', declineCookies);
+            }
+
+            const manageButtons = document.getElementsByClassName('js-cookie-consent-manage');
+            for (let i = 0; i < manageButtons.length; ++i) {
+                manageButtons[i].addEventListener('click', manageCookies);
             }
 
             return {
                 consentWithCookies: consentWithCookies,
+                declineCookies: declineCookies,
+                manageCookies: manageCookies,
                 hideCookieDialog: hideCookieDialog
             };
         })();
