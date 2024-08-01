1684f33fb3fef41a4c8bc37e1629873c99800303
zehrasenakgul <zehrasenakgul@gmail.com>
2024-08-01 11:42:04 +0300

    update

--- resources/views/vendor/cookie-consent/dialogContents.blade.php
+++ resources/views/vendor/cookie-consent/dialogContents.blade.php
@@ -2,10 +2,16 @@
 <div id="cookie-management-modal"
     class="fixed inset-0 z-50 hidden flex items-center justify-center bg-black bg-opacity-50">
     <div class="bg-white rounded-lg shadow-lg p-6 w-11/12 max-w-lg">
-        <div class="d-flex justify-content-between align-items-center">
+        <div class="d-flex justify-content-between align-items-center mb-3">
             <h2 class="text-xl font-semibold mb-0 pb-0">Çerez Yönetimi</h2>
 
-            <button id="close-modal" class="px-4 py-2 bg-gray-300 hover:bg-gray-400 text-sm font-medium rounded-md border-none"><svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></button>
+            <button id="close-modal"
+                class="p-2 bg-gray-300 hover:bg-gray-400 text-sm font-medium rounded-md border-none"><svg
+                    viewBox="0 0 24 24" width="15" height="16" stroke="currentColor" stroke-width="2" fill="none"
+                    stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1">
+                    <line x1="18" y1="6" x2="6" y2="18"></line>
+                    <line x1="6" y1="6" x2="18" y2="18"></line>
+                </svg></button>
         </div>
         <p class="mb-4">Çerezlerinizin ayarlarını burada yönetebilirsiniz. Aşağıdaki seçenekleri kullanarak çerez
             tercihlerinizi güncelleyebilirsiniz:</p>
