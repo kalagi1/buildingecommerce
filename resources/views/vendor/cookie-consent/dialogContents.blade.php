121393364fb67e49d999284536936b73fb95cf89
zehrasenakgul <zehrasenakgul@gmail.com>
2024-08-01 14:06:15 +0300

    update

--- resources/views/vendor/cookie-consent/dialogContents.blade.php
+++ resources/views/vendor/cookie-consent/dialogContents.blade.php
@@ -127,7 +127,7 @@
                 </div>
 
                 <!-- Butonlar için flex-col ve md:flex-row sınıflarını kullanarak mobil ve masaüstü görünümünü ayarlayın -->
-                <div class="flex flex-col sm:flex-row gap-2 mt-2 justify-content-end">
+                <div class="d-flex flex-col sm:flex-row gap-2 mt-2 justify-content-end">
                     <button
                         class="js-cookie-consent-agree cookie-consent__agree w-full sm:w-auto cursor-pointer flex items-center justify-center px-4 py-2 rounded-md text-sm font-medium text-yellow-800 bg-yellow-400 hover:bg-yellow-300">
                         {{ trans('cookie-consent::texts.agree') }}
