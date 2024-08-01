0e6a87b45d44f0c9491782a05918ac763995b344
zehrasenakgul <zehrasenakgul@gmail.com>
2024-08-01 11:37:50 +0300

    update

--- resources/views/vendor/cookie-consent/dialogContents.blade.php
+++ resources/views/vendor/cookie-consent/dialogContents.blade.php
@@ -99,12 +99,13 @@
             </ul>
         </div>
 
+    </div>
+    
         <!-- Modal Kapatma Butonu -->
         <div class="mt-6 flex justify-end">
             <button id="close-modal"
                 class="px-4 py-2 bg-gray-300 hover:bg-gray-400 text-sm font-medium rounded-md">Kapat</button>
         </div>
-    </div>
 </div>
 
 
