a1f06f14079b1b8ec37cf159eada899394492148
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-08-02 17:41:17 +0300

    update

--- resources/js/define/variables.jsx
+++ resources/js/define/variables.jsx
@@ -1,9 +1,8 @@
 import { openDB } from "idb";
 
 
-export const baseUrl = "http://127.0.0.1:8000/react/";
-export const baseUrl = "https://private.emlaksepette.com/react/";
-export const frontEndUrl = "http://127.0.0.1:8000/";
+export const baseUrl = "https://emlaksepette.com/react/";
+export const frontEndUrl = "https://emlaksepette.com/";
 
 
 export const dotNumberFormat = (number) => {
--- resources/views/client/layouts/partials/footer.blade.php
+++ resources/views/client/layouts/partials/footer.blade.php
@@ -1868,7 +1868,7 @@
         $('body').on("click", ".toggle-favorite", toggleFavorite);
 
     });
-    const appUrl = "https://private.emlaksepette.com/"; // Uygulama URL'si
+    const appUrl = "https://emlaksepette.com/"; // Uygulama URL'si
     let timeout; // AJAX isteği için zamanlayıcı değişkeni
 
     function showSearchingMessage() {
@@ -2088,7 +2088,7 @@
     })
     'use strict';
     $(function() {
-        const appUrl = "https://private.emlaksepette.com/"; // Uygulama URL'si
+        const appUrl = "https://emlaksepette.com/"; // Uygulama URL'si
         let timeout; // AJAX isteği için zamanlayıcı değişkeni
 
         function showSearchingMessage() {
