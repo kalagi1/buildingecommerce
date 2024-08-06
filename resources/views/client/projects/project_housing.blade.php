39b5ce86babbb6d2952371add2d5d790e9193308
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-08-06 15:04:03 +0300

    update

--- resources/views/client/projects/project_housing.blade.php
+++ resources/views/client/projects/project_housing.blade.php
@@ -575,11 +575,17 @@
 
                     @if (
                         ($off_sale_2 && Auth::check() && $isUserType2EmlakOfisi && $canAddToProject) ||
-                            (!$off_sale_4 && Auth::check() && $canAddToProject) ||
+                            ($canAddToProject && Auth::check()) ||
+                            !$off_sale_4 ||
                             (!$off_sale_1 && Auth::check() && $canAddToProject) ||
                             ($off_sale_3 && (Auth::check() && ($isUserType2EmlakOfisi || $isUserType1)) && $canAddToProject))
-dsfdsfds
-                        @if (($sold && $sold->status == '2') || !$sold || $off_sale_1)
+
+{{$off_sale_1}}
+{{$off_sale_2}}
+{{$off_sale_3}}
+{{$off_sale_4}}
+
+@if (($sold && $sold->status == '2') || !$sold || $off_sale_1)
                             <div class="moveCollection mb-3">
                                 <div class="add-to-collections-wrapper addCollectionMobile addCollection"
                                     data-type='project' data-id="{{ $housingOrder }}"
