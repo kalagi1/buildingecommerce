7cad861c7af6cc4f1a7b4b8ade834b37d2d33eed
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-08-06 18:10:58 +0300

    update

--- resources/views/client/layouts/partials/headerPanel.blade.php
+++ resources/views/client/layouts/partials/headerPanel.blade.php
@@ -675,7 +675,7 @@
                                             auth()->user()->type != 3 &&
                                             auth()->user()->type != 21)
 
-                                            {{dd(auth()->user())}}
+                                            {{dd(auth()->user()->corporate_type)}}
                                         @include('client.layouts.partials.dropdown_user_icon', [
                                             'mainLink' => 'Hesabım',
                                             'links' => [
