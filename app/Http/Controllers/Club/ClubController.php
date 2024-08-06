118297425b6e72879780d660da9635e22402d0a6
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-08-06 17:01:29 +0300

    update

--- app/Http/Controllers/Club/ClubController.php
+++ app/Http/Controllers/Club/ClubController.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\DB;
 
 class ClubController extends Controller
 {
-    public function dashboard( $parentSlug = null, $slug, $userID, )
-    {    
+    public function dashboard($parentSlug = null, $slug, $userID,)
+    {
         $store = User::where('id', $userID)
-            ->with('projects.housings', 'housings', 'city', 'town', 'district', "parent",'neighborhood', 'brands', "child.collections.clicks", 'banners')
+            ->with('projects.housings', 'housings', 'city', 'town', 'district', "parent", 'neighborhood', 'brands', "child.collections.clicks", 'banners')
             ->first();
 
         if (empty($store->parent_id)) {
@@ -29,12 +29,12 @@ class ClubController extends Controller
                 ->where('user_id', $store->id)
                 ->get();
         }
-    
+
         // Hiç link yoksa koleksiyonu filtrele
         $collections = $collections->reject(function ($collection) {
             return $collection->links->isEmpty();
         });
-    
+
         return view('client.club.dashboard', compact('store', 'collections', 'slug'));
     }
     public function dashboardSatisNoktalari($slug, $userID)
@@ -43,40 +43,40 @@ class ClubController extends Controller
         $store = User::where('id', $userID)
             ->with('projects.housings', 'housings', 'city', 'town', 'district', 'parent', 'neighborhood', 'brands', 'child.collections.clicks', 'banners')
             ->first();
-    
+
         // Kullanıcının emlak ilanlarını ve projelerini al
         $projects = $store->projects; // Mağazanın projeleri
         $housings = $store->housings; // Mağazanın emlak ilanları
-    
+
         // Proje ve emlak ilanlarını içeren koleksiyonları al
-        $collectionsWithProjects = Collection::whereHas('links.project', function($query) use ($projects) {
+        $collectionsWithProjects = Collection::whereHas('links.project', function ($query) use ($projects) {
             $query->whereIn('id', $projects->pluck('id'));
         })->with('user')->get();
-    
-        $collectionsWithHousings = Collection::whereHas('links.housing', function($query) use ($housings) {
+
+        $collectionsWithHousings = Collection::whereHas('links.housing', function ($query) use ($housings) {
             $query->whereIn('id', $housings->pluck('id'));
         })->with('user')->get();
-    
+
         // Koleksiyonların kullanıcılarını al
         $usersFromCollections = $collectionsWithProjects->merge($collectionsWithHousings)
-        ->load('links', 'parent') // İlgili ilişkileri yükle
+            ->load('user.parent') // İlgili ilişkileri yükle
 
             ->pluck('user')
             ->unique()
             ->values();
-    
-            return $usersFromCollections;
+
+        return $usersFromCollections;
         return view('client.club.dashboardSatisNoktalari', [
             'usersFromCollections' => $usersFromCollections,
             'store' => $store
         ]);
     }
-    
 
-    public function dashboard2( $slug, $userID, )
-    {    
+
+    public function dashboard2($slug, $userID,)
+    {
         $store = User::where('id', $userID)
-            ->with('projects.housings', 'housings', 'city', 'town', 'district', "parent",'neighborhood', 'brands', "child.collections.clicks", 'banners')
+            ->with('projects.housings', 'housings', 'city', 'town', 'district', "parent", 'neighborhood', 'brands', "child.collections.clicks", 'banners')
             ->first();
 
         if (empty($store->parent_id)) {
@@ -89,14 +89,12 @@ class ClubController extends Controller
                 ->where('user_id', $store->id)
                 ->get();
         }
-    
+
         // Hiç link yoksa koleksiyonu filtrele
         $collections = $collections->reject(function ($collection) {
             return $collection->links->isEmpty();
         });
-    
+
         return view('client.club.dashboard', compact('store', 'collections', 'slug'));
     }
-    
-    
 }
