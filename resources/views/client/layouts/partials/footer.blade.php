9df096e56bc20d8aaea968648ffa8b194fafb434
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-08-08 13:28:33 +0300

    update

--- resources/views/client/layouts/partials/footer.blade.php
+++ resources/views/client/layouts/partials/footer.blade.php
@@ -188,39 +188,45 @@
             <table class="payment-plan table">
                 <div class="row align-items-center" style="width:100%;margin:0 auto">
                     <div class="col-md-12">
-                        <a id="whatsappButton" class="btn btn-outline-light"><svg xmlns="http://www.w3.org/2000/svg"
-                                x="0px" y="0px" width="18" height="18" viewBox="0 0 48 48">
-                                <path fill="#fff"
-                                    d="M4.9,43.3l2.7-9.8C5.9,30.6,5,27.3,5,24C5,13.5,13.5,5,24,5c5.1,0,9.8,2,13.4,5.6	C41,14.2,43,18.9,43,24c0,10.5-8.5,19-19,19c0,0,0,0,0,0h0c-3.2,0-6.3-0.8-9.1-2.3L4.9,43.3z">
-                                </path>
-                                <path fill="#fff"
-                                    d="M4.9,43.8c-0.1,0-0.3-0.1-0.4-0.1c-0.1-0.1-0.2-0.3-0.1-0.5L7,33.5c-1.6-2.9-2.5-6.2-2.5-9.6	C4.5,13.2,13.3,4.5,24,4.5c5.2,0,10.1,2,13.8,5.7c3.7,3.7,5.7,8.6,5.7,13.8c0,10.7-8.7,19.5-19.5,19.5c-3.2,0-6.3-0.8-9.1-2.3	L5,43.8C5,43.8,4.9,43.8,4.9,43.8z">
-                                </path>
-                                <path fill="#cfd8dc"
-                                    d="M24,5c5.1,0,9.8,2,13.4,5.6C41,14.2,43,18.9,43,24c0,10.5-8.5,19-19,19h0c-3.2,0-6.3-0.8-9.1-2.3	L4.9,43.3l2.7-9.8C5.9,30.6,5,27.3,5,24C5,13.5,13.5,5,24,5 M24,43L24,43L24,43 M24,43L24,43L24,43 M24,4L24,4C13,4,4,13,4,24	c0,3.4,0.8,6.7,2.5,9.6L3.9,43c-0.1,0.3,0,0.7,0.3,1c0.2,0.2,0.4,0.3,0.7,0.3c0.1,0,0.2,0,0.3,0l9.7-2.5c2.8,1.5,6,2.2,9.2,2.2	c11,0,20-9,20-20c0-5.3-2.1-10.4-5.8-14.1C34.4,6.1,29.4,4,24,4L24,4z">
-                                </path>
-                                <path fill="#40c351"
-                                    d="M35.2,12.8c-3-3-6.9-4.6-11.2-4.6C15.3,8.2,8.2,15.3,8.2,24c0,3,0.8,5.9,2.4,8.4L11,33l-1.6,5.8	l6-1.6l0.6,0.3c2.4,1.4,5.2,2.2,8,2.2h0c8.7,0,15.8-7.1,15.8-15.8C39.8,19.8,38.2,15.8,35.2,12.8z">
-                                </path>
-                                <path fill="#fff" fill-rule="evenodd"
-                                    d="M19.3,16c-0.4-0.8-0.7-0.8-1.1-0.8c-0.3,0-0.6,0-0.9,0	s-0.8,0.1-1.3,0.6c-0.4,0.5-1.7,1.6-1.7,4s1.7,4.6,1.9,4.9s3.3,5.3,8.1,7.2c4,1.6,4.8,1.3,5.7,1.2c0.9-0.1,2.8-1.1,3.2-2.3	c0.4-1.1,0.4-2.1,0.3-2.3c-0.1-0.2-0.4-0.3-0.9-0.6s-2.8-1.4-3.2-1.5c-0.4-0.2-0.8-0.2-1.1,0.2c-0.3,0.5-1.2,1.5-1.5,1.9	c-0.3,0.3-0.6,0.4-1,0.1c-0.5-0.2-2-0.7-3.8-2.4c-1.4-1.3-2.4-2.8-2.6-3.3c-0.3-0.5,0-0.7,0.2-1c0.2-0.2,0.5-0.6,0.7-0.8	c0.2-0.3,0.3-0.5,0.5-0.8c0.2-0.3,0.1-0.6,0-0.8C20.6,19.3,19.7,17,19.3,16z"
-                                    clip-rule="evenodd"></path>
-                            </svg><span class="ml-3">Ödeme Planını Paylaş</span>
-                        </a>
+                        <div class="d-flex align-items-center justify-space-between">
+                            <a id="whatsappButton" class="btn btn-outline-light"><svg
+                                    xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="18" height="18"
+                                    viewBox="0 0 48 48">
+                                    <path fill="#fff"
+                                        d="M4.9,43.3l2.7-9.8C5.9,30.6,5,27.3,5,24C5,13.5,13.5,5,24,5c5.1,0,9.8,2,13.4,5.6	C41,14.2,43,18.9,43,24c0,10.5-8.5,19-19,19c0,0,0,0,0,0h0c-3.2,0-6.3-0.8-9.1-2.3L4.9,43.3z">
+                                    </path>
+                                    <path fill="#fff"
+                                        d="M4.9,43.8c-0.1,0-0.3-0.1-0.4-0.1c-0.1-0.1-0.2-0.3-0.1-0.5L7,33.5c-1.6-2.9-2.5-6.2-2.5-9.6	C4.5,13.2,13.3,4.5,24,4.5c5.2,0,10.1,2,13.8,5.7c3.7,3.7,5.7,8.6,5.7,13.8c0,10.7-8.7,19.5-19.5,19.5c-3.2,0-6.3-0.8-9.1-2.3	L5,43.8C5,43.8,4.9,43.8,4.9,43.8z">
+                                    </path>
+                                    <path fill="#cfd8dc"
+                                        d="M24,5c5.1,0,9.8,2,13.4,5.6C41,14.2,43,18.9,43,24c0,10.5-8.5,19-19,19h0c-3.2,0-6.3-0.8-9.1-2.3	L4.9,43.3l2.7-9.8C5.9,30.6,5,27.3,5,24C5,13.5,13.5,5,24,5 M24,43L24,43L24,43 M24,43L24,43L24,43 M24,4L24,4C13,4,4,13,4,24	c0,3.4,0.8,6.7,2.5,9.6L3.9,43c-0.1,0.3,0,0.7,0.3,1c0.2,0.2,0.4,0.3,0.7,0.3c0.1,0,0.2,0,0.3,0l9.7-2.5c2.8,1.5,6,2.2,9.2,2.2	c11,0,20-9,20-20c0-5.3-2.1-10.4-5.8-14.1C34.4,6.1,29.4,4,24,4L24,4z">
+                                    </path>
+                                    <path fill="#40c351"
+                                        d="M35.2,12.8c-3-3-6.9-4.6-11.2-4.6C15.3,8.2,8.2,15.3,8.2,24c0,3,0.8,5.9,2.4,8.4L11,33l-1.6,5.8	l6-1.6l0.6,0.3c2.4,1.4,5.2,2.2,8,2.2h0c8.7,0,15.8-7.1,15.8-15.8C39.8,19.8,38.2,15.8,35.2,12.8z">
+                                    </path>
+                                    <path fill="#fff" fill-rule="evenodd"
+                                        d="M19.3,16c-0.4-0.8-0.7-0.8-1.1-0.8c-0.3,0-0.6,0-0.9,0	s-0.8,0.1-1.3,0.6c-0.4,0.5-1.7,1.6-1.7,4s1.7,4.6,1.9,4.9s3.3,5.3,8.1,7.2c4,1.6,4.8,1.3,5.7,1.2c0.9-0.1,2.8-1.1,3.2-2.3	c0.4-1.1,0.4-2.1,0.3-2.3c-0.1-0.2-0.4-0.3-0.9-0.6s-2.8-1.4-3.2-1.5c-0.4-0.2-0.8-0.2-1.1,0.2c-0.3,0.5-1.2,1.5-1.5,1.9	c-0.3,0.3-0.6,0.4-1,0.1c-0.5-0.2-2-0.7-3.8-2.4c-1.4-1.3-2.4-2.8-2.6-3.3c-0.3-0.5,0-0.7,0.2-1c0.2-0.2,0.5-0.6,0.7-0.8	c0.2-0.3,0.3-0.5,0.5-0.8c0.2-0.3,0.1-0.6,0-0.8C20.6,19.3,19.7,17,19.3,16z"
+                                        clip-rule="evenodd"></path>
+                                </svg><span class="ml-3">Ödeme Planını Paylaş</span>
+                            </a>
+                            <span class="btn addCollection mobileAddCollection" data-type='project'>
+                                <i class="fa fa-bookmark-o"></i> Koleksiyona Ekle & Hızlı Sat
+                            </span>
+                        </div>
+
                     </div>
                     <div class="col-md-12">
                         @if (
                             (Auth::check() && Auth::user()->type == '2' && Auth::user()->corporate_type == 'Emlak Ofisi') ||
                                 (Auth::check() && Auth::user()->type == '1'))
-                                <div class="d-flex align-items-center justify-content-center pb-3">
-                                    <img src="{{asset('demo-beauty-salon-icon-01.webp')}}" alt=""
-                                    style="width: 30px;margin-right:5px"/>
-                                    <span class="textAlert"
-                                        style="width: 100%;
+                            <div class="d-flex align-items-center justify-content-center pb-3">
+                                <img src="{{ asset('demo-beauty-salon-icon-01.webp') }}" alt=""
+                                    style="width: 25px;margin-right:5px" />
+                                <span class="textAlert"
+                                    style="width: 100%;
                                         display: block;">
-                                       </span>
-                                </div>
-                               
+                                </span>
+                            </div>
                         @endif
                     </div>
 
@@ -1103,11 +1109,12 @@
                                             discount +
                                             " oranındaki indirimle müşterilerinize sunmanıza aracılık eder. Bu fırsatı değerlendirin ve ilanın avantajlarını müşterilerinize daha etkili bir şekilde iletin.";
 
-                                            if (talepSayisi > 0) {
-                alertMessage += " Ayrıca, bu ilana şu anda <strong><span style='color: #EC2F2E;'>" +
-                    talepSayisi +
-                    " ilgili müşteri var</span></strong>. İlana olan ilgi yüksek ve bu fırsatı kaçırmayın!";
-            }
+                                        if (talepSayisi > 0) {
+                                            alertMessage +=
+                                                " Ayrıca, bu ilana şu anda <strong><span style='color: #EC2F2E;'>" +
+                                                talepSayisi +
+                                                " ilgili müşteri var</span></strong>. İlana olan ilgi yüksek ve bu fırsatı kaçırmayın!";
+                                        }
                                     } else if (userCheck.type === "1") {
                                         alertMessage =
                                             "Emlak Sepette, bu ilanı en yakın emlak ofisinizin koleksiyonundan %" +
@@ -1121,7 +1128,15 @@
 
 
 
+                            // Elemanı seç
+                            var element = document.querySelector(
+                                '.btn.addCollection.mobileAddCollection');
 
+                            // data-type özelliğini kontrol et ve mevcutsa data-project ve data-id ekle
+                            if (element) {
+                                element.setAttribute('data-project', projectId);
+                                element.setAttribute('data-id', paymentPlanId);
+                            }
 
 
 
